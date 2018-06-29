b8dff783e1f9883bf090b1e69ffca86f0de957cc
Arturo Filastò <arturo@filasto.net>
2018-06-29 15:10:19 +0200

    Add sentry for sending crash logs

--- cmd/ooni/main.go
+++ cmd/ooni/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	// commands
 	"github.com/apex/log"
+	"github.com/getsentry/raven-go"
 
 	_ "github.com/ooni/probe-cli/internal/cli/geoip"
 	_ "github.com/ooni/probe-cli/internal/cli/info"
@@ -18,6 +19,8 @@ import (
 )
 
 func main() {
+	raven.SetDSN("https://cb4510e090f64382ac371040c19b2258:8448daeebfa643c289ef398f8645980b@sentry.io/1234954")
+
 	err := app.Run()
 	if err == nil {
 		return
