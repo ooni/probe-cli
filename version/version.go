a488c8d2a32d2cd5878524f819ac933bdca1bf65
Simone Basso <bassosimone@gmail.com>
2019-10-03 11:22:13 +0200

    version.go: actually bump version
    Classic mistake: you don't bump the version.
    
    We need https://github.com/ooni/probe-cli/issues/43.

--- version/version.go
+++ version/version.go
@@ -3,7 +3,7 @@ package version
 
 const (
 	// Version is the software version
-	Version = "3.0.0-beta.3"
+	Version = "3.0.0-beta.4"
 
 	// UserAgent is the OONI user-agent header
 	UserAgent = "ooniprobe-desktop/" + Version
