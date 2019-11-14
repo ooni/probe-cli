222b0bfbca84e4547d8507261b7e1f847776c2ec
Arturo Filastò <arturo@filasto.net>
2019-11-14 18:03:31 +0100

    Bump version number

--- version/version.go
+++ version/version.go
@@ -3,7 +3,7 @@ package version
 
 const (
 	// Version is the software version
-	Version = "3.0.0-rc.2"
+	Version = "3.0.0-rc.3"
 
 	// UserAgent is the OONI user-agent header
 	UserAgent = "ooniprobe-desktop/" + Version
