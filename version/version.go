6391e25154ab92df9571d333d34f4cb7eace3f4a
Simone Basso <bassosimone@gmail.com>
2020-01-28 10:14:45 +0100

    version/version.go: roll out 3.0.0-rc.7

--- version/version.go
+++ version/version.go
@@ -3,7 +3,7 @@ package version
 
 const (
 	// Version is the software version
-	Version = "3.0.0-rc.6"
+	Version = "3.0.0-rc.7"
 
 	// UserAgent is the OONI user-agent header
 	UserAgent = "ooniprobe-desktop/" + Version
