2f5486dbd601d89f09d41cb40aff71f5e4af0052
Arturo Filastò <arturo@filasto.net>
2020-01-31 10:49:16 +0100

    Bump tag

--- version/version.go
+++ version/version.go
@@ -3,7 +3,7 @@ package version
 
 const (
 	// Version is the software version
-	Version = "3.0.0-rc.7"
+	Version = "3.0.0-rc.8"
 
 	// UserAgent is the OONI user-agent header
 	UserAgent = "ooniprobe-desktop/" + Version
