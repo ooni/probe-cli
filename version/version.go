4a563ebe1396c1d368d5e8cfff52fe73c78a899f
Simone Basso <bassosimone@gmail.com>
2020-04-06 13:22:10 +0200

    Pin to latest probe-engine and bless v3.0.0-rc.13
    I'm pinning to ooni/probe-engine@72c12b0b81bbbbf814ccec7f75190268a49960f8
    because it generates more correct data and I think we're going to have
    most likely this probe-cli version in the stable desktop release.

--- version/version.go
+++ version/version.go
@@ -3,7 +3,7 @@ package version
 
 const (
 	// Version is the software version
-	Version = "3.0.0-rc.12"
+	Version = "3.0.0-rc.13"
 
 	// UserAgent is the OONI user-agent header
 	UserAgent = "ooniprobe-desktop/" + Version
