ec10e54b5c19c09e50b9ad1b14a302d991178348
Simone Basso <bassosimone@gmail.com>
2020-04-08 13:05:00 +0200

    Bless v3.0.0-rc.14

--- version/version.go
+++ version/version.go
@@ -3,7 +3,7 @@ package version
 
 const (
 	// Version is the software version
-	Version = "3.0.0-rc.13"
+	Version = "3.0.0-rc.14"
 
 	// UserAgent is the OONI user-agent header
 	UserAgent = "ooniprobe-desktop/" + Version
