a9897466b689d81d142714af6556990d3e31dbf4
Simone Basso <bassosimone@gmail.com>
2020-07-10 11:25:04 +0200

    version/version.go: crank patch version number

--- version/version.go
+++ version/version.go
@@ -3,7 +3,7 @@ package version
 
 const (
 	// Version is the software version
-	Version = "3.0.3"
+	Version = "3.0.4"
 
 	// UserAgent is the OONI user-agent header
 	UserAgent = "ooniprobe-desktop/" + Version
