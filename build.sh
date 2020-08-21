a2b8cf12a30c449483529f2b015b29ccd8617763
Simone Basso <bassosimone@gmail.com>
2020-08-21 18:05:23 +0200

    oh, we're missing a git repository

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 set -ex
 
+v=`git describe --tags || echo $GITHUB_SHA`
+
 case $1 in
   windows)
-    v=`git describe --tags`
     # Note! This assumes we've installed the mingw-w64 compiler.
     GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc \
       go build -ldflags='-s -w' ./cmd/ooniprobe
@@ -13,7 +14,6 @@ case $1 in
     ;;
 
   linux)
-    v=`git describe --tags`
     docker run -v`pwd`:/ooni -w/ooni golang:1.14-alpine ./build.sh _alpine
     tar -cvzf ooniprobe_${v}_linux_amd64.tar.gz LICENSE.md Readme.md ooniprobe
     mv ooniprobe ./CLI/linux/amd64/
@@ -25,7 +25,6 @@ case $1 in
     ;;
 
   macos)
-    v=`git describe --tags`
     # Note! The following line _assumes_ you have a working C compiler. If you
     # have Xcode command line tools installed, you are fine.
     go build -ldflags='-s -w' ./cmd/ooniprobe
