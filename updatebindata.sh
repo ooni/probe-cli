9a59f62df1dcb014756c0cf7ef2d86caacb293d0
Simone Basso <bassosimone@gmail.com>
2020-08-21 15:08:39 +0200

    x

--- updatebindata.sh
+++ updatebindata.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 set -e
+
+fatal() {
+  echo ""
+  echo "FATAL: cannot find go-bindata binary in `go env GOPATH`"
+  echo ""
+  echo "Please install/update go-bindata with:"
+  echo ""
+  echo "  go get -u github.com/shuLhan/go-bindata/..."
+  echo ""
+  exit 1
+}
+
 gobindata=`go env GOPATH`/bin/go-bindata
+if [ ! -x $gobindata ]; then
+  fatal
+fi
 version=`$gobindata -version | grep go-bin | cut -d ' ' -f2`
 if [ "$version" != "3.3.0" ]; then
-    echo ""
-    echo "FATAL: cannot find go-bindata binary in `go env GOPATH`"
-    echo ""
-    echo "Please install/update go-bindata with:"
-    echo ""
-    echo "  go get -u github.com/shuLhan/go-bindata/..."
-    echo ""
-    exit 1
+  fatal
 fi
 set -x
 $gobindata -nometadata -o internal/bindata/bindata.go -pkg bindata data/...
