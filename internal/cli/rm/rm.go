cffe199caf00bb4bcc021be43a1966afcd34a7fb
Arturo Filastò <arturo@filasto.net>
2020-11-09 15:01:54 +0100

    Add support for deleting all measurements
    Implements: https://github.com/ooni/probe/issues/1276

--- internal/cli/rm/rm.go
+++ internal/cli/rm/rm.go
@@ -12,9 +12,46 @@ import (
 	db "upper.io/db.v3"
 )
 
+func deleteAll(sess sqlbuilder.Database, skipInteractive bool) error {
+	if skipInteractive == false {
+		answer := ""
+		confirm := &survey.Select{
+			Message: fmt.Sprintf("Are you sure you wish to delete ALL results"),
+			Options: []string{"true", "false"},
+			Default: "false",
+		}
+		survey.AskOne(confirm, &answer, nil)
+		if answer == "false" {
+			return errors.New("canceled by user")
+		}
+	}
+	doneResults, incompleteResults, err := database.ListResults(sess)
+	if err != nil {
+		log.WithError(err).Error("failed to list results")
+		return err
+	}
+	cnt := 0
+	for _, result := range incompleteResults {
+		err = database.DeleteResult(ctx.DB, result.Result.ID)
+		if err == db.ErrNoMoreRows {
+			log.WithError(err).Errorf("failed to delete resul #%d", result.Result.ID)
+		}
+		cnt += 1
+	}
+	for _, result := range doneResults {
+		err = database.DeleteResult(ctx.DB, result.Result.ID)
+		if err == db.ErrNoMoreRows {
+			log.WithError(err).Errorf("failed to delete resul #%d", result.Result.ID)
+		}
+		cnt += 1
+	}
+	log.Infof("Deleted #%d measurements", cnt)
+}
+
 func init() {
 	cmd := root.Command("rm", "Delete a result")
 	yes := cmd.Flag("yes", "Skip interactive prompt").Bool()
+	yes := cmd.Flag("all", "Delete all measurements").Bool()
 
 	resultID := cmd.Arg("id", "the id of the result to delete").Int64()
 
@@ -23,6 +60,10 @@ func init() {
 		if err != nil {
 			log.Errorf("%s", err)
 			return err
+		}
+
+		if *all == true {
+			return deleteAll(ctx.DB, *yes)
 		}
 
 		if *yes == true {
