7eedf4d9472eef2c7d8b22f9bb9cdb9eee33daaa
Simone Basso <bassosimone@gmail.com>
2020-11-13 21:16:43 +0100

    feat(geoip, info): write comprehensive unit tests

--- internal/cli/geoip/geoip.go
+++ internal/cli/geoip/geoip.go
@@ -4,38 +4,54 @@ import (
 	"github.com/alecthomas/kingpin"
 	"github.com/apex/log"
 	"github.com/ooni/probe-cli/internal/cli/root"
+	"github.com/ooni/probe-cli/internal/ooni"
 	"github.com/ooni/probe-cli/internal/output"
 )
 
 func init() {
 	cmd := root.Command("geoip", "Perform a geoip lookup")
-
 	cmd.Action(func(_ *kingpin.ParseContext) error {
-		output.SectionTitle("GeoIP lookup")
-		probeCLI, err := root.Init()
-		if err != nil {
-			return err
-		}
-
-		engine, err := probeCLI.NewProbeEngine()
-		if err != nil {
-			return err
-		}
-		defer engine.Close()
-
-		err = engine.MaybeLookupLocation()
-		if err != nil {
-			return err
-		}
-
-		log.WithFields(log.Fields{
-			"type":         "table",
-			"asn":          engine.ProbeASNString(),
-			"network_name": engine.ProbeNetworkName(),
-			"country_code": engine.ProbeCC(),
-			"ip":           engine.ProbeIP(),
-		}).Info("Looked up your location")
-
-		return nil
+		return dogeoip(defaultconfig)
 	})
+}
+
+type dogeoipconfig struct {
+	Logger       log.Interface
+	NewProbeCLI  func() (ooni.ProbeCLI, error)
+	SectionTitle func(string)
+}
+
+var defaultconfig = dogeoipconfig{
+	Logger:       log.Log,
+	NewProbeCLI:  root.NewProbeCLI,
+	SectionTitle: output.SectionTitle,
+}
+
+func dogeoip(config dogeoipconfig) error {
+	config.SectionTitle("GeoIP lookup")
+	probeCLI, err := config.NewProbeCLI()
+	if err != nil {
+		return err
+	}
+
+	engine, err := probeCLI.NewProbeEngine()
+	if err != nil {
+		return err
+	}
+	defer engine.Close()
+
+	err = engine.MaybeLookupLocation()
+	if err != nil {
+		return err
+	}
+
+	config.Logger.WithFields(log.Fields{
+		"type":         "table",
+		"asn":          engine.ProbeASNString(),
+		"network_name": engine.ProbeNetworkName(),
+		"country_code": engine.ProbeCC(),
+		"ip":           engine.ProbeIP(),
+	}).Info("Looked up your location")
+
+	return nil
 }
--- internal/cli/info/info.go
+++ internal/cli/info/info.go
@@ -4,24 +4,33 @@ import (
 	"github.com/alecthomas/kingpin"
 	"github.com/apex/log"
 	"github.com/ooni/probe-cli/internal/cli/root"
+	"github.com/ooni/probe-cli/internal/ooni"
 )
 
 func init() {
 	cmd := root.Command("info", "Display information about OONI Probe")
-
 	cmd.Action(func(_ *kingpin.ParseContext) error {
-		probeCLI, err := root.Init()
-		if err != nil {
-			log.Errorf("%s", err)
-			return err
-		}
-		log.WithFields(log.Fields{
-			"path": probeCLI.Home(),
-		}).Info("Home")
-		log.WithFields(log.Fields{
-			"path": probeCLI.TempDir(),
-		}).Info("TempDir")
-
-		return nil
+		return doinfo(defaultconfig)
 	})
+}
+
+type doinfoconfig struct {
+	Logger      log.Interface
+	NewProbeCLI func() (ooni.ProbeCLI, error)
+}
+
+var defaultconfig = doinfoconfig{
+	Logger:      log.Log,
+	NewProbeCLI: root.NewProbeCLI,
+}
+
+func doinfo(config doinfoconfig) error {
+	probeCLI, err := config.NewProbeCLI()
+	if err != nil {
+		config.Logger.Errorf("%s", err)
+		return err
+	}
+	config.Logger.WithFields(log.Fields{"path": probeCLI.Home()}).Info("Home")
+	config.Logger.WithFields(log.Fields{"path": probeCLI.TempDir()}).Info("TempDir")
+	return nil
 }
