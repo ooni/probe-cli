823d96ac85c3526182d49513f6504b2d675a96b3
Simone Basso <bassosimone@gmail.com>
2021-02-02 09:09:29 +0100

    fix(build.sh): build inside v3
    While there, hide unexported actions and further split actions
    such that they can be invoked from many systems.

--- build.sh
+++ build.sh
@@ -7,24 +7,36 @@ v=`git describe --tags || echo $GITHUB_SHA`
 case $1 in
   windows)
     set -x
-    $0 windows_amd64
-    $0 windows_386
+    $0 __windows_amd64
+    $0 __windows_386
     ;;
 
-  windows_amd64)
+  __build_windows_amd64)
     # Note! This assumes we've installed the mingw-w64 compiler.
-    GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc \
-      go build -ldflags='-s -w' ./cmd/ooniprobe
+    set -x
+    cd v3 && GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc \
+      go build -ldflags='-s -w' -o ../ooniprobe.exe ./cmd/ooniprobe
+    ;;
+
+  __windows_amd64)
+    set -x
+    $0 __build_windows_amd64
     tar -cvzf ooniprobe_${v}_windows_amd64.tar.gz LICENSE.md Readme.md ooniprobe.exe
     # We don't have zip inside the github actions runner
     zip ooniprobe_${v}_windows_amd64.zip LICENSE.md Readme.md ooniprobe.exe || true
     mv ooniprobe.exe ./CLI/windows/amd64/
     ;;
 
-  windows_386)
+  __build_windows_386)
     # Note! This assumes we've installed the mingw-w64 compiler.
-    GOOS=windows GOARCH=386 CGO_ENABLED=1 CC=i686-w64-mingw32-gcc \
-      go build -ldflags='-s -w' ./cmd/ooniprobe
+    set -x
+    cd v3 && GOOS=windows GOARCH=386 CGO_ENABLED=1 CC=i686-w64-mingw32-gcc \
+      go build -ldflags='-s -w' -o ../ooniprobe.exe ./cmd/ooniprobe
+    ;;
+
+  __windows_386)
+    set -x
+    $0 __build_windows_386
     tar -cvzf ooniprobe_${v}_windows_386.tar.gz LICENSE.md Readme.md ooniprobe.exe
     # We don't have zip inside the github actions runner
     zip ooniprobe_${v}_windows_386.zip LICENSE.md Readme.md ooniprobe.exe || true
@@ -33,36 +45,50 @@ case $1 in
 
   linux)
     set -x
-    $0 linux_amd64
-    $0 linux_386
+    $0 __linux_amd64
+    $0 __linux_386
     ;;
 
-  linux_amd64)
+  __linux_amd64)
     docker pull --platform linux/amd64 golang:1.14-alpine
-    docker run --platform linux/amd64 -v`pwd`:/ooni -w/ooni golang:1.14-alpine ./build.sh _alpine
+    docker run --platform linux/amd64 -v`pwd`:/ooni -w/ooni golang:1.14-alpine \
+      ./build.sh __alpine
     tar -cvzf ooniprobe_${v}_linux_amd64.tar.gz LICENSE.md Readme.md ooniprobe
     mv ooniprobe ./CLI/linux/amd64/
     ;;
 
-  linux_386)
+  __linux_386)
     docker pull --platform linux/386 golang:1.14-alpine
-    docker run --platform linux/386 -v`pwd`:/ooni -w/ooni golang:1.14-alpine ./build.sh _alpine
+    docker run --platform linux/386 -v`pwd`:/ooni -w/ooni golang:1.14-alpine \
+      ./build.sh __alpine
     tar -cvzf ooniprobe_${v}_linux_386.tar.gz LICENSE.md Readme.md ooniprobe
     mv ooniprobe ./CLI/linux/386/
     ;;
 
-  _alpine)
+  __alpine)
+    set -x
     apk update
     apk upgrade
     apk add --no-progress gcc git linux-headers musl-dev
-    go build -tags netgo -ldflags='-s -w -extldflags "-static"' ./cmd/ooniprobe
+    $0 __build_linux_static
+    ;;
+
+  __build_linux_static)
+    set -x
+    cd v3 && go build -tags netgo -ldflags='-s -w -extldflags "-static"' \
+      -o ../ooniprobe ./cmd/ooniprobe
+    ;;
+
+  __build_unix)
+    # Note! The following lines _assumes_ you have a working C compiler. If you
+    # have Xcode command line tools installed, you are fine.
+    set -x
+    cd v3 && go build -ldflags='-s -w' -o ../ooniprobe ./cmd/ooniprobe
     ;;
 
   macos|darwin)
     set -x
-    # Note! The following line _assumes_ you have a working C compiler. If you
-    # have Xcode command line tools installed, you are fine.
-    go build -ldflags='-s -w' ./cmd/ooniprobe
+    $0 __build_unix
     tar -cvzf ooniprobe_${v}_darwin_amd64.tar.gz LICENSE.md Readme.md ooniprobe
     mv ooniprobe ./CLI/darwin/amd64/
     ;;
@@ -75,8 +101,6 @@ case $1 in
     ;;
 
   *)
-
-    set +x
     echo "Usage: $0 darwin|linux|macos|windows|release"
     echo ""
     echo "You need a C compiler and Go >= 1.14. The C compiler must be a"
