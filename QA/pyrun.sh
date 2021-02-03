0c48bc0746d5036603e648c7110bde0ab20258d9
Simone Basso <bassosimone@gmail.com>
2021-02-03 13:20:37 +0100

    refactor: enable QA tests and jafar self test (#208)
    * refactor: enable QA tests and jafar self test
    
    Part of https://github.com/ooni/probe/issues/1335
    
    * chore: make sure all workflows run on release branches

--- QA/pyrun.sh
+++ QA/pyrun.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -ex
 export GOPATH=/jafar/QA/GOPATH GOCACHE=/jafar/QA/GOCACHE GO111MODULE=on
-go build -v ./cmd/miniooni
-go build -v ./cmd/jafar
+go build -v ./internal/cmd/miniooni
+go build -v ./internal/cmd/jafar
 sudo ./QA/$1.py ./miniooni
