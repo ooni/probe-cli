978cd28d881e446b4990fa52dc2d08260ac14bad
Arturo Filastò <arturo@openobservatory.org>
2021-02-12 19:49:18 +0100

    Disable sentry based crash reporting in probe-cli (#228)

--- cmd/ooniprobe/main.go
+++ cmd/ooniprobe/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/apex/log"
 	"github.com/ooni/probe-cli/v3/cmd/ooniprobe/internal/cli/app"
 	_ "github.com/ooni/probe-cli/v3/cmd/ooniprobe/internal/cli/autorun"
 	_ "github.com/ooni/probe-cli/v3/cmd/ooniprobe/internal/cli/geoip"
@@ -14,12 +13,8 @@ import (
 	_ "github.com/ooni/probe-cli/v3/cmd/ooniprobe/internal/cli/show"
 	_ "github.com/ooni/probe-cli/v3/cmd/ooniprobe/internal/cli/upload"
 	_ "github.com/ooni/probe-cli/v3/cmd/ooniprobe/internal/cli/version"
-	"github.com/ooni/probe-cli/v3/cmd/ooniprobe/internal/crashreport"
 )
 
 func main() {
-	if err, _ := crashreport.CapturePanic(app.Run, nil); err != nil {
-		log.WithError(err.(error)).Error("panic in app.Run")
-		crashreport.Wait()
-	}
+	app.Run()
 }
