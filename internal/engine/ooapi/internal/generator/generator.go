d61b1ed4582b7f3d852a5877c81e1d6d05c3771c
Simone Basso <bassosimone@gmail.com>
2021-02-26 15:45:59 +0100

    fix(ooapi): only generate each file once

--- internal/engine/ooapi/internal/generator/generator.go
+++ internal/engine/ooapi/internal/generator/generator.go
@@ -13,21 +13,41 @@
 // `go generate ./...` command to regenerate all files.
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
+var flagFile = flag.String("file", "", "Indicate which file to regenerate")
+
 func main() {
-	// TODO(bassosimone): the current generator algorithm is not
-	// efficient because we re-generate all files each time. We
-	// should instead use a flag to tell the code which is the file
-	// we want to regenerate for each invocation.
-	GenAPIsGo()
-	GenResponsesGo()
-	GenRequestsGo()
-	GenSwaggerGo()
-	GenAPIsTestGo()
-	GenCallersGo()
-	GenCachingGo()
-	GenLoginGo()
-	GenClonersGo()
-	GenFakeAPITestGo()
-	GenCachingTestGo()
-	GenLoginTestGo()
+	flag.Parse()
+	switch file := *flagFile; file {
+	case "apis.go":
+		GenAPIsGo(file)
+	case "responses.go":
+		GenResponsesGo(file)
+	case "requests.go":
+		GenRequestsGo(file)
+	case "swagger_test.go":
+		GenSwaggerTestGo(file)
+	case "apis_test.go":
+		GenAPIsTestGo(file)
+	case "callers.go":
+		GenCallersGo(file)
+	case "caching.go":
+		GenCachingGo(file)
+	case "login.go":
+		GenLoginGo(file)
+	case "cloners.go":
+		GenClonersGo(file)
+	case "fakeapi_test.go":
+		GenFakeAPITestGo(file)
+	case "caching_test.go":
+		GenCachingTestGo(file)
+	case "login_test.go":
+		GenLoginTestGo(file)
+	default:
+		panic(fmt.Sprintf("don't know how to create this file: %s", file))
+	}
 }
