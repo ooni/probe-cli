7be7da385c92114e5703ad62f2357a5f65ba8290
Simone Basso <bassosimone@gmail.com>
2021-03-10 10:25:10 +0100

    fix(sessionresolver): honour the proxy
    In reality, we are not going to use the sessionresolver when we're
    using a proxy (I just tested). But, it nonetheless feels a lot more
    robust to write a correct sessionresolver that handles the proxy
    in the most correct way. That is, the sessionresolver will now skip
    all the entries that cannot use a socks5 proxy (including among them
    also the system resolver). What's more, it will construct a child
    resolver that propagates the proxy.
    
    We have confidence that this holds true because we have added a test
    ensuring that we are really using the configured proxy.
    
    See https://github.com/ooni/probe/issues/1381

--- internal/engine/session.go
+++ internal/engine/session.go
@@ -108,14 +108,15 @@ func NewSession(config SessionConfig) (*Session, error) {
 		ByteCounter:  sess.byteCounter,
 		BogonIsError: true,
 		Logger:       sess.logger,
+		ProxyURL:     config.ProxyURL,
 	}
 	sess.resolver = &sessionresolver.Resolver{
 		ByteCounter: sess.byteCounter,
 		KVStore:     config.KVStore,
 		Logger:      sess.logger,
+		ProxyURL:    config.ProxyURL,
 	}
 	httpConfig.FullResolver = sess.resolver
-	httpConfig.ProxyURL = config.ProxyURL // no need to proxy the resolver
 	sess.httpDefaultTransport = netx.NewHTTPTransport(httpConfig)
 	return sess, nil
 }
