f5b5ac47b0a7364c4238dbaad03b6c1972cd8da4
Simone Basso <bassosimone@gmail.com>
2021-03-29 19:03:53 +0200

    refactor: merge libminiooni into cmd/miniooni (#268)
    We used to have an external package called libminiooni so that
    third parties could use it. We wrote this such that we could
    support github.com/bassosimone/aladdin.
    
    That was actually a not-so-good idea because it added to the APIs
    we needed to maintain.
    
    Since the merge of engine into cli, such an API is not public
    anymore and aladdin has been deprecated and archived.
    
    Therefore, we can now cleanup the situation and merge libminiooni
    into miniooni again, thus making the codebase more local.
    
    This cleanup has been identified while working on
    https://github.com/ooni/probe/issues/1299.

--- internal/cmd/miniooni/libminiooni.go
+++ internal/cmd/miniooni/libminiooni.go
@@ -1,21 +1,4 @@
-// Package libminiooni implements the cmd/miniooni CLI. Miniooni is our
-// experimental client used for research and QA testing.
-//
-// This CLI has CLI options that do not conflict with Measurement Kit
-// v0.10.x CLI options. There are some options conflict with the legacy
-// OONI Probe CLI options. Perfect backwards compatibility is not a
-// design goal for miniooni. Rather, we aim to have as little conflict
-// as possible such that we can run side by side QA checks.
-//
-// We extracted this package from cmd/miniooni to allow us to further
-// integrate the miniooni CLI into other binaries (see for example the
-// code at github.com/bassosimone/aladdin).
-package libminiooni
-
-// TODO(bassosimone): now that this code cannot be imported from
-// external sources anymore, we should move it back into the
-// internal/cmd/miniooni folder and reduce the number of packages
-// that are unnecessarily exposed inside ./internal/engine.
+package main
 
 // TODO(bassosimone): we need to deprecate or remove --limit.
 
