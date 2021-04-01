3c625001600c26061683eca2fc9f68ecdea3d98c
Simone Basso <bassosimone@gmail.com>
2021-04-01 18:56:44 +0200

    chore(oohelper): increase tests verbosity
    Hopefully this helps with https://github.com/ooni/probe/issues/1409.

--- internal/cmd/oohelper/oohelper_test.go
+++ internal/cmd/oohelper/oohelper_test.go
@@ -3,6 +3,7 @@ package main
 import "testing"
 
 func TestSmoke(t *testing.T) {
+	*debug = true // To help with https://github.com/ooni/probe/issues/1409
 	*target = "http://www.example.com"
 	main()
 }
