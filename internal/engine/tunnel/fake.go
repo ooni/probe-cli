9f3270b301daa310924ae9272b934e9578face29
Simone Basso <bassosimone@gmail.com>
2021-04-05 18:17:37 +0200

    fix comment

--- internal/engine/tunnel/fake.go
+++ internal/engine/tunnel/fake.go
@@ -40,6 +40,16 @@ func (t *fakeTunnel) SOCKS5ProxyURL() *url.URL {
 
 // fakeStart starts the fake tunnel.
 func fakeStart(ctx context.Context, config *Config) (Tunnel, error) {
+	// do the same things other tunnels do:
+	//
+	// 1. abort if context is cancelled
+	//
+	// 2. check for tunnelDir being not empty
+	//
+	// 3. attempt to create tunnelDir
+	//
+	// after that, it's all fake and we just create a simple
+	// socks5 server that we can use
 	select {
 	case <-ctx.Done():
 		return nil, ctx.Err() // simplifies unit testing this code
