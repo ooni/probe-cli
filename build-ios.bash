1cbc0c04b082b4a5913db84f79e4c029822bf1eb
Simone Basso <bassosimone@gmail.com>
2021-04-08 16:33:26 +0200

    feat(ios): adapt build to post bintray world
    Part of https://github.com/ooni/probe/issues/1437

--- build-ios.bash
+++ build-ios.bash
@@ -7,3 +7,9 @@ go get -u golang.org/x/mobile/cmd/gomobile
 gomobile init
 output=MOBILE/ios/oonimkall.framework
 gomobile bind -target=ios -o $output -ldflags="-s -w" ./pkg/oonimkall
+release=$(git describe --tags)
+version=$(date -u +%Y.%m.%d-%H%M%S)
+podspecfile=./MOBILE/ios/oonimkall.podspec
+(cd ./MOBILE/ios && rm -f oonimkall.framework.zip && zip -yr oonimkall.framework.zip oonimkall.framework)
+podspectemplate=./MOBILE/template.podspec
+cat $podspectemplate|sed -e "s/@VERSION@/$version/g" -e "s/@RELEASE@/$release/g" > $podspecfile
