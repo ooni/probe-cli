34f47495c31207ba4a4aff5f85487eb9ecc50297
Simone Basso <bassosimone@gmail.com>
2021-04-22 18:37:17 +0200

    improvements to build script

--- build-miniooni.sh
+++ build-miniooni.sh
@@ -3,7 +3,7 @@ set -e
 case $1 in
   macos|darwin|linux|windows)
     set -x
-    ./build miniooni --no-embed-psiphon --no-ooni-go "$1"
+    ./build miniooni --no-embed-psiphon --no-download-go "$1"
     ;;
   *)
     echo "usage: $0 darwin|linux|windows" 1>&2
