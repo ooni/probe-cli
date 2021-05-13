6125eb96a6fde712c04547db651914273746c6b2
Simone Basso <bassosimone@gmail.com>
2021-05-13 18:04:47 +0200

    refactor: reorganize and to end and smoke testing (#348)
    1. we can merge the e2eminiooni.yml test into the miniooni.yml test
    so to reduce the number of tests we run;
    
    2. ideally we would like the smoketest.sh test to evolve and also
    check whether we can fetch the measurements we submitted, so start
    moving this script into the `./E2E` folder, add a note saying we
    would like to do that, and direct all the tests to run this script
    at its new location and with its new name (`ooniprobe.sh`).
    
    With these two changes, it's fine to remove the ooniprobe2debian.yml
    test in ooni/e2etesting because we're moving its functionality to this
    repository. (We mentioned the need to do this move in a previous TODO
    comment at the top of such a script.)
    
    Work part of https://github.com/ooni/probe/issues/1468

--- smoketest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -ex
-if [ "$#" != 1 ]; then
-    echo "Usage: $0 <binary>" 1>&2
-    exit 1
-fi
-$1 onboard --yes
-# Important! DO NOT run performance from CI b/c it will overload m-lab servers
-$1 run websites --config cmd/ooniprobe/testdata/testing-config.json --no-collector
