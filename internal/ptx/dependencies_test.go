8e8924d0333118c257bb6af5293012b332098995
Simone Basso <bassosimone@gmail.com>
2021-06-10 18:36:43 +0200

    Update internal/ptx/dependencies_test.go
    Co-authored-by: Arturo Filastò <arturo@openobservatory.org>

--- internal/ptx/dependencies_test.go
+++ internal/ptx/dependencies_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestCoverSilentLogger(t *testing.T) {
 	// let us not be distracted by uncovered lines that can
-	// easily covered, we can easily cover defaultLogger
+	// easily be covered, we can easily cover defaultLogger
 	defaultLogger.Debugf("foo")
 	defaultLogger.Infof("bar")
 	defaultLogger.Warnf("baz")
