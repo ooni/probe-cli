40398b77d3f24d401980e8ef9c0e5a7939e3d9a0
Simone Basso <bassosimone@gmail.com>
2021-06-30 15:47:20 +0200

    refactor: introduce factory for stdlib http transport
    With this factory, we want to construct ourselves the TLS dialer
    so that we can use a dialer wrapper that always sets timeouts when
    reading, addressing https://github.com/ooni/probe/issues/1609.
    
    As a result, we cannot immediately replace the i/e/netx factory
    for creating a new HTTP transport, since the functions signatures
    are not directly compatible.
    
    Refactoring is part of https://github.com/ooni/probe/issues/1505.

--- internal/engine/netx/httptransport/http3transport.go
+++ internal/engine/netx/httptransport/http3transport.go
@@ -5,6 +5,10 @@ import (
 )
 
 // NewHTTP3Transport creates a new HTTP3Transport instance.
+//
+// Deprecation warning
+//
+// New code should use netxlite.NewHTTP3Transport instead.
 func NewHTTP3Transport(config Config) RoundTripper {
 	return netxlite.NewHTTP3Transport(config.QUICDialer, config.TLSConfig)
 }
