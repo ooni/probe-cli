61ad698f6a3a11554f794eedcd13543101aa2ae9
Simone Basso <bassosimone@gmail.com>
2021-07-01 21:51:00 +0200

    Update internal/engine/netx/quicdialer/connectionstate.go

--- internal/engine/netx/quicdialer/connectionstate.go
+++ internal/engine/netx/quicdialer/connectionstate.go
@@ -6,7 +6,7 @@ import (
 	"github.com/lucas-clemente/quic-go"
 )
 
-// connectionState returns the connectionState of a QUIC Session.
+// connectionState returns the ConnectionState of a QUIC Session.
 func connectionState(sess quic.EarlySession) tls.ConnectionState {
 	return sess.ConnectionState().TLS.ConnectionState
 }
