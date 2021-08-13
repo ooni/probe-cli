ed3b1cd80bb5135ee9019f931131757e7d1bba62
kelmenhorst <kelmenhorst@uni-osnabrueck.de>
2021-08-13 12:13:58 +0200

    (review) tls.go: added http/1.1 in NextProtos

--- internal/cmd/oohelperd/internal/nwcth/tls.go
+++ internal/cmd/oohelperd/internal/nwcth/tls.go
@@ -8,7 +8,7 @@ import (
 func TLSDo(conn net.Conn, hostname string) (*tls.Conn, error) {
 	tlsConn := tls.Client(conn, &tls.Config{
 		ServerName: hostname,
-		NextProtos: []string{"h2"},
+		NextProtos: []string{"h2", "http/1.1"},
 	})
 	err := tlsConn.Handshake()
 	return tlsConn, err
