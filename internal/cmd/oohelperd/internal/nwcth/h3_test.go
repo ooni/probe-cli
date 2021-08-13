52533bc1b3aa3323ae7c6b51cc7e951fce63f68e
kelmenhorst <kelmenhorst@uni-osnabrueck.de>
2021-08-13 17:56:41 +0200

    added h3.go tests

---
+++ internal/cmd/oohelperd/internal/nwcth/h3_test.go
@@ -0,0 +1,39 @@
+package nwcth
+
+import (
+	"net/http"
+	"net/url"
+	"testing"
+
+	"github.com/ooni/probe-cli/v3/internal/runtimex"
+)
+
+func TestGetH3URLInvalidURL(t *testing.T) {
+	resp := &http.Response{
+		Request: &http.Request{},
+	}
+	h3URL, err := getH3URL(resp)
+	if err == nil {
+		t.Fatal("expected an error here")
+	}
+	if h3URL != nil {
+		t.Fatal("h3URL should be nil")
+	}
+
+}
+
+func TestParseUnsupportedScheme(t *testing.T) {
+	URL, err := url.Parse("h3://google.com")
+	runtimex.PanicOnError(err, "url.Parse failed")
+	parsed, err := parseAltSvc(nil, URL)
+	if err == nil {
+		t.Fatal("expected an error here")
+	}
+	if err != ErrUnsupportedScheme {
+		t.Fatal("unexpected error type")
+	}
+	if parsed != nil {
+		t.Fatal("h3URL should be nil")
+	}
+
+}
