0259579e760d3d4afdcd733093a8383d61da04bf
kelmenhorst <kelmenhorst@uni-osnabrueck.de>
2021-08-16 15:10:27 +0200

    removed unused function

--- internal/cmd/oohelperd/internal/nwcth/generate.go
+++ internal/cmd/oohelperd/internal/nwcth/generate.go
@@ -3,7 +3,6 @@ package nwcth
 import (
 	"context"
 	"crypto/tls"
-	"io"
 	"net"
 	"net/http"
 
@@ -270,33 +269,6 @@ func (g *DefaultGenerator) GenerateH3Endpoint(ctx context.Context, rt *RoundTrip
 		StatusCode: int64(resp.StatusCode),
 	}
 	return currentEndpoint
-}
-
-// HTTPDo performs the HTTP check.
-// Input:
-// req *http.Request
-//      The same request than the one used by the Explore step.
-//      This means that req contains the headers set by the original CtrlRequest, as well as,
-//      in case of a redirect chain, additional headers that were added due to redirects
-// transport http.RoundTripper:
-//      The transport to use, either http.Transport, or http3.RoundTripper.
-func (g *DefaultGenerator) HTTPDo(req *http.Request, transport http.RoundTripper) (*http.Response, []byte, error) {
-	clnt := http.Client{
-		CheckRedirect: func(r *http.Request, reqs []*http.Request) error {
-			return http.ErrUseLastResponse
-		},
-		Transport: transport,
-	}
-	resp, err := clnt.Do(req)
-	if err != nil {
-		return nil, nil, err
-	}
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return resp, nil, nil
-	}
-	return resp, body, nil
 }
 
 // mergeAddresses creates a (duplicate-free) union set of the IP addresses provided by the client,
