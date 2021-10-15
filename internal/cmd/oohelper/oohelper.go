855e85d52884c7dc0538deebe7dcec3ebd9d2661
Simone Basso <bassosimone@gmail.com>
2021-10-15 16:05:21 +0200

    fix: increment timeout for the TH

--- internal/cmd/oohelper/oohelper.go
+++ internal/cmd/oohelper/oohelper.go
@@ -19,7 +19,7 @@ import (
 )
 
 var (
-	ctx, cancel = context.WithTimeout(context.Background(), 45*time.Second)
+	ctx, cancel = context.WithTimeout(context.Background(), 90*time.Second)
 	debug       = flag.Bool("debug", false, "Toggle debug mode")
 	httpClient  *http.Client
 	resolver    netx.Resolver
