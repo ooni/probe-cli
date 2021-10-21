b0f1dc59b8e2ab3134ba9badb92c5d3395f48df4
Simone Basso <bassosimone@gmail.com>
2021-10-21 12:09:26 +0200

    this should fix invocation

--- E2E/debian.sh
+++ E2E/debian.sh
@@ -8,7 +8,7 @@ set -e
 install_flow() {
 	set -x
 	export DEBIAN_FRONTEND=noninteractive
-	dpkg --add-architecture
+	dpkg --add-architecture "$1"
 	apt-get update
 	apt-get install --yes gnupg
 	apt-key adv --verbose --keyserver hkp://keyserver.ubuntu.com --recv-keys 'B5A08F01796E7F521861B449372D1FF271F2DD50'
@@ -25,7 +25,7 @@ docker_flow() {
 	}
 	set -x
 	docker pull debian:stable
-	docker run -v "$(pwd):/ooni" -w /ooni debian:stable ./E2E/debian.sh install $1
+	docker run -v "$(pwd):/ooni" -w /ooni debian:stable ./E2E/debian.sh install "$1"
 }
 
 if [ "$1" = "docker" ]; then
@@ -40,6 +40,6 @@ elif [ "$1" = "install" ]; then
 
 else
 	echo "usage: $0 docker {i386,amd64,armhf,arm64}" 1>&2
-	echo "       $0 install" 1>&2
+	echo "       $0 install {i386,amd64,armhf,arm64}" 1>&2
 	exit 1
 fi
