1eaacd5c6c66ab3323b7a3078ba8f2c7f8635434
Simone Basso <bassosimone@gmail.com>
2021-11-03 13:47:54 +0100

    x

--- QA/index.mjs
+++ QA/index.mjs
@@ -9,7 +9,7 @@ import path from "path"
 // for hijacking popular DNS servers with `miniooni --censor`.
 //
 // This function is an helper function for populating test cases.
-const hijackPopularDNSServers = () => {
+function hijackPopularDNSServers() {
     return {
         // cloudflare
         "1.1.1.1:53/udp": "hijack-dns",
@@ -27,7 +27,7 @@ const hijackPopularDNSServers = () => {
 // set in the output report file.
 //
 // This function helps to implement per-experiment checkers.
-const checkExperimentName = (name, report) => {
+function checkExperimentName(name, report) {
     const result = report.test_name === name
     console.log(`checking whether the experiment name is correct... ${result}`)
     return result
@@ -37,7 +37,7 @@ const checkExperimentName = (name, report) => {
 // to validate the top-level test keys of a result.
 //
 // This function helps to implement per-experiment checkers.
-const checkTopLevelKeys = (name, report) => {
+function checkTopLevelKeys(name, report) {
     let result = true
     result = result && checkExperimentName(name, report)
     return result
@@ -59,7 +59,7 @@ const checkTopLevelKeys = (name, report) => {
 // - experiments (object): names of the experiments to run
 // mapping to the function to use to verify the results.
 const testCases = [{
-    name: "0000_web_dns_nxdomain",
+    name: "web_dns_nxdomain",
     description: "nxdomain for the domain inside the URL",
     input: "https://nexa.polito.it/",
     blocking: {
@@ -79,7 +79,7 @@ const testCases = [{
 },
 
 {
-    name: "0001_web_dns_refused",
+    name: "web_dns_refused",
     description: "refused for the domain inside the URL",
     input: "https://nexa.polito.it/",
     blocking: {
@@ -99,8 +99,8 @@ const testCases = [{
 },
 
 {
-    name: "0002_web_dns_bogon",
-    description: "bogon reply for the domain inside the URL",
+    name: "web_dns_localhost",
+    description: "localhost reply for the domain inside the URL",
     input: "https://nexa.polito.it/",
     blocking: {
         Domains: {
@@ -119,7 +119,7 @@ const testCases = [{
 },
 
 {
-    name: "0003_web_dns_no_answer",
+    name: "web_dns_no_answer",
     description: "no answer for the domain inside the URL",
     input: "https://nexa.polito.it/",
     blocking: {
@@ -139,7 +139,7 @@ const testCases = [{
 },
 
 {
-    name: "0004_web_dns_timeout",
+    name: "web_dns_timeout",
     description: "timeout when resolving the domain inside the URL",
     input: "https://nexa.polito.it/",
     blocking: {
@@ -160,7 +160,7 @@ const testCases = [{
 },
 
 {
-    name: "0005_web_tcp_connect_timeout",
+    name: "web_tcp_connect_timeout",
     description: "timeout when connecting to the IP address",
     input: "https://nexa.polito.it/",
     blocking: {
@@ -179,12 +179,12 @@ const testCases = [{
 },
 
 {
-    name: "0006_web_tcp_connection_refused",
+    name: "web_tcp_connection_refused",
     description: "connection refused when connecting to the IP address",
     input: "https://nexa.polito.it/",
     blocking: {
         Endpoints: {
-            "130.192.16.171:443/tcp": "tcp-reject",
+            "130.192.16.171:443/tcp": "tcp-reject-syn",
         },
     },
     experiments: {
@@ -198,7 +198,7 @@ const testCases = [{
 },
 
 {
-    name: "0007_web_tls_handshake_timeout",
+    name: "web_tls_handshake_timeout",
     description: "timeout when performing the TLS handshake",
     input: "https://nexa.polito.it/",
     blocking: {
@@ -217,7 +217,7 @@ const testCases = [{
 },
 
 {
-    name: "0008_web_tls_handshake_reset",
+    name: "web_tls_handshake_reset",
     description: "reset when performing the TLS handshake",
     input: "https://nexa.polito.it/",
     blocking: {
@@ -239,13 +239,16 @@ const testCases = [{
 },
 
 {
-    name: "0009_web_tls_handshake_validation",
+    name: "web_tls_handshake_validation",
     description: "validation issues when performing the TLS handshake",
     input: "https://nexa.polito.it/",
     blocking: {
         Endpoints: {
-            "130.192.16.171:443/tcp": "hijack-tls-mitm",
+            "130.192.16.171:443/tcp": "divert",
         },
+        Divert: {
+            "130.192.16.171:443/tcp": "8.8.4.4:443/tcp",
+        }
     },
     experiments: {
         websteps: (name, report) => {
@@ -259,7 +262,7 @@ const testCases = [{
 },
 
 {
-    name: "0010_web_quic_handshake_timeout",
+    name: "web_quic_handshake_timeout",
     description: "timeout when performing the QUIC handshake",
     input: "https://dns.google/",
     blocking: {
@@ -278,16 +281,22 @@ const testCases = [{
 },
 
 {
-    name: "0011_web_quic_handshake_validation",
+    name: "web_quic_handshake_validation",
     description: "validation error when performing the QUIC handshake",
     input: "https://dns.google/",
     blocking: {
         Endpoints: {
-            "8.8.8.8:443/udp": "hijack-quic-mitm",
-            "8.8.4.4:443/udp": "hijack-quic-mitm",
-            "[2001:4860:4860::8888]:443/udp": "hijack-quic-mitm",
-            "[2001:4860:4860::8844]:443/udp": "hijack-quic-mitm",
+            "8.8.8.8:443/udp": "divert",
+            "8.8.4.4:443/udp": "divert",
+            "[2001:4860:4860::8888]:443/udp": "divert",
+            "[2001:4860:4860::8844]:443/udp": "divert",
         },
+        Divert: {
+            "8.8.8.8:443/udp": "1.1.1.1:443/udp",
+            "8.8.4.4:443/udp": "1.1.1.1:443/udp",
+            "[2001:4860:4860::8888]:443/udp": "1.1.1.1:443/udp",
+            "[2001:4860:4860::8844]:443/udp": "1.1.1.1:443/udp",
+        }
     },
     experiments: {
         websteps: (name, report) => {
@@ -297,7 +306,7 @@ const testCases = [{
 },
 
 {
-    name: "0012_web_http_reset",
+    name: "web_http_reset",
     description: "reset when performing the HTTP round trip",
     input: "http://nexa.polito.it/",
     blocking: {
@@ -319,7 +328,7 @@ const testCases = [{
 },
 
 {
-    name: "0013_web_http_timeout",
+    name: "web_http_timeout",
     description: "timeout when performing the HTTP round trip",
     input: "http://nexa.polito.it/",
     blocking: {
@@ -341,7 +350,7 @@ const testCases = [{
 },
 
 {
-    name: "0014_web_http_451",
+    name: "web_http_451",
     description: "451 when performing the HTTP round trip",
     input: "http://nexa.polito.it/",
     blocking: {
@@ -379,19 +388,19 @@ const testCases = [{
 //
 // See https://stackoverflow.com/questions/7055061 for an insightful
 // discussion on how one should or should not create temp files.
-const tempFile = (suffix) => {
-    return path.join(`tmp-${crypto.randomBytes(16).toString('hex')}.${suffix}`);
+function tempFile(suffix) {
+    return path.join(`tmp-${crypto.randomBytes(16).toString('hex')}.${suffix}`)
 }
 
 // exec executes a command. This function throws on failure.
-const exec = (command) => {
+function exec(command) {
     console.log(`+ ${command}`)
     child_process.execSync(command)
 }
 
 // writeCensorJsonFile writes a censor.json file using a file name
 // containing random characters and returns the file name.
-const writeCensorJsonFile = (testCase) => {
+function writeCensorJsonFile(testCase) {
     const fileName = tempFile("json")
     fs.writeFileSync(fileName, JSON.stringify(testCase.blocking))
     return fileName
@@ -399,13 +408,13 @@ const writeCensorJsonFile = (testCase) => {
 
 // readReportFile reads and parses the report file, thus returning
 // the JSON object contained inside the report file.
-const readReportFile = (reportFile) => {
+function readReportFile(reportFile) {
     const data = fs.readFileSync(reportFile, { "encoding": "utf-8" })
     return JSON.parse(data)
 }
 
 // runExperiment runs the given test case with the given experiment.
-const runExperiment = (testCase, experiment, checker) => {
+function runExperiment(testCase, experiment, checker) {
     console.log(`## running: ${testCase.name}.${experiment}`)
     console.log("")
     const censorJson = writeCensorJsonFile(testCase)
@@ -427,7 +436,7 @@ const runExperiment = (testCase, experiment, checker) => {
 }
 
 // runTestCase runs the given test case.
-const runTestCase = (testCase) => {
+function runTestCase(testCase) {
     console.log("")
     console.log(`# running: ${testCase.name}`)
     let result = true
@@ -438,7 +447,7 @@ const runTestCase = (testCase) => {
 }
 
 // checkForDuplicateTestCases ensures there are no duplicate names.
-const checkForDuplicateTestCases = () => {
+function checkForDuplicateTestCases() {
     let dups = {}
     for (let i = 0; i < testCases.length; i++) {
         const testCase = testCases[i]
@@ -451,7 +460,7 @@ const checkForDuplicateTestCases = () => {
 }
 
 // runAllTestCases runs all the available test cases.
-const runAllTestCases = () => {
+function runAllTestCases() {
     let result = true
     for (let i = 0; i < testCases.length; i++) {
         result = result && runTestCase(testCases[i])
@@ -461,7 +470,7 @@ const runAllTestCases = () => {
 
 // makeTestCasesMap creates a map from the test case name
 // to the test case definition.
-const makeTestCasesMap = () => {
+function makeTestCasesMap() {
     var map = {}
     for (let i = 0; i < testCases.length; i++) {
         const testCase = testCases[i]
@@ -471,12 +480,12 @@ const makeTestCasesMap = () => {
 }
 
 // recompileMiniooni recompiles miniooni if needed.
-const recompileMiniooni = () => {
+function recompileMiniooni() {
     exec("go build -v ./internal/cmd/miniooni")
 }
 
 // commandRun implements the run command.
-const commandRun = (args) => {
+function commandRun(args) {
     recompileMiniooni()
     const bailOnFailure = (result) => {
         if (!result) {
@@ -503,7 +512,7 @@ const commandRun = (args) => {
 }
 
 // commandList implements the list command.
-const commandList = () => {
+function commandList() {
     for (let i = 0; i < testCases.length; i++) {
         const testCase = testCases[i]
         console.log(`${testCase.name}:`)
@@ -512,7 +521,7 @@ const commandList = () => {
 }
 
 // main is the main function.
-const main = () => {
+function main() {
     const usageAndExit = (exitcode) => {
         console.log("usage: node ./QA/web.mjs list")
         console.log("usage: node ./QA/web.mjs run [test_case_name...]")
