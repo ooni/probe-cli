dad8db4a369d8f0846397011812b9bd8e40d04e5
Simone Basso <bassosimone@gmail.com>
2021-11-05 11:31:10 +0100

    fix(httpheader): use same Accept-Language as Brave (#574)
    Closes https://github.com/ooni/probe/issues/1792

--- internal/engine/httpheader/acceptlanguage.go
+++ internal/engine/httpheader/acceptlanguage.go
@@ -2,5 +2,5 @@ package httpheader
 
 // AcceptLanguage returns the Accept-Language header used for measuring.
 func AcceptLanguage() string {
-	return "en-US;q=0.8,en;q=0.5"
+	return "en-US,en;q=0.9"
 }
