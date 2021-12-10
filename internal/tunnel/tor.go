cafba7994b5df5a56045640db187656795dfea7c
Simone Basso <bassosimone@gmail.com>
2021-12-10 11:49:46 +0100

    I am the enemy of blank lines inside functions XD

--- internal/tunnel/tor.go
+++ internal/tunnel/tor.go
@@ -72,13 +72,11 @@ func torStart(ctx context.Context, config *Config) (Tunnel, error) {
 	extraArgs = append(extraArgs, "notice stderr")
 	extraArgs = append(extraArgs, "Log")
 	extraArgs = append(extraArgs, fmt.Sprintf(`notice file %s`, logfile))
-
 	config.logger().Infof("tunnel: tor: exec params: %s %+v", stateDir, extraArgs)
 	torStartConf, err := getTorStartConf(config, stateDir, extraArgs)
 	if err != nil {
 		return nil, err
 	}
-
 	instance, err := config.torStart(ctx, torStartConf)
 	if err != nil {
 		return nil, err
