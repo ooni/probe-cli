fad24acf7cbb62cd521540be7cecb703d5152b9f
Simone Basso <bassosimone@gmail.com>
2022-01-04 13:28:41 +0100

    cleanup(httpx): remove unused FetchResourceAndVerify

--- internal/engine/httpx/fetch.go
+++ internal/engine/httpx/fetch.go
@@ -2,8 +2,6 @@ package httpx
 
 import (
 	"context"
-	"crypto/sha256"
-	"fmt"
 )
 
 // FetchResource fetches the specified resource and returns it.
@@ -13,19 +11,4 @@ func (c Client) FetchResource(ctx context.Context, URLPath string) ([]byte, erro
 		return nil, err
 	}
 	return c.Do(request)
-}
-
-// FetchResourceAndVerify fetches and verifies a specific resource.
-func (c Client) FetchResourceAndVerify(ctx context.Context, URL, SHA256Sum string) ([]byte, error) {
-	c.Logger.Debugf("httpx: expected SHA256: %s", SHA256Sum)
-	data, err := c.FetchResource(ctx, URL)
-	if err != nil {
-		return nil, err
-	}
-	s := fmt.Sprintf("%x", sha256.Sum256(data))
-	c.Logger.Debugf("httpx: real SHA256: %s", s)
-	if SHA256Sum != s {
-		return nil, fmt.Errorf("httpx: SHA256 mismatch: got %s and expected %s", s, SHA256Sum)
-	}
-	return data, nil
 }
--- internal/engine/httpx/fetch_test.go
+++ internal/engine/httpx/fetch_test.go
@@ -47,27 +47,6 @@ func TestFetchResourceExpiredContext(t *testing.T) {
 	}
 }
 
-func TestFetchResourceAndVerifyIntegration(t *testing.T) {
-	log.SetLevel(log.DebugLevel)
-	ctx := context.Background()
-	data, err := (httpx.Client{
-		BaseURL:    "https://github.com/",
-		HTTPClient: http.DefaultClient,
-		Logger:     log.Log,
-		UserAgent:  "ooniprobe-engine/0.1.0",
-	}).FetchResourceAndVerify(
-		ctx,
-		"/measurement-kit/generic-assets/releases/download/20190426155936/generic-assets-20190426155936.tar.gz",
-		"34d8a9c8ab30c242469482dc280be832d8a06b4400f8927604dd361bf979b795",
-	)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(data) <= 0 {
-		t.Fatal("Did not expect an empty resource")
-	}
-}
-
 func TestFetchResourceInvalidURL(t *testing.T) {
 	log.SetLevel(log.DebugLevel)
 	ctx := context.Background()
@@ -102,50 +81,6 @@ func TestFetchResource400(t *testing.T) {
 		UserAgent:     "ooniprobe-engine/0.1.0",
 	}).FetchResource(ctx, "")
 	if err == nil || !strings.HasSuffix(err.Error(), "400 Bad Request") {
-		t.Fatal("not the error we expected")
-	}
-	if len(data) != 0 {
-		t.Fatal("expected an empty resource")
-	}
-}
-
-func TestFetchResourceAndVerify400(t *testing.T) {
-	server := httptest.NewServer(http.HandlerFunc(
-		func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(400)
-		},
-	))
-	defer server.Close()
-	log.SetLevel(log.DebugLevel)
-	ctx := context.Background()
-	data, err := (httpx.Client{
-		BaseURL:    server.URL,
-		HTTPClient: http.DefaultClient,
-		Logger:     log.Log,
-		UserAgent:  "ooniprobe-engine/0.1.0",
-	}).FetchResourceAndVerify(ctx, "", "abcde")
-	if err == nil || !strings.HasSuffix(err.Error(), "400 Bad Request") {
-		t.Fatal("not the error we expected")
-	}
-	if len(data) != 0 {
-		t.Fatal("expected an empty resource")
-	}
-}
-
-func TestFetchResourceAndVerifyInvalidSHA256(t *testing.T) {
-	log.SetLevel(log.DebugLevel)
-	ctx := context.Background()
-	data, err := (httpx.Client{
-		BaseURL:    "https://github.com/",
-		HTTPClient: http.DefaultClient,
-		Logger:     log.Log,
-		UserAgent:  "ooniprobe-engine/0.1.0",
-	}).FetchResourceAndVerify(
-		ctx,
-		"/measurement-kit/generic-assets/releases/download/20190426155936/generic-assets-20190426155936.tar.gz",
-		"34d8a9ceeb30c242469482dc280be832d8a06b4400f8927604dd361bf979b795",
-	)
-	if err == nil || !strings.HasPrefix(err.Error(), "httpx: SHA256 mismatch:") {
 		t.Fatal("not the error we expected")
 	}
 	if len(data) != 0 {
