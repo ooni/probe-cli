8f71b7d33a52c8106cad60c250b0f0f66bfa9787
Simone Basso <bassosimone@gmail.com>
2022-01-04 12:40:02 +0100

    cleanup(ptx): remove redundant dialer definition
    We can just use model.SimpleDialer here.
    
    See https://github.com/ooni/probe/issues/1885.

--- internal/ptx/dependencies.go
+++ internal/ptx/dependencies.go
@@ -1,17 +1,8 @@
 package ptx
 
 import (
-	"context"
-	"net"
-
 	"github.com/ooni/probe-cli/v3/internal/model"
 )
-
-// UnderlyingDialer is the underlying dialer used for dialing.
-type UnderlyingDialer interface {
-	// DialContext behaves like net.Dialer.DialContext.
-	DialContext(ctx context.Context, network, address string) (net.Conn, error)
-}
 
 // defaultLogger is the default silentLogger instance.
 var defaultLogger model.Logger = model.DiscardLogger
