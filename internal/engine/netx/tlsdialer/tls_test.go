d6227374f7454c6a2434a8bd3541d827aa100dc7
Simone Basso <bassosimone@gmail.com>
2022-01-05 18:45:25 +0100

    cleanup: remove the original netx implementation
    This commit completely removes the original netx implementation,
    which was only used by `tor`, since this has changed in
    https://github.com/ooni/probe-cli/pull/652.
    
    The original netx implementation was my first attempt at performing
    network measurements using Go. It started its life inside of the
    https://github.com/ooni/netx repository. It was later merged into
    the https://github.com/ooni/probe-engine repository. It finally
    ended up into this repository when we merged probe-engine with it.
    
    The main issue with the original implementation is that it was
    a bit too complex and used channels where they were probably not
    necessary. Because of that, later I introduced a second netx
    implementation, which currently lives in ./internal/engine/netx.
    
    The current netx implementation, the third one, lives in the
    ./internal/netxlite package. We are currently working to replace
    the second implementation with the third one, but this is happening
    at a slow pace. Also, the second implementation does not have big
    maintenance concerns but it's just a bit too bureaucratic to use
    since it involves creating lots of `Config` structures.
    
    The reference issue is probably https://github.com/ooni/probe/issues/1688,
    since this diff has been enabled by rewriting Tor to use `measurex`
    (a library living on top of `netxlite`).

--- internal/engine/netx/tlsdialer/tls_test.go
+++ internal/engine/netx/tlsdialer/tls_test.go
@@ -3,13 +3,10 @@ package tlsdialer_test
 import (
 	"context"
 	"crypto/tls"
-	"errors"
 	"io"
 	"testing"
 	"time"
 
-	"github.com/ooni/probe-cli/v3/internal/engine/legacy/netx/handlers"
-	"github.com/ooni/probe-cli/v3/internal/engine/legacy/netx/modelx"
 	"github.com/ooni/probe-cli/v3/internal/engine/netx/tlsdialer"
 	"github.com/ooni/probe-cli/v3/internal/netxlite"
 )
@@ -35,41 +32,4 @@ type SetDeadlineConn struct {
 func (c *SetDeadlineConn) SetDeadline(t time.Time) error {
 	c.deadlines = append(c.deadlines, t)
 	return nil
-}
-
-func TestEmitterTLSHandshakerFailure(t *testing.T) {
-	saver := &handlers.SavingHandler{}
-	ctx := modelx.WithMeasurementRoot(context.Background(), &modelx.MeasurementRoot{
-		Beginning: time.Now(),
-		Handler:   saver,
-	})
-	h := tlsdialer.EmitterTLSHandshaker{TLSHandshaker: tlsdialer.EOFTLSHandshaker{}}
-	conn, _, err := h.Handshake(ctx, tlsdialer.EOFConn{}, &tls.Config{
-		ServerName: "www.kernel.org",
-	})
-	if !errors.Is(err, io.EOF) {
-		t.Fatal("not the error that we expected")
-	}
-	if conn != nil {
-		t.Fatal("expected nil con here")
-	}
-	events := saver.Read()
-	if len(events) != 2 {
-		t.Fatal("Wrong number of events")
-	}
-	if events[0].TLSHandshakeStart == nil {
-		t.Fatal("missing TLSHandshakeStart event")
-	}
-	if events[0].TLSHandshakeStart.DurationSinceBeginning == 0 {
-		t.Fatal("expected nonzero DurationSinceBeginning")
-	}
-	if events[0].TLSHandshakeStart.SNI != "www.kernel.org" {
-		t.Fatal("expected nonzero SNI")
-	}
-	if events[1].TLSHandshakeDone == nil {
-		t.Fatal("missing TLSHandshakeDone event")
-	}
-	if events[1].TLSHandshakeDone.DurationSinceBeginning == 0 {
-		t.Fatal("expected nonzero DurationSinceBeginning")
-	}
 }
