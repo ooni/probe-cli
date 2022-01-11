ecc7e28e9cfc3bd6f8df5394b557cd4aed7df615
Simone Basso <bassosimone@gmail.com>
2022-01-11 16:08:57 +0100

    feat(archival): more unit tests

--- internal/archival/saver_test.go
+++ internal/archival/saver_test.go
@@ -1,13 +1,37 @@
 package archival
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/ooni/probe-cli/v3/internal/fakefill"
+)
 
 func TestSaverNewSaver(t *testing.T) {
-	t.Run("on success", func(t *testing.T) {})
-	t.Run("on failure", func(t *testing.T) {})
+	saver := NewSaver()
+	if saver.trace == nil {
+		t.Fatal("expected non-nil trace here")
+	}
 }
 
 func TestSaverMoveOutTrace(t *testing.T) {
-	t.Run("on success", func(t *testing.T) {})
-	t.Run("on failure", func(t *testing.T) {})
+	saver := NewSaver()
+	var ev DNSRoundTripEvent
+	ff := &fakefill.Filler{}
+	ff.Fill(&ev)
+	if len(ev.Query) < 1 {
+		t.Fatal("did not fill") // be sure
+	}
+	saver.appendDNSRoundTripEvent(&ev)
+	trace := saver.MoveOutTrace()
+	if len(saver.trace.DNSRoundTrip) != 0 {
+		t.Fatal("expected zero length")
+	}
+	if len(trace.DNSRoundTrip) != 1 {
+		t.Fatal("expected one entry")
+	}
+	entry := trace.DNSRoundTrip[0]
+	if diff := cmp.Diff(&ev, entry); diff != "" {
+		t.Fatal(diff)
+	}
 }
