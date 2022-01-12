8d5977511a7b3ef5abb33abe9394d89dbe067694
Simone Basso <bassosimone@gmail.com>
2022-01-12 14:17:09 +0100

    doc(netxlite): document we know how to deal w/ wrapped io.EOF

--- internal/netxlite/iox.go
+++ internal/netxlite/iox.go
@@ -14,6 +14,10 @@ import (
 // the long-running goroutine, close the connection
 // bound to the reader. Until such a connection is closed,
 // you're leaking the backround goroutine and doing I/O.
+//
+// As of Go 1.17.6, ReadAllContext additionally deals
+// with wrapped io.EOF correctly, while io.ReadAll does
+// not. See https://github.com/ooni/probe/issues/1965.
 func ReadAllContext(ctx context.Context, r io.Reader) ([]byte, error) {
 	datach, errch := make(chan []byte, 1), make(chan error, 1) // buffers
 	go func() {
@@ -42,6 +46,10 @@ func ReadAllContext(ctx context.Context, r io.Reader) ([]byte, error) {
 // when the context expires. This function has the same
 // caveats of ReadAllContext regarding the temporary leaking
 // of the background I/O goroutine.
+//
+// As of Go 1.17.6, CopyContext additionally deals
+// with wrapped io.EOF correctly, while io.Copy does
+// not. See https://github.com/ooni/probe/issues/1965.
 func CopyContext(ctx context.Context, dst io.Writer, src io.Reader) (int64, error) {
 	countch, errch := make(chan int64, 1), make(chan error, 1) // buffers
 	go func() {
