f687f29e2f987b6100e4c7030be38f13ba16acb8
Simone Basso <bassosimone@gmail.com>
2022-01-12 13:20:51 +0100

    chore(netxlite/iox_test.go): add failing unit tests
    These tests directly show how the Go implementation of ReadAll
    and Copy has the issue of checking for io.EOF equality.

--- internal/netxlite/iox_test.go
+++ internal/netxlite/iox_test.go
@@ -24,6 +24,41 @@ func TestReadAllContext(t *testing.T) {
 		}
 	})
 
+	t.Run("with success and wrapped io.EOF", func(t *testing.T) {
+		// See https://github.com/ooni/probe/issues/1965
+		wg := &sync.WaitGroup{}
+		wg.Add(1)
+		r := &mocks.Reader{
+			MockRead: func(b []byte) (int, error) {
+				defer wg.Done()
+				// "When Read encounters an error or end-of-file condition
+				// after successfully reading n > 0 bytes, it returns
+				// the number of bytes read. It may return the (non-nil)
+				// error from the same call or return the error (and n == 0)
+				// from a subsequent call.""
+				//
+				// See https://pkg.go.dev/io#Reader
+				//
+				// Note: Returning a wrapped error to ensure we address
+				// https://github.com/ooni/probe/issues/1965
+				return len(b), NewErrWrapper(classifyGenericError,
+					ReadOperation, io.EOF)
+			},
+		}
+		out, err := ReadAllContext(context.Background(), r)
+		if err != nil {
+			t.Fatal(err)
+		}
+		var errWrapper *ErrWrapper
+		if !errors.As(err, &errWrapper) {
+			t.Fatal("the returned error is not wrapped")
+		}
+		if len(out) <= 0 {
+			t.Fatal("we expected to see a positive number of bytes here")
+		}
+		wg.Wait()
+	})
+
 	t.Run("with failure and background context", func(t *testing.T) {
 		expected := errors.New("mocked error")
 		r := &mocks.Reader{
@@ -121,6 +156,41 @@ func TestCopyContext(t *testing.T) {
 		if out != 8 {
 			t.Fatal("not the expected number of bytes")
 		}
+	})
+
+	t.Run("with success and wrapped io.EOF", func(t *testing.T) {
+		// See https://github.com/ooni/probe/issues/1965
+		wg := &sync.WaitGroup{}
+		wg.Add(1)
+		r := &mocks.Reader{
+			MockRead: func(b []byte) (int, error) {
+				defer wg.Done()
+				// "When Read encounters an error or end-of-file condition
+				// after successfully reading n > 0 bytes, it returns
+				// the number of bytes read. It may return the (non-nil)
+				// error from the same call or return the error (and n == 0)
+				// from a subsequent call.""
+				//
+				// See https://pkg.go.dev/io#Reader
+				//
+				// Note: Returning a wrapped error to ensure we address
+				// https://github.com/ooni/probe/issues/1965
+				return len(b), NewErrWrapper(classifyGenericError,
+					ReadOperation, io.EOF)
+			},
+		}
+		out, err := CopyContext(context.Background(), io.Discard, r)
+		if err != nil {
+			t.Fatal(err)
+		}
+		var errWrapper *ErrWrapper
+		if !errors.As(err, &errWrapper) {
+			t.Fatal("the returned error is not wrapped")
+		}
+		if out <= 0 {
+			t.Fatal("we expected to see a positive number of bytes here")
+		}
+		wg.Wait()
 	})
 
 	t.Run("with failure and background context", func(t *testing.T) {
