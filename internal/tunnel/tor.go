4d50dd6d54e36e677081c66044ef5859a0c5063e
Simone Basso <bassosimone@gmail.com>
2022-01-25 20:43:27 +0100

    fix(i/t/tor.go): show correct command line (#673)
    While there, ensure that we print a warning if we cannot find
    the correct tor binary.
    
    Work part of https://github.com/ooni/probe/issues/1917.

--- internal/tunnel/tor.go
+++ internal/tunnel/tor.go
@@ -72,7 +72,6 @@ func torStart(ctx context.Context, config *Config) (Tunnel, error) {
 	extraArgs = append(extraArgs, "notice stderr")
 	extraArgs = append(extraArgs, "Log")
 	extraArgs = append(extraArgs, fmt.Sprintf(`notice file %s`, logfile))
-	config.logger().Infof("tunnel: tor: exec params: %s %+v", stateDir, extraArgs)
 	torStartConf, err := getTorStartConf(config, stateDir, extraArgs)
 	if err != nil {
 		return nil, err
--- internal/tunnel/tordesktop.go
+++ internal/tunnel/tordesktop.go
@@ -4,16 +4,22 @@ package tunnel
 
 // This file implements our strategy for running tor on desktop.
 
-import "github.com/cretz/bine/tor"
+import (
+	"strings"
+
+	"github.com/cretz/bine/tor"
+)
 
 // getTorStartConf in this configuration uses torExePath to get a
 // suitable tor binary and then executes it.
 func getTorStartConf(config *Config, dataDir string, extraArgs []string) (*tor.StartConf, error) {
 	exePath, err := config.torBinary()
 	if err != nil {
+		config.logger().Warnf("cannot find tor binary: %s", err.Error())
 		return nil, err
 	}
-	config.logger().Infof("tunnel: tor: exec binary: %s", exePath)
+	config.logger().Infof("tunnel: tor: exec: %s %s %s", exePath,
+		dataDir, strings.Join(extraArgs, " "))
 	return &tor.StartConf{
 		ExePath:   exePath,
 		DataDir:   dataDir,
