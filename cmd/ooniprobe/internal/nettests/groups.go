340cd826ffe42b6b34a660aee3370e571436e15c
Yeganathan S <hilariousengineer@protonmail.com>
2022-03-15 20:54:21 +0530

    fix: experimental should be unattended (#704)
    See https://github.com/ooni/probe/issues/2056

--- cmd/ooniprobe/internal/nettests/groups.go
+++ cmd/ooniprobe/internal/nettests/groups.go
@@ -22,6 +22,8 @@ var All = map[string]Group{
 			Dash{},
 			NDT{},
 		},
+		// unattendedOK is explicitly set to false, since there is no need for consumption of excessive amounts of data with background tests
+		UnattendedOK: false,
 	},
 	"middlebox": {
 		Label: "Middleboxes",
@@ -56,5 +58,6 @@ var All = map[string]Group{
 			STUNReachability{},
 			TorSf{},
 		},
+		UnattendedOK: true,
 	},
 }
