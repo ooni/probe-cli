0e254bfc6ba3bfd65365ce3d8de2c8ec51b925ff
Simone Basso <bassosimone@gmail.com>
2022-05-06 11:58:47 +0200

    Revert "cleanup(netxlite): http3dialer can be removed"
    This reverts commit c62244c620cee5fadcc2ca89d8228c8db0b96add
    to investigate the build failure mentioned at
    https://github.com/ooni/probe-cli/pull/715#issuecomment-1119450484

--- internal/netxlite/http3.go
+++ internal/netxlite/http3.go
@@ -1,13 +1,29 @@
 package netxlite
 
 import (
+	"context"
 	"crypto/tls"
 	"io"
 	"net/http"
 
+	"github.com/lucas-clemente/quic-go"
 	"github.com/lucas-clemente/quic-go/http3"
 	"github.com/ooni/probe-cli/v3/internal/model"
 )
+
+// http3Dialer adapts a QUICContextDialer to work with
+// an http3.RoundTripper. This is necessary because the
+// http3.RoundTripper does not support DialContext.
+type http3Dialer struct {
+	model.QUICDialer
+}
+
+// dial is like QUICContextDialer.DialContext
+func (d *http3Dialer) dial(ctx context.Context, network string, address string, tlsConfig *tls.Config,
+	quicConfig *quic.Config) (quic.EarlyConnection, error) {
+	return d.QUICDialer.DialContext(
+		ctx, network, address, tlsConfig, quicConfig)
+}
 
 // http3RoundTripper is the abstract type of quic-go/http3.RoundTripper.
 type http3RoundTripper interface {
@@ -47,7 +63,7 @@ func NewHTTP3Transport(
 	return &httpTransportLogger{
 		HTTPTransport: &http3Transport{
 			child: &http3.RoundTripper{
-				Dial: dialer.DialContext,
+				Dial: (&http3Dialer{dialer}).dial,
 				// The following (1) reduces the number of headers that Go will
 				// automatically send for us and (2) ensures that we always receive
 				// back the true headers, such as Content-Length. This change is
