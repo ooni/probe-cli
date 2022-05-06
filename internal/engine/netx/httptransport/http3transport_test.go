20140cfaa739143e0fcbcaa6fb52c7339e675534
Simone Basso <bassosimone@gmail.com>
2022-05-06 12:01:24 +0200

    chore(netx): show that test was already broken
    We didn't see the breakage before because we were not using
    the created transport, but the issue of using a nil dialer was
    already present before, we just didn't see it.
    
    Now we understand why removing the http3transport in
    c62244c620cee5fadcc2ca89d8228c8db0b96add did cause the
    breakage mentioned at
    https://github.com/ooni/probe-cli/pull/715#issuecomment-1119450484

--- internal/engine/netx/httptransport/http3transport_test.go
+++ internal/engine/netx/httptransport/http3transport_test.go
@@ -1,12 +1,22 @@
 package httptransport_test
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/ooni/probe-cli/v3/internal/engine/netx/httptransport"
 )
 
 func TestNewHTTP3Transport(t *testing.T) {
-	// mainly to cover a line which otherwise won't be directly covered
-	httptransport.NewHTTP3Transport(httptransport.Config{})
+	// make sure we can create a working transport using this factory.
+	txp := httptransport.NewHTTP3Transport(httptransport.Config{})
+	req, err := http.NewRequest("GET", "https://google.com", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp, err := txp.RoundTrip(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp.Body.Close()
 }
