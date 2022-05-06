c62244c620cee5fadcc2ca89d8228c8db0b96add
Simone Basso <bassosimone@gmail.com>
2022-05-06 11:37:40 +0200

    cleanup(netxlite): http3dialer can be removed
    We used to use http3dialer to glue a QUIC dialer, which had a
    context as its first argument, to the Dial function used by the
    HTTP3 transport, which did not have a context as its first
    argument.
    
    Now that HTTP3 transport has a Dial function taking a context as
    its first argument, we don't need http3dialer
    anymore, since we can use the QUIC dialer directly.
    
    Cc: @DecFox

--- internal/netxlite/http3_test.go
+++ internal/netxlite/http3_test.go
@@ -1,38 +1,16 @@
 package netxlite
 
 import (
-	"context"
 	"crypto/tls"
 	"errors"
 	"net/http"
 	"testing"
 
 	"github.com/apex/log"
-	"github.com/lucas-clemente/quic-go"
 	"github.com/lucas-clemente/quic-go/http3"
 	"github.com/ooni/probe-cli/v3/internal/model/mocks"
 	nlmocks "github.com/ooni/probe-cli/v3/internal/netxlite/mocks"
 )
-
-func TestHTTP3Dialer(t *testing.T) {
-	t.Run("Dial", func(t *testing.T) {
-		expected := errors.New("mocked error")
-		d := &http3Dialer{
-			QUICDialer: &mocks.QUICDialer{
-				MockDialContext: func(ctx context.Context, network, address string, tlsConfig *tls.Config, quicConfig *quic.Config) (quic.EarlyConnection, error) {
-					return nil, expected
-				},
-			},
-		}
-		qconn, err := d.dial(context.Background(), "", "", &tls.Config{}, &quic.Config{})
-		if !errors.Is(err, expected) {
-			t.Fatal("unexpected err", err)
-		}
-		if qconn != nil {
-			t.Fatal("unexpected resp")
-		}
-	})
-}
 
 func TestHTTP3Transport(t *testing.T) {
 	t.Run("CloseIdleConnections", func(t *testing.T) {
