4c5510278971b47dbf58b6ed516afcd7c215a50a
ParitoshKabra <76113183+ParitoshKabra@users.noreply.github.com>
2022-05-06 14:06:26 +0530

    fix(torsf): ensure tor-logs-filtering regexp is correct (#707)
    * Fix Regex in TorProgressRegex
    
    * fix: update regexp link
    
    As suggested by @hellais
    
    Co-authored-by: Simone Basso <bassosimone@gmail.com>

--- internal/engine/experiment/torsf/torsf.go
+++ internal/engine/experiment/torsf/torsf.go
@@ -196,9 +196,9 @@ func (m *Measurer) bootstrap(ctx context.Context, sess model.ExperimentSession,
 
 // torProgressRegexp helps to extract progress info from logs.
 //
-// See https://regex101.com/r/3YfIed/1.
+// See https://regex101.com/r/cer3lm/1.
 var torProgressRegexp = regexp.MustCompile(
-	`^[A-Za-z0-9.: ]+ \[notice\] Bootstrapped [0-9]+% \([a-zA-z]+\): [A-Za-z0-9 ]+$`)
+	`^[A-Za-z0-9.: ]+ \[notice\] Bootstrapped [0-9]+% \([A-Za-z_]+\): [A-Za-z0-9 ]+$`)
 
 // readTorLogs attempts to read and include the tor logs into
 // the test keys if this operation is possible.
