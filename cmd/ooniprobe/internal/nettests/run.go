143fd498d502bd95daa38a1030293d74fa05c280
stergem <sgemelas8@gmail.com>
2022-05-18 16:32:38 +0300

    fix(ooniprobe): remove empty measurements directories (#720)
    See https://github.com/ooni/probe/issues/2090
    
    Co-authored-by: Simone Basso <bassosimone@gmail.com>

--- cmd/ooniprobe/internal/nettests/run.go
+++ cmd/ooniprobe/internal/nettests/run.go
@@ -2,6 +2,7 @@ package nettests
 
 import (
 	"context"
+	"os"
 	"sync"
 	"time"
 
@@ -111,6 +112,18 @@ func RunGroup(config RunGroupConfig) error {
 		if err = nt.Run(ctl); err != nil {
 			log.WithError(err).Errorf("Failed to run %s", group.Label)
 		}
+	}
+
+	// Remove the directory if it's emtpy, which happens when the corresponding
+	// measurements have been submitted (see https://github.com/ooni/probe/issues/2090)
+	dir, err := os.Open(result.MeasurementDir)
+	if err != nil {
+		return err
+	}
+	defer dir.Close()
+	_, err = dir.Readdirnames(1)
+	if err != nil {
+		os.Remove(result.MeasurementDir)
 	}
 
 	if err = result.Finished(config.Probe.DB()); err != nil {
