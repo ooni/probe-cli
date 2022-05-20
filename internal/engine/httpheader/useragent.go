0dab8ee053ad13313493254deb1f5e540a9f45ed
Simone Basso <bassosimone@gmail.com>
2022-05-20 11:50:56 +0200

    chore: update the user-agent used for measuring
    See https://github.com/ooni/probe/issues/2100

--- internal/engine/httpheader/useragent.go
+++ internal/engine/httpheader/useragent.go
@@ -3,8 +3,8 @@ package httpheader
 
 // UserAgent returns the User-Agent header used for measuring.
 func UserAgent() string {
-	// 7.6% as of Apr 12, 2022 according to https://techblog.willshouse.com/2012/01/03/most-common-user-agents/
-	const ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0"
+	// 13.7% as of May 20, 2022 according to https://techblog.willshouse.com/2012/01/03/most-common-user-agents/
+	const ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"
 	return ua
 }
 
@@ -12,5 +12,5 @@ func UserAgent() string {
 // pretent to be a command line HTTP client.
 func CLIUserAgent() string {
 	// here we always put the latest version of cURL.
-	return "curl/7.82.0"
+	return "curl/7.83.1"
 }
