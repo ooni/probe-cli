f1b8071c6517560521408fab3b2592ed59cb29e2
Simone Basso <bassosimone@gmail.com>
2022-06-08 15:06:15 +0200

    hotfix(sessionresolver): prevent data race inside http3 (#809)
    See https://github.com/ooni/probe/issues/2135#issuecomment-1149840579

--- internal/engine/internal/sessionresolver/childresolver_test.go
+++ internal/engine/internal/sessionresolver/childresolver_test.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"io"
 	"testing"
-	"time"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/ooni/probe-cli/v3/internal/model/mocks"
@@ -42,26 +41,4 @@ func TestTimeLimitedLookupFailure(t *testing.T) {
 	if out != nil {
 		t.Fatal("expected nil here")
 	}
-}
-
-func TestTimeLimitedLookupWillTimeout(t *testing.T) {
-	done := make(chan bool)
-	block := make(chan bool)
-	re := &mocks.Resolver{
-		MockLookupHost: func(ctx context.Context, domain string) ([]string, error) {
-			defer close(done)
-			<-block
-			return nil, io.EOF
-		},
-	}
-	ctx := context.Background()
-	out, err := timeLimitedLookupWithTimeout(ctx, re, "dns.google", 10*time.Millisecond)
-	if !errors.Is(err, context.DeadlineExceeded) {
-		t.Fatal("not the error we expected", err)
-	}
-	if out != nil {
-		t.Fatal("expected nil here")
-	}
-	close(block)
-	<-done
 }
