d2ec1aa5a8960833abcacd8f1e5070e91c38ce95
Ain Ghazal <ainghazal42@gmail.com>
2022-08-16 20:10:47 +0200

    feat: avoid safe options to be serialized into the measurement
    Skip options that begin with the `Safe` prefix from appearing in the
    serialization of a Measurement that will be submitted to the OONI
    backend.
    
    - Resolves: https://github.com/ooni/probe/issues/2214

--- internal/oonirun/experiment_test.go
+++ internal/oonirun/experiment_test.go
@@ -3,6 +3,8 @@ package oonirun
 import (
 	"context"
 	"os"
+	"reflect"
+	"sort"
 	"testing"
 	"time"
 
@@ -56,5 +58,47 @@ func TestExperimentRunWithExample(t *testing.T) {
 	}
 	if err := desc.Run(ctx); err != nil {
 		t.Fatal(err)
+	}
+}
+
+func Test_experimentOptionsToStringList(t *testing.T) {
+	type args struct {
+		options map[string]any
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantOut []string
+	}{
+		{
+			name: "happy path: a map with three entries returns three items",
+			args: args{
+				map[string]any{
+					"foo":  1,
+					"bar":  2,
+					"baaz": 3,
+				},
+			},
+			wantOut: []string{"baaz=3", "bar=2", "foo=1"},
+		},
+		{
+			name: "an option beginning with `Safe` is skipped from the output",
+			args: args{
+				map[string]any{
+					"foo":     1,
+					"Safefoo": 42,
+				},
+			},
+			wantOut: []string{"foo=1"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotOut := experimentOptionsToStringList(tt.args.options)
+			sort.Strings(gotOut)
+			if !reflect.DeepEqual(gotOut, tt.wantOut) {
+				t.Errorf("experimentOptionsToStringList() = %v, want %v", gotOut, tt.wantOut)
+			}
+		})
 	}
 }
