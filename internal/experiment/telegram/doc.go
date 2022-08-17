23cf1920a0c116c90ccd7ac7cae2c7c7129d6e4e
Simone Basso <bassosimone@gmail.com>
2022-08-17 10:27:11 +0200

    start removing code which shan't go in this PR

--- internal/experiment/telegram/doc.go
+++
@@ -1,4 +0,0 @@
-// Package telegram implements the telegram experiment.
-//
-// Spec: https://github.com/ooni/spec/blob/master/nettests/ts-020-telegram.md.
-package telegram
--- internal/experiment/webconnectivity/doc.go
+++
@@ -1,4 +0,0 @@
-// Package webconnectivity implements the web_connectivity experiment.
-//
-// Spec: https://github.com/ooni/spec/blob/master/nettests/ts-017-web-connectivity.md.
-package webconnectivity
