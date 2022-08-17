64861b51574af3c68c97f81be5250db2b13ff5b3
Simone Basso <bassosimone@gmail.com>
2022-08-17 12:06:03 +0200

    fix(registry): ensure we can run telegram and webconnectivity
    I made a mistake while adapting code from an experimental branch thus
    breaking these two experiments because of interface conversion.
    
    This diff fixes it.
    
    While there, remove the panic trap for miniooni. Because miniooni is
    an experimental tool, we want to see the full panic text, which definitely
    leads to a more pleasant and effective debugging experience.
    
    See https://github.com/ooni/probe/issues/2216 for context on why we
    were trying to change how we register experiments.
    
    The broken commit is https://github.com/ooni/probe-cli/commit/6a0ae5c70b68a72206ff7ffb530c758661545cc4.

--- internal/cmd/miniooni/main.go
+++ internal/cmd/miniooni/main.go
@@ -6,16 +6,6 @@ package main
 // Main function
 //
 
-import (
-	"fmt"
-	"os"
-)
-
 func main() {
-	defer func() {
-		if s := recover(); s != nil {
-			fmt.Fprintf(os.Stderr, "FATAL: %s\n", s)
-		}
-	}()
 	Main()
 }
