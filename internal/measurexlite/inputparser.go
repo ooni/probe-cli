e3bf9d0d57dabb4cffdcd46f807f0e2da668805e
decfox <decfox@github.com>
2022-08-26 23:00:22 +0530

    feat: introduce input parser for experiments

---
+++ internal/measurexlite/inputparser.go
@@ -0,0 +1,75 @@
+package measurexlite
+
+//
+// Input parsing for experiments
+//
+
+import (
+	"errors"
+	"net"
+	"net/url"
+)
+
+var (
+	// ErrInvalidConfiguration indiactes that the parser configuration is invalid
+	ErrInvalidConfiguration = errors.New("invalid configuration for InputParser.AllowEndpoints")
+
+	// ErrInvalidInput indicates that the input is invalid
+	ErrInvalidInput = errors.New("input is invalid")
+
+	// ErrInvalidScheme indicates that the scheme is invalid
+	ErrInvalidScheme = errors.New("parsed scheme is invalid")
+)
+
+// InputParser helps to print the experiment's input.
+type InputParser struct {
+	// List of accepted URL schemes.
+	AcceptedSchemes []string
+
+	// Whether to allow endpoints in input.
+	AllowEndpoints bool
+
+	// The default scheme to use if AllowEndpoints == true.
+	DefaultScheme string
+}
+
+// Parse parses the experiment input and returns the resulting URL.
+func (ip *InputParser) Parse(input string) (*url.URL, error) {
+	// put this check at top-level such that we always see if the parser is misconfigured
+	if ip.AllowEndpoints && ip.DefaultScheme == "" {
+		return nil, ErrInvalidConfiguration
+	}
+	URL, err := url.ParseRequestURI(input)
+	if err != nil {
+		return nil, ErrInvalidInput
+	}
+	if URL.Host == "" {
+		return ip.maybeAllowEndpoints(URL)
+	}
+	for _, scheme := range ip.AcceptedSchemes {
+		if URL.Scheme == scheme {
+			return URL, nil
+		}
+	}
+	return nil, ErrInvalidScheme
+}
+
+// Conditionally allows endpoints when ip.AllowEndpoints is true.
+func (ip *InputParser) maybeAllowEndpoints(URL *url.URL) (*url.URL, error) {
+	if !ip.AllowEndpoints {
+		return nil, ErrInvalidConfiguration
+	}
+	if URL.Scheme != "" && URL.Opaque != "" && URL.User == nil &&
+		URL.Host == "" && URL.Path == "" && URL.RawPath == "" &&
+		URL.RawQuery == "" && URL.Fragment == "" && URL.RawFragment == "" {
+		// See https://go.dev/play/p/Rk5pS_zGY5U
+		//
+		// Note that we know that `ip.DefaultScheme != ""` from the above runtime check.
+		out := &url.URL{
+			Scheme: ip.DefaultScheme,
+			Host:   net.JoinHostPort(URL.Scheme, URL.Opaque),
+		}
+		return out, nil
+	}
+	return nil, ErrInvalidInput
+}
---
+++ internal/measurexlite/inputparser_test.go
@@ -0,0 +1,150 @@
+package measurexlite
+
+import (
+	"errors"
+	"net/url"
+	"testing"
+)
+
+func TestInputParser(t *testing.T) {
+	t.Run("invalid configuration", func(t *testing.T) {
+		inputParser := &InputParser{
+			AcceptedSchemes: []string{},
+			AllowEndpoints:  true,
+			DefaultScheme:   "",
+		}
+		parsed, err := inputParser.Parse("www.example.com:80")
+		if !errors.Is(err, ErrInvalidConfiguration) {
+			t.Fatal("unexpected error")
+		}
+		if parsed != nil {
+			t.Fatal("expected nil url")
+		}
+	})
+
+	t.Run("returns URL on success", func(t *testing.T) {
+		inputParser := &InputParser{
+			AcceptedSchemes: []string{"https"},
+			AllowEndpoints:  false,
+			DefaultScheme:   "",
+		}
+		parsed, err := inputParser.Parse("https://example.com")
+		if err != nil {
+			t.Fatal("unexpected error")
+		}
+		if parsed.Scheme != "https" {
+			t.Fatal("unexpected scheme in url")
+		}
+		if parsed.Host != "example.com" {
+			t.Fatal("unexpected host in url")
+		}
+	})
+
+	t.Run("failure on invalid input", func(t *testing.T) {
+		inputParser := &InputParser{
+			AcceptedSchemes: []string{"https"},
+			AllowEndpoints:  true,
+			DefaultScheme:   "tlshandshake",
+		}
+		parsed, err := inputParser.Parse("")
+		if !errors.Is(err, ErrInvalidInput) {
+			t.Fatal("unexpected error")
+		}
+		if parsed != nil {
+			t.Fatal("expected nil url")
+		}
+	})
+
+	t.Run("forwards endpoints to maybeAllowEndpoints", func(t *testing.T) {
+		inputParser := &InputParser{
+			AcceptedSchemes: []string{"https"},
+			AllowEndpoints:  true,
+			DefaultScheme:   "tlshandshake",
+		}
+		parsed, err := inputParser.Parse("www.example.com:80")
+		if err != nil {
+			t.Fatal("unexpected error")
+		}
+		if parsed.Scheme != "tlshandshake" {
+			t.Fatal("unexpected url scheme")
+		}
+		if parsed.Host != "www.example.com:80" {
+			t.Fatal("unexpected url host")
+		}
+	})
+
+	t.Run("falure in scheme", func(t *testing.T) {
+		inputParser := &InputParser{
+			AcceptedSchemes: []string{"https"},
+			AllowEndpoints:  false,
+			DefaultScheme:   "",
+		}
+		parsed, err := inputParser.Parse("tlshandshake://example.com")
+		if !errors.Is(err, ErrInvalidScheme) {
+			t.Fatal("unexpected error")
+		}
+		if parsed != nil {
+			t.Fatal("expected nil url")
+		}
+	})
+}
+
+func TestMaybeAllowEndpoints(t *testing.T) {
+	t.Run("invalid configuration", func(t *testing.T) {
+		inputParser := &InputParser{
+			AcceptedSchemes: []string{},
+			AllowEndpoints:  false,
+			DefaultScheme:   "",
+		}
+		url := &url.URL{
+			Host: "example.com",
+		}
+		parsed, err := inputParser.maybeAllowEndpoints(url)
+		if !errors.Is(err, ErrInvalidConfiguration) {
+			t.Fatal("unexpected error")
+		}
+		if parsed != nil {
+			t.Fatal("expected nil url")
+		}
+	})
+
+	t.Run("returns URL on success", func(t *testing.T) {
+		inputParser := &InputParser{
+			AcceptedSchemes: []string{"https"},
+			AllowEndpoints:  true,
+			DefaultScheme:   "tlshandshake",
+		}
+		url := &url.URL{
+			Scheme: "www.example.com",
+			Opaque: "80",
+		}
+		parsed, err := inputParser.maybeAllowEndpoints(url)
+		if err != nil {
+			t.Fatal("unexpected error")
+		}
+		if parsed.Scheme != "tlshandshake" {
+			t.Fatal("unexpected url scheme")
+		}
+		if parsed.Host != "www.example.com:80" {
+			t.Fatal("unexpected url host")
+		}
+	})
+
+	t.Run("failure on invalid input", func(t *testing.T) {
+		inputParser := &InputParser{
+			AcceptedSchemes: []string{"https"},
+			AllowEndpoints:  true,
+			DefaultScheme:   "tlshandshake",
+		}
+		url := &url.URL{
+			Host: "example.com",
+		}
+		parsed, err := inputParser.maybeAllowEndpoints(url)
+		if !errors.Is(err, ErrInvalidInput) {
+			t.Fatal("unexpected error")
+		}
+		if parsed != nil {
+			t.Fatal("expected nil url")
+		}
+	})
+}
