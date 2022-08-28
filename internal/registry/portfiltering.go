e72906e186f363fb562395d431946146b1f7092e
decfox <decfox@github.com>
2022-08-28 22:28:09 +0530

    feat: make experiment runnable using miniooni

---
+++ internal/registry/portfiltering.go
@@ -0,0 +1,23 @@
+package registry
+
+//
+// Registers the 'portfiltering' experiment
+//
+
+import (
+	"github.com/ooni/probe-cli/v3/internal/engine/experiment/portfiltering"
+	"github.com/ooni/probe-cli/v3/internal/model"
+)
+
+func init() {
+	allexperiments["portfiltering"] = &Factory{
+		build: func(config any) model.ExperimentMeasurer {
+			return portfiltering.NewExperimentMeasurer(
+				config.(portfiltering.Config),
+			)
+		},
+		config:        portfiltering.Config{},
+		interruptible: false,
+		inputPolicy:   model.InputNone,
+	}
+}
