0d7d935b6a1a7006d049b9a639f2a4acd8ec06c8
Simone Basso <bassosimone@gmail.com>
2022-09-07 14:08:03 +0200

    make QA scripts at least run again with go1.18.x

--- QA/pyrun.sh
+++ QA/pyrun.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -ex
-export GOPATH=/jafar/QA/GOPATH GOCACHE=/jafar/QA/GOCACHE GO111MODULE=on
-git config --global --add safe.directory /jafar
+# TODO(bassosimone): investigate why using CGO_ENABLED=1 is such
+# that all DNS lookups return `dns_nxdomain_error`
+export CGO_ENABLED=0 GOPATH=/jafar/QA/GOPATH GOCACHE=/jafar/QA/GOCACHE
 go build -v ./internal/cmd/miniooni
 go build -v ./internal/cmd/jafar
 sudo ./QA/$1.py ./miniooni
