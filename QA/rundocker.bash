1a56bfc2297caa25c4a9023107c7757688e2ff01
Simone Basso <bassosimone@gmail.com>
2022-09-07 14:42:43 +0200

    feat: automatically generate the right dockerfile

--- QA/rundocker.bash
+++ QA/rundocker.bash
@@ -4,6 +4,13 @@ set -euxo pipefail
 
 DOCKER=${DOCKER:-docker}
 
+GOVERSION=$(cat GOVERSION)
+
+cat > QA/Dockerfile << EOF
+FROM golang:$GOVERSION-alpine
+RUN apk add gcc go git musl-dev iptables tmux bind-tools curl sudo python3
+EOF
+
 $DOCKER build -t jafar-qa ./QA/
 
 $DOCKER run --privileged -v$(pwd):/jafar -w/jafar jafar-qa ./QA/dockermain.sh "$@"
