800217d15beb269247854d9214dd6376943a4f69
Simone Basso <bassosimone@gmail.com>
2022-09-08 11:22:42 +0200

    chore: bump web_connectivity@v0.5 version to 0.5.5 (#945)
    chore: web_connectivity v0.5.5
    
    We're bumping the version number to reflect recent improvements in the
    data format implemented in these pull requests:
    
    - https://github.com/ooni/probe-cli/pull/942
    
    - https://github.com/ooni/probe-cli/pull/943
    
    - https://github.com/ooni/probe-cli/pull/944
    
    Reference issue: https://github.com/ooni/probe/issues/2238

--- internal/experiment/webconnectivity/measurer.go
+++ internal/experiment/webconnectivity/measurer.go
@@ -36,7 +36,7 @@ func (m *Measurer) ExperimentName() string {
 
 // ExperimentVersion implements model.ExperimentMeasurer.
 func (m *Measurer) ExperimentVersion() string {
-	return "0.5.4"
+	return "0.5.5"
 }
 
 // Run implements model.ExperimentMeasurer.
