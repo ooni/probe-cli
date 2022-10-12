5a56171411e7a0bdf67b3cee6f8a05b7d5471932
Simone Basso <bassosimone@gmail.com>
2022-10-12 17:32:34 +0200

    fix: maybe a larger timeout makes tests work on windows?

--- internal/netxlite/tproxy_test.go
+++ internal/netxlite/tproxy_test.go
@@ -11,8 +11,7 @@ func TestDefaultTProxy(t *testing.T) {
 	t.Run("DialContext honours the timeout", func(t *testing.T) {
 		tp := &DefaultTProxy{}
 		ctx := context.Background()
-		timeout := time.Nanosecond
-		conn, err := tp.DialContext(ctx, timeout, "tcp", "1.1.1.1:443")
+		conn, err := tp.DialContext(ctx, 100*time.Microsecond, "tcp", "1.1.1.1:443")
 		if err == nil || !strings.HasSuffix(err.Error(), "i/o timeout") {
 			t.Fatal(err)
 		}
