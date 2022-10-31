5f2aea3ec620345d9cbfcf8b9329d89603008bee
Arturo Filastò <arturo@filasto.net>
2022-10-31 17:30:12 +0100

    Fix the signal root CA (#978)
    This cherry picks 606bc93ad26043aadf97159637112193d4157ce3.
    
    See https://github.com/ooni/probe/issues/2344

--- internal/engine/experiment/signal/signal_test.go
+++ internal/engine/experiment/signal/signal_test.go
@@ -17,7 +17,7 @@ func TestNewExperimentMeasurer(t *testing.T) {
 	if measurer.ExperimentName() != "signal" {
 		t.Fatal("unexpected name")
 	}
-	if measurer.ExperimentVersion() != "0.2.1" {
+	if measurer.ExperimentVersion() != "0.2.2" {
 		t.Fatal("unexpected version")
 	}
 }
