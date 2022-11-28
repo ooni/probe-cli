f69e20469e2001c90df066b0a45b9db636e1ce08
Simone Basso <bassosimone@gmail.com>
2022-11-28 13:45:28 +0100

    [ci skip] fix docustrings

--- internal/runtimex/runtimex.go
+++ internal/runtimex/runtimex.go
@@ -16,7 +16,7 @@ func PanicOnError(err error, message string) {
 }
 
 // Assert calls panic if assertion is false. The type passed to
-// panic is an error type wrapping the original error.
+// panic is an error constructed using errors.New(message).
 func Assert(assertion bool, message string) {
 	if !assertion {
 		panic(errors.New(message))
@@ -24,13 +24,13 @@ func Assert(assertion bool, message string) {
 }
 
 // PanicIfTrue calls panic if assertion is true. The type passed to
-// panic is an error type wrapping the original error.
+// panic is an error constructed using errors.New(message).
 func PanicIfTrue(assertion bool, message string) {
 	Assert(!assertion, message)
 }
 
 // PanicIfNil calls panic if the given interface is nil. The type passed to
-// panic is an error type wrapping the original error.
+// panic is an error constructed using errors.New(message).
 func PanicIfNil(v any, message string) {
 	PanicIfTrue(v == nil, message)
 }
