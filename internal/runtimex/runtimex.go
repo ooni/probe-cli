a125674f890f8b1a1827bf55e3de0fd7f4879064
Simone Basso <bassosimone@gmail.com>
2022-11-28 13:40:59 +0100

    runtimex: always pass to panic an error type
    This diff makes the behavior of the functions exported by
    runtimex slightly more consistent. Now all of them pass to
    panic an error type. Before, only PanicOnError passed an
    error type while other functions passed a string. Yet, having
    an uniform way of throwing error types is better because we
    also have a better way of catching errors.

--- internal/runtimex/runtimex.go
+++ internal/runtimex/runtimex.go
@@ -2,28 +2,35 @@
 // https://pkg.go.dev/github.com/m-lab/go/rtx, except that it's simpler.
 package runtimex
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-// PanicOnError calls panic() if err is not nil.
+// PanicOnError calls panic() if err is not nil. The type passed
+// to panic is an error type wrapping the original error.
 func PanicOnError(err error, message string) {
 	if err != nil {
 		panic(fmt.Errorf("%s: %w", message, err))
 	}
 }
 
-// Assert calls panic if assertion is false.
+// Assert calls panic if assertion is false. The type passed to
+// panic is an error type wrapping the original error.
 func Assert(assertion bool, message string) {
 	if !assertion {
-		panic(message)
+		panic(errors.New(message))
 	}
 }
 
-// PanicIfTrue calls panic if assertion is true.
+// PanicIfTrue calls panic if assertion is true. The type passed to
+// panic is an error type wrapping the original error.
 func PanicIfTrue(assertion bool, message string) {
 	Assert(!assertion, message)
 }
 
-// PanicIfNil calls panic if the given interface is nil.
-func PanicIfNil(v interface{}, message string) {
+// PanicIfNil calls panic if the given interface is nil. The type passed to
+// panic is an error type wrapping the original error.
+func PanicIfNil(v any, message string) {
 	PanicIfTrue(v == nil, message)
 }
