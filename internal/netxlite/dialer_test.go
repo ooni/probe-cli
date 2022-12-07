df96bb7a102cc75b303427fb797d6c302b1a9cec
Simone Basso <bassosimone@gmail.com>
2022-12-07 11:07:33 +0100

    fix(netxlite): make integration test a bit more robust

--- internal/netxlite/dialer_test.go
+++ internal/netxlite/dialer_test.go
@@ -118,7 +118,9 @@ func TestDialerSystem(t *testing.T) {
 		})
 
 		t.Run("enforces the configured timeout", func(t *testing.T) {
-			const timeout = 1 * time.Nanosecond
+			// Raised this timer to one second on 2022-12-07 because we've seen
+			// flaky results on Windows systems otherwise.
+			const timeout = 1 * time.Second
 			d := &DialerSystem{timeout: timeout}
 			ctx := context.Background()
 			start := time.Now()
