998633651b2a29bf0df637fe7c49a2b13bd289fe
Simone Basso <bassosimone@gmail.com>
2022-12-09 09:58:49 +0100

    make more progress but recognize extra work to do

--- internal/engine/probeservices/checkin.go
+++ internal/engine/probeservices/checkin.go
@@ -9,11 +9,26 @@ import (
 	"github.com/ooni/probe-cli/v3/internal/runtimex"
 )
 
+// TODO(bassosimone): move newHTTPAPIEndpoint inside probeservices.go when
+// we have converted several APIs to use httpapi instead of httpx.
+
+// TODO(bassosimone): convert all APIs to use httpapi's API
+
+// newHTTPAPIEndpoint is a convenience function for constructing a new
+// instance of *httpapi.Endpoint based on the content of Client
+func (c Client) newHTTPAPIEndpoint() *httpapi.Endpoint {
+	return &httpapi.Endpoint{
+		BaseURL:    c.BaseURL,
+		HTTPClient: c.HTTPClient,
+		Host:       c.Host,
+		UserAgent:  c.UserAgent,
+	}
+}
+
 // CheckIn function is called by probes asking if there are tests to be run
 // The config argument contains the mandatory settings.
 // Returns the list of tests to run and the URLs, on success, or an explanatory error, in case of failure.
 func (c Client) CheckIn(ctx context.Context, config model.OOAPICheckInConfig) (*model.OOAPICheckInNettests, error) {
-	// TODO(bassosimone): convert all APIs to use httpapi's API
 	endpoint := &httpapi.Endpoint{
 		BaseURL:    c.BaseURL,
 		HTTPClient: c.HTTPClient,
@@ -21,6 +36,7 @@ func (c Client) CheckIn(ctx context.Context, config model.OOAPICheckInConfig) (*
 		UserAgent:  c.UserAgent,
 	}
 	// TODO(bassosimone): refactor this code to be able to pass c.Logger as the logger
+	// TODO(bassosimone): the logger _actually_ belongs to the endpoint!
 	desc, err := httpapi.NewPOSTJSONWithJSONResponseDescriptor(log.Log, "/api/v1/check-in", config)
 	runtimex.PanicOnError(err, "httpapi.NewPOSTJSONWithJSONResponseDescriptor failed")
 	desc.AcceptEncodingGzip = true
