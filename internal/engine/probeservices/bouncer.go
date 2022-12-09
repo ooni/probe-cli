a05050f90f411ab941623bc499eef3c61c7159d5
Simone Basso <bassosimone@gmail.com>
2022-12-09 09:55:54 +0100

    back out unnecessary changes

--- internal/engine/probeservices/bouncer.go
+++ internal/engine/probeservices/bouncer.go
@@ -3,25 +3,12 @@ package probeservices
 import (
 	"context"
 
-	"github.com/apex/log"
-	"github.com/ooni/probe-cli/v3/internal/httpapi"
 	"github.com/ooni/probe-cli/v3/internal/model"
-	"github.com/ooni/probe-cli/v3/internal/runtimex"
 )
 
 // GetTestHelpers is like GetCollectors but for test helpers.
 func (c Client) GetTestHelpers(
 	ctx context.Context) (output map[string][]model.OOAPIService, err error) {
-	endpoint := &httpapi.Endpoint{
-		BaseURL:    c.BaseURL,
-		HTTPClient: c.HTTPClient,
-		Host:       c.Host,
-		UserAgent:  c.UserAgent,
-	}
-	// TODO(bassosimone): refactor this code to be able to pass c.Logger as the logger
-	desc := httpapi.NewGETJSONDescriptor(log.Log, "/api/v1/test-helpers")
-	runtimex.PanicOnError(err, "httpapi.NewPOSTJSONWithJSONResponseDescriptor failed")
-	desc.AcceptEncodingGzip = true
-	err = httpapi.CallWithJSONResponse(ctx, desc, endpoint, &output)
+	err = c.APIClientTemplate.WithBodyLogging().Build().GetJSON(ctx, "/api/v1/test-helpers", &output)
 	return
 }
