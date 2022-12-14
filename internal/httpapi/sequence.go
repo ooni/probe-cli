4f4e56bcec39ae47f43b0b3d31ba9f77e5637e68
Simone Basso <bassosimone@gmail.com>
2022-12-14 12:12:56 +0100

    cleanup(httpapi): remove optional logging-related complexity (#1008)
    1. we don't care _this much_ about body logging that we need to have
    a specific test making sure we _actually_ log the bodies. If we break
    body logging by incident, that's annoying but not really bad. In
    exchange, we remove the most complex test of this package.
    
    2. it's pointless to log that the content-type header is not the
    expected one when expecting JSON. In case of parsing failure, the
    user could use `-v` to turn on verbose mode and see logs. Hence,
    we don't need to be this picky and this also allows us to zap some
    extra code and testing complexity.
    
    3. while there, avoid using `|foo|` in documentation comments
    because the proper form is actually `[foo]`.
    
    All this work is really just yak shaving to reduce the noise in a
    diff for https://github.com/ooni/probe/issues/2372.

--- internal/httpapi/sequence.go
+++ internal/httpapi/sequence.go
@@ -15,20 +15,20 @@ import (
 	"github.com/ooni/probe-cli/v3/internal/multierror"
 )
 
-// SequenceCaller calls the API specified by |Descriptor| once for each of
-// the available |Endpoints| until one of them succeeds.
+// SequenceCaller calls the API specified by [Descriptor] once for each of
+// the available [Endpoint]s until one of them succeeds.
 //
 // CAVEAT: this code will ONLY retry API calls with subsequent endpoints when
 // the error originates in the HTTP round trip or while reading the body.
 type SequenceCaller struct {
-	// Descriptor is the API |Descriptor|.
+	// Descriptor is the API [Descriptor].
 	Descriptor *Descriptor
 
-	// Endpoints is the list of |Endpoint| to use.
+	// Endpoints is the list of [Endpoint] to use.
 	Endpoints []*Endpoint
 }
 
-// NewSequenceCaller is a factory for creating a |SequenceCaller|.
+// NewSequenceCaller is a factory for creating a [SequenceCaller].
 func NewSequenceCaller(desc *Descriptor, endpoints ...*Endpoint) *SequenceCaller {
 	return &SequenceCaller{
 		Descriptor: desc,
@@ -40,14 +40,14 @@ func NewSequenceCaller(desc *Descriptor, endpoints ...*Endpoint) *SequenceCaller
 var ErrAllEndpointsFailed = errors.New("httpapi: all endpoints failed")
 
 // shouldRetry returns true when we should try with another endpoint given the
-// value of |err| which could (obviously) be nil in case of success.
+// value of err which could (obviously) be nil in case of success.
 func (sc *SequenceCaller) shouldRetry(err error) bool {
 	var kind *errMaybeCensorship
 	belongs := errors.As(err, &kind)
 	return belongs
 }
 
-// Call calls |Call| for each |Endpoint| and |Descriptor| until one endpoint succeeds. The
+// Call calls [Call] for each [Endpoint] and [Descriptor] until one endpoint succeeds. The
 // return value is the response body and the selected endpoint index or the error.
 //
 // CAVEAT: this code will ONLY retry API calls with subsequent endpoints when
@@ -69,8 +69,8 @@ func (sc *SequenceCaller) Call(ctx context.Context) ([]byte, int, error) {
 	return nil, -1, merr
 }
 
-// CallWithJSONResponse is like |SequenceCaller.Call| except that it invokes the
-// underlying |CallWithJSONResponse| rather than invoking |Call|.
+// CallWithJSONResponse is like [SequenceCaller.Call] except that it invokes the
+// underlying [CallWithJSONResponse] rather than invoking [Call].
 //
 // CAVEAT: this code will ONLY retry API calls with subsequent endpoints when
 // the error originates in the HTTP round trip or while reading the body.
