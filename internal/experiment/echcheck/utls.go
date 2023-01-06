cc38abc7c4c8e3acd9115ab7472f02ebce64cb5e
kelmenhorst <k.elmenhorst@mailbox.org>
2023-01-06 13:00:44 +0100

    cli: make utlsConn public

--- internal/experiment/echcheck/utls.go
+++ internal/experiment/echcheck/utls.go
@@ -3,13 +3,16 @@ package echcheck
 import (
 	"context"
 	"crypto/tls"
-	"github.com/ooni/probe-cli/v3/internal/model"
-	utls "gitlab.com/yawning/utls.git"
 	"net"
+
+	"github.com/ooni/probe-cli/v3/internal/model"
+	"github.com/ooni/probe-cli/v3/internal/netxlite"
+	"github.com/ooni/probe-cli/v3/internal/runtimex"
+	utls "gitlab.com/yawning/utls.git"
 )
 
 type tlsHandshakerWithExtensions struct {
-	conn       utlsConn
+	conn       *netxlite.UTLSConn
 	extensions []utls.TLSExtension
 	dl         model.DebugLogger
 	id         *utls.ClientHelloID
@@ -31,60 +34,16 @@ func newHandshakerWithExtensions(extensions []utls.TLSExtension) func(dl model.D
 
 func (t *tlsHandshakerWithExtensions) Handshake(ctx context.Context, conn net.Conn, tlsConfig *tls.Config) (
 	net.Conn, tls.ConnectionState, error) {
-	t.conn = newConnUTLSWithHelloID(conn, tlsConfig, t.id)
+	var err error
+	t.conn, err = netxlite.NewUTLSConn(conn, tlsConfig, t.id)
+	runtimex.Assert(err == nil, "unexpected error when creating UTLSConn")
 
 	if t.extensions != nil && len(t.extensions) != 0 {
 		t.conn.BuildHandshakeState()
 		t.conn.Extensions = append(t.conn.Extensions, t.extensions...)
 	}
 
-	err := t.conn.Handshake()
+	err = t.conn.Handshake()
 
 	return t.conn.NetConn(), t.conn.ConnectionState(), err
-}
-
-// TODO remove after https://github.com/ooni/probe/issues/2378
-// utlsConn is a utls connection
-type utlsConn struct {
-	*utls.UConn
-	nc net.Conn
-}
-
-// newConnUTLSWithHelloID creates a new connection with the given client hello ID.
-func newConnUTLSWithHelloID(conn net.Conn, config *tls.Config, cid *utls.ClientHelloID) utlsConn {
-	uConfig := &utls.Config{
-		DynamicRecordSizingDisabled: config.DynamicRecordSizingDisabled,
-		InsecureSkipVerify:          config.InsecureSkipVerify,
-		RootCAs:                     config.RootCAs,
-		NextProtos:                  config.NextProtos,
-		ServerName:                  config.ServerName,
-	}
-	tlsConn := utls.UClient(conn, uConfig, *cid)
-	oconn := utlsConn{
-		UConn: tlsConn,
-		nc:    conn,
-	}
-	return oconn
-}
-
-func (c *utlsConn) ConnectionState() tls.ConnectionState {
-	uState := c.Conn.ConnectionState()
-	return tls.ConnectionState{
-		Version:                     uState.Version,
-		HandshakeComplete:           uState.HandshakeComplete,
-		DidResume:                   uState.DidResume,
-		CipherSuite:                 uState.CipherSuite,
-		NegotiatedProtocol:          uState.NegotiatedProtocol,
-		NegotiatedProtocolIsMutual:  uState.NegotiatedProtocolIsMutual,
-		ServerName:                  uState.ServerName,
-		PeerCertificates:            uState.PeerCertificates,
-		VerifiedChains:              uState.VerifiedChains,
-		SignedCertificateTimestamps: uState.SignedCertificateTimestamps,
-		OCSPResponse:                uState.OCSPResponse,
-		TLSUnique:                   uState.TLSUnique,
-	}
-}
-
-func (c *utlsConn) NetConn() net.Conn {
-	return c.nc
 }
