c6b5d14788808979df3717cb1b1f0d08f690f3bc
Simone Basso <bassosimone@gmail.com>
2023-01-08 16:51:06 +0100

    finish porting monorepo to probe-cli

--- MONOREPO/w/build-android-with-cli.bash
+++ MONOREPO/w/build-android-with-cli.bash
@@ -6,15 +6,18 @@ cd $reporoot
 
 source $reporoot/MONOREPO/tools/libcore.bash
 
+./MOBILE/android/newkeystore
+
 ./MOBILE/gomobile android ./pkg/oonimkall
 
-run mv -v MOBILE/android/oonimkall.aar ./MONOREPO/repo/probe-android/engine-experimental/
+run cp -v MOBILE/android/oonimkall.aar ./MONOREPO/repo/probe-android/engine-experimental/
 
 (
 	run export ANDROID_HOME=$(./MOBILE/android/home)
 	run cd ./MONOREPO/repo/probe-android
 	run ./gradlew assembleExperimentalFullRelease
-	# output: ./app/build/outputs/apk/experimentalFull/release/app-experimental-full-release-unsigned.apk"
+	apkdir=./app/build/outputs/apk/experimentalFull/release
+	run cp -v $apkdir/app-experimental-full-release-unsigned.apk  $reporoot/MOBILE/android/app-unsigned.apk
 )
 
-# TODO(bassosimone): we need to sign the APK with a suitable key.
+./MOBILE/android/sign
