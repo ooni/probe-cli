c455b64c7e75965b8d6ccfe02cb15e69958bcc39
Simone Basso <bassosimone@gmail.com>
2023-01-24 14:58:05 +0000

    refactor(shellx): simplify writing tests of code using shellx
    See https://github.com/ooni/probe/issues/2399

--- internal/shellx/shellx.go
+++ internal/shellx/shellx.go
@@ -3,76 +3,110 @@ package shellx
 
 import (
 	"errors"
+	"fmt"
 	"os"
+	"os/exec"
 	"strings"
 
 	"github.com/google/shlex"
 	"github.com/ooni/probe-cli/v3/internal/model"
-	"golang.org/x/sys/execabs"
 )
 
-// runconfig is the configuration for run.
-type runconfig struct {
-	// args contains the command line arguments.
-	args []string
+// Dependencies is the library on which this package depends.
+type Dependencies interface {
+	// CmdOutput is equivalent to calling c.Output.
+	CmdOutput(c *exec.Cmd) ([]byte, error)
 
-	// command is the command to execute.
-	command string
+	// CmdRun is equivalent to calling c.Run.
+	CmdRun(c *exec.Cmd) error
 
-	// loginfof is the logging function.
-	loginfof func(format string, v ...interface{})
-
-	// stdout is the standard output.
-	stdout *os.File
-
-	// stderr is the standard error.
-	stderr *os.File
+	// LookPath is equivalent to calling exec.LookPath.
+	LookPath(file string) (string, error)
 }
 
-// run is the internal function for running commands.
-func run(config runconfig) error {
-	config.loginfof(
-		"exec: %s %s", config.command, strings.Join(config.args, " "))
-	// implementation note: here we're using execabs because
-	// of https://blog.golang.org/path-security.
-	cmd := execabs.Command(config.command, config.args...)
-	cmd.Stdout = config.stdout
-	cmd.Stderr = config.stderr
-	err := cmd.Run()
-	config.loginfof("exec result: %+v", err)
-	return err
+// Library contains the default dependencies. You will want to change
+// this variable when writing tests.
+var Library Dependencies = &StdlibDependencies{}
+
+// StdlibDependencies contains the stdlib implementation of the [Dependencies].
+type StdlibDependencies struct{}
+
+// CmdOutput implements Dependencies
+func (*StdlibDependencies) CmdOutput(c *exec.Cmd) ([]byte, error) {
+	return c.Output()
 }
 
-// Run executes the specified command with the specified args.
-func Run(logger model.InfoLogger, name string, arg ...string) error {
-	return run(runconfig{
-		args:     arg,
-		command:  name,
-		loginfof: logger.Infof,
-		stdout:   os.Stdout,
-		stderr:   os.Stderr,
-	})
+// CmdRun implements Dependencies
+func (*StdlibDependencies) CmdRun(c *exec.Cmd) error {
+	return c.Run()
 }
 
-// quietInfof is an infof function that does nothing.
-func quietInfof(format string, v ...interface{}) {}
-
-// RunQuiet is like Run but it does not emit any output.
-func RunQuiet(name string, arg ...string) error {
-	return run(runconfig{
-		args:     arg,
-		command:  name,
-		loginfof: quietInfof,
-		stdout:   nil,
-		stderr:   nil,
-	})
+// LookPath implements Dependencies
+func (*StdlibDependencies) LookPath(file string) (string, error) {
+	return exec.LookPath(file)
 }
 
-// ErrNoCommandToExecute means that the command line is empty.
-var ErrNoCommandToExecute = errors.New("shellx: no command to execute")
+// Env is the environment in which we execute commands.
+type Env struct {
+	// Vars contains the environment variables to add to the current
+	// environment when we're executing commands.
+	Vars []string
+}
 
-// RunCommandline executes the given command line.
-func RunCommandline(logger model.InfoLogger, cmdline string) error {
+// Append appends an environment variable to the environment.
+func (e *Env) Append(key, value string) {
+	e.Vars = append(e.Vars, fmt.Sprintf("%s=%s", key, value))
+}
+
+// OutputQuiet is like RunQuiet except that, in case of success, it captures
+// the standard output and returns it to the caller.
+func (e *Env) OutputQuiet(command string, args ...string) ([]byte, error) {
+	return e.Output(nil, command, args...)
+}
+
+// Output is like OutputQuiet except that it logs the command to be executed
+// and the environment variables specific to this command.
+func (e *Env) Output(logger model.Logger, command string, args ...string) ([]byte, error) {
+	cmd, err := e.cmd(logger, command, args...)
+	if err != nil {
+		return nil, err
+	}
+	if logger != nil {
+		// note: cmd.Output wants the stdout to be nil
+		cmd.Stderr = os.Stderr
+	}
+	return Library.CmdOutput(cmd) // allows mocking
+}
+
+// RunQuiet runs the given command without emitting any output and
+// using the environment variables in the current [Environ].
+func (e *Env) RunQuiet(command string, args ...string) error {
+	return Run(nil, command, args...)
+}
+
+// Run is like RunQuiet except that it also logs the command to be
+// executed, the environment variables specific to this command, the
+// text logged to stdout and stderr.
+func (e *Env) Run(logger model.Logger, command string, args ...string) error {
+	cmd, err := e.cmd(logger, command, args...)
+	if err != nil {
+		return err
+	}
+	if logger != nil {
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+	}
+	return Library.CmdRun(cmd) // allows mocking
+}
+
+// RunCommandLineQuiet is like RunQuiet but takes a command line as argument.
+func (e *Env) RunCommandLineQuiet(cmdline string) error {
+	return e.RunCommandLine(nil, cmdline)
+}
+
+// RunCommandLine is like RunCommandLineQuiet but logs the command to
+// execute as well as the command-specific environment variables.
+func (e *Env) RunCommandLine(logger model.Logger, cmdline string) error {
 	args, err := shlex.Split(cmdline)
 	if err != nil {
 		return err
@@ -80,5 +114,90 @@ func RunCommandline(logger model.InfoLogger, cmdline string) error {
 	if len(args) < 1 {
 		return ErrNoCommandToExecute
 	}
-	return Run(logger, args[0], args[1:]...)
+	return e.Run(logger, args[0], args[1:]...)
+}
+
+// OutputCommandLineQuiet is like OutputQuiet but takes a command line as argument.
+func (e *Env) OutputCommandLineQuiet(cmdline string) ([]byte, error) {
+	return e.OutputCommandLine(nil, cmdline)
+}
+
+// OutputCommandLine is like OutputCommandLineQuiet but logs the command to
+// execute as well as the command-specific environment variables.
+func (e *Env) OutputCommandLine(logger model.Logger, cmdline string) ([]byte, error) {
+	args, err := shlex.Split(cmdline)
+	if err != nil {
+		return nil, err
+	}
+	if len(args) < 1 {
+		return nil, ErrNoCommandToExecute
+	}
+	return e.Output(logger, args[0], args[1:]...)
+}
+
+// cmd is an internal factory for creating a new command.
+func (e *Env) cmd(logger model.Logger, command string, args ...string) (*exec.Cmd, error) {
+	fullpath, err := Library.LookPath(command) // allows mocking
+	if err != nil {
+		return nil, err
+	}
+	// Implementation note: since Go 1.19 we don't need to use the execabs
+	// package anymore. See <https://tip.golang.org/doc/go1.19>.
+	cmd := exec.Command(fullpath, args...)
+	cmd.Env = os.Environ()
+	for _, entry := range e.Vars {
+		if logger != nil {
+			logger.Infof("+ export %s", entry)
+		}
+		cmd.Env = append(cmd.Env, entry)
+	}
+	if logger != nil {
+		cmdline := quotedCommandLine(fullpath, args...)
+		logger.Infof("+ %s", cmdline)
+	}
+	return cmd, nil
+}
+
+// Run calls [Env.Run] using an empty [Env].
+func Run(logger model.Logger, program string, args ...string) error {
+	return (&Env{}).Run(logger, program, args...)
+}
+
+// RunQuiet calls [Env.RunQuiet] using an empty [Env].
+func RunQuiet(program string, args ...string) error {
+	return (&Env{}).RunQuiet(program, args...)
+}
+
+// ErrNoCommandToExecute means that the command line is empty.
+var ErrNoCommandToExecute = errors.New("shellx: no command to execute")
+
+// RunCommandLine calls [Env.RunCommandLine] using an empty [Env].
+func RunCommandLine(logger model.Logger, cmdline string) error {
+	return (&Env{}).RunCommandLine(logger, cmdline)
+}
+
+// RunCommandLineQuiet calls [Env.RunCommandLineQuiet] using an empty [Env].
+func RunCommandLineQuiet(cmdline string) error {
+	return (&Env{}).RunCommandLineQuiet(cmdline)
+}
+
+// quotedCommandLine returns a quoted command line.
+func quotedCommandLine(command string, args ...string) string {
+	v := []string{}
+	v = append(v, maybeQuoteArg(command))
+	for _, a := range args {
+		v = append(v, maybeQuoteArg(a))
+	}
+	return strings.Join(v, " ")
+}
+
+// maybeQuoteArg quotes a command line argument if needed.
+func maybeQuoteArg(a string) string {
+	if strings.Contains(a, "\"") {
+		a = strings.ReplaceAll(a, "\"", "\\\"")
+	}
+	if strings.Contains(a, " ") {
+		a = "\"" + a + "\""
+	}
+	return a
 }
--- internal/shellx/shellx_test.go
+++ internal/shellx/shellx_test.go
@@ -1,48 +1,301 @@
 package shellx
 
 import (
+	"errors"
+	"runtime"
+	"strings"
 	"testing"
 
-	"github.com/apex/log"
+	"github.com/google/go-cmp/cmp"
+	"github.com/ooni/probe-cli/v3/internal/model"
 )
 
-func TestRun(t *testing.T) {
-	if err := Run(log.Log, "whoami"); err != nil {
-		t.Fatal(err)
-	}
-	if err := Run(log.Log, "./nonexistent/command"); err == nil {
-		t.Fatal("expected an error here")
+// testGolangExe is the golang exe to use in this test suite
+var testGolangExe string
+
+func init() {
+	switch runtime.GOOS {
+	case "windows":
+		testGolangExe = "go.exe"
+	default:
+		testGolangExe = "go"
 	}
 }
 
+// testErrorIsExecutableNotFound returns whether the error
+// is the one returned when an executable isn't found
+func testErrorIsExecutableNotFound(err error) bool {
+	return err != nil && strings.Contains(err.Error(), "executable file not found")
+}
+
+// testErrorIsCannotParseCmdLine returns whether the error
+// is the one returned when you cannot parse a cmdline.
+func testErrorIsCannotParseCmdLine(err error) bool {
+	return err != nil && err.Error() == "EOF found when expecting closing quote"
+}
+
+func TestRun(t *testing.T) {
+	t.Run("with a valid command", func(t *testing.T) {
+		if err := Run(model.DiscardLogger, testGolangExe, "version"); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("with an invalid command", func(t *testing.T) {
+		err := Run(model.DiscardLogger, "nonexistent", "version")
+		if !testErrorIsExecutableNotFound(err) {
+			t.Fatal("unexpected error", err)
+		}
+	})
+}
+
 func TestRunQuiet(t *testing.T) {
-	if err := RunQuiet("true"); err != nil {
-		t.Fatal(err)
-	}
-	if err := RunQuiet("./nonexistent/command"); err == nil {
-		t.Fatal("expected an error here")
-	}
+	t.Run("with a valid command", func(t *testing.T) {
+		if err := RunQuiet(testGolangExe, "version"); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("with an invalid command", func(t *testing.T) {
+		err := RunQuiet("nonexistent", "version")
+		if !testErrorIsExecutableNotFound(err) {
+			t.Fatal("unexpected error", err)
+		}
+	})
 }
 
 func TestRunCommandline(t *testing.T) {
 	t.Run("when the command does not parse", func(t *testing.T) {
-		if err := RunCommandline(log.Log, `"foobar`); err == nil {
-			t.Fatal("expected an error here")
+		err := RunCommandLine(model.DiscardLogger, `"foobar`)
+		if !testErrorIsCannotParseCmdLine(err) {
+			t.Fatal("unexpected error", err)
 		}
 	})
+
 	t.Run("when we have no arguments", func(t *testing.T) {
-		if err := RunCommandline(log.Log, ""); err == nil {
-			t.Fatal("expected an error here")
+		err := RunCommandLine(model.DiscardLogger, "")
+		if !errors.Is(err, ErrNoCommandToExecute) {
+			t.Fatal("unexpected error", err)
 		}
 	})
-	t.Run("when we have a single argument", func(t *testing.T) {
-		if err := RunCommandline(log.Log, "ls"); err != nil {
+
+	t.Run("when we have arguments", func(t *testing.T) {
+		if err := RunCommandLine(model.DiscardLogger, testGolangExe+" version"); err != nil {
 			t.Fatal(err)
 		}
 	})
-	t.Run("when we have more than one argument", func(t *testing.T) {
-		if err := RunCommandline(log.Log, "ls ."); err != nil {
+}
+
+func TestRunCommandlineQuiet(t *testing.T) {
+	t.Run("when the command does not parse", func(t *testing.T) {
+		err := RunCommandLineQuiet(`"foobar`)
+		if !testErrorIsCannotParseCmdLine(err) {
+			t.Fatal("unexpected error", err)
+		}
+	})
+
+	t.Run("when we have no arguments", func(t *testing.T) {
+		err := RunCommandLineQuiet("")
+		if !errors.Is(err, ErrNoCommandToExecute) {
+			t.Fatal("unexpected error", err)
+		}
+	})
+
+	t.Run("when we have arguments", func(t *testing.T) {
+		if err := RunCommandLineQuiet(testGolangExe + " version"); err != nil {
 			t.Fatal(err)
 		}
 	})
+}
+
+func TestEnv(t *testing.T) {
+	t.Run("Output", func(t *testing.T) {
+		t.Run("with a valid command", func(t *testing.T) {
+			env := &Env{}
+			output, err := env.Output(model.DiscardLogger, testGolangExe, "env")
+			if err != nil {
+				t.Fatal(err)
+			}
+			if len(output) <= 0 {
+				t.Fatal("expected to see output")
+			}
+		})
+
+		t.Run("with an invalid command", func(t *testing.T) {
+			env := &Env{}
+			output, err := env.Output(model.DiscardLogger, "nonexistent", "env")
+			if !testErrorIsExecutableNotFound(err) {
+				t.Fatal("unexpected error", err)
+			}
+			if len(output) > 0 {
+				t.Fatal("expected to see no output")
+			}
+		})
+	})
+
+	t.Run("OutputQuiet", func(t *testing.T) {
+		t.Run("with a valid command", func(t *testing.T) {
+			env := &Env{}
+			output, err := env.OutputQuiet(testGolangExe, "env")
+			if err != nil {
+				t.Fatal(err)
+			}
+			if len(output) <= 0 {
+				t.Fatal("expected to see output")
+			}
+		})
+
+		t.Run("with an invalid command", func(t *testing.T) {
+			env := &Env{}
+			output, err := env.OutputQuiet("nonexistent", "env")
+			if !testErrorIsExecutableNotFound(err) {
+				t.Fatal("unexpected error", err)
+			}
+			if len(output) > 0 {
+				t.Fatal("expected to see no output")
+			}
+		})
+	})
+
+	t.Run("OutputCommandLine", func(t *testing.T) {
+		t.Run("with a valid command", func(t *testing.T) {
+			env := &Env{}
+			output, err := env.OutputCommandLine(model.DiscardLogger, testGolangExe+" env")
+			if err != nil {
+				t.Fatal(err)
+			}
+			if len(output) <= 0 {
+				t.Fatal("expected to see output")
+			}
+		})
+
+		t.Run("with an invalid command", func(t *testing.T) {
+			env := &Env{}
+			output, err := env.OutputCommandLine(model.DiscardLogger, "nonexistent env")
+			if !testErrorIsExecutableNotFound(err) {
+				t.Fatal("unexpected error", err)
+			}
+			if len(output) > 0 {
+				t.Fatal("expected to see no output")
+			}
+		})
+
+		t.Run("with empty command line", func(t *testing.T) {
+			env := &Env{}
+			output, err := env.OutputCommandLine(model.DiscardLogger, "")
+			if !errors.Is(err, ErrNoCommandToExecute) {
+				t.Fatal("unexpected error", err)
+			}
+			if len(output) > 0 {
+				t.Fatal("expected to see no output")
+			}
+		})
+
+		t.Run("with invalid command line", func(t *testing.T) {
+			env := &Env{}
+			output, err := env.OutputCommandLine(model.DiscardLogger, "\"foobar")
+			if !testErrorIsCannotParseCmdLine(err) {
+				t.Fatal("unexpected error", err)
+			}
+			if len(output) > 0 {
+				t.Fatal("expected to see no output")
+			}
+		})
+
+		t.Run("with environment variables", func(t *testing.T) {
+			env := &Env{}
+			env.Append("GOCACHE", "/foobar")
+			output, err := env.OutputCommandLine(model.DiscardLogger, "go env GOCACHE")
+			if err != nil {
+				t.Fatal(err)
+			}
+			expected := []byte("/foobar\n")
+			if diff := cmp.Diff(expected, output); diff != "" {
+				t.Fatal(diff)
+			}
+		})
+	})
+
+	t.Run("OutputCommandLineQuiet", func(t *testing.T) {
+		t.Run("with a valid command", func(t *testing.T) {
+			env := &Env{}
+			output, err := env.OutputCommandLineQuiet(testGolangExe + " env")
+			if err != nil {
+				t.Fatal(err)
+			}
+			if len(output) <= 0 {
+				t.Fatal("expected to see output")
+			}
+		})
+
+		t.Run("with an invalid command", func(t *testing.T) {
+			env := &Env{}
+			output, err := env.OutputCommandLineQuiet("nonexistent env")
+			if !testErrorIsExecutableNotFound(err) {
+				t.Fatal("unexpected error", err)
+			}
+			if len(output) > 0 {
+				t.Fatal("expected to see no output")
+			}
+		})
+
+		t.Run("with environment variables", func(t *testing.T) {
+			env := &Env{}
+			env.Append("GOCACHE", "/foobar")
+			output, err := env.OutputCommandLineQuiet("go env GOCACHE")
+			if err != nil {
+				t.Fatal(err)
+			}
+			expected := []byte("/foobar\n")
+			if diff := cmp.Diff(expected, output); diff != "" {
+				t.Fatal(diff)
+			}
+		})
+	})
+}
+
+func Test_maybeQuoteArg(t *testing.T) {
+	type args struct {
+		a string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{{
+		name: "for empty string",
+		args: args{},
+		want: "",
+	}, {
+		name: "without spaces",
+		args: args{
+			a: "helloworld",
+		},
+		want: "helloworld",
+	}, {
+		name: "with spaces",
+		args: args{
+			a: "hello world",
+		},
+		want: "\"hello world\"",
+	}, {
+		name: "with quotes",
+		args: args{
+			a: "hello\"world",
+		},
+		want: "hello\\\"world",
+	}, {
+		name: "with quotes and spaces",
+		args: args{
+			a: "hello \" world",
+		},
+		want: "\"hello \\\" world\"",
+	}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maybeQuoteArg(tt.args.a); got != tt.want {
+				t.Errorf("maybeQuoteArg() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
