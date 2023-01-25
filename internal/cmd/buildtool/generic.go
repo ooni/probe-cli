801faedbb3d20409ad02622a8f02c7fc73c1fd87
Simone Basso <bassosimone@gmail.com>
2023-01-25 17:40:31 +0100

    feat: rewrite generic build rules in Go
    This diff rewrites the generic build rules in Go.
    
    See https://github.com/ooni/probe/issues/2401.

---
+++ internal/cmd/buildtool/generic.go
@@ -0,0 +1,63 @@
+package main
+
+//
+// Generic builder for the current GOOS/GOARCH
+//
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/apex/log"
+	"github.com/ooni/probe-cli/v3/internal/cmd/buildtool/internal/buildtoolmodel"
+	"github.com/ooni/probe-cli/v3/internal/runtimex"
+	"github.com/ooni/probe-cli/v3/internal/shellx"
+	"github.com/spf13/cobra"
+)
+
+// genericSubcommand returns the generic sucommand.
+func genericSubcommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "generic",
+		Short: fmt.Sprintf("Generic Go builder for %s/%s", runtime.GOOS, runtime.GOARCH),
+	}
+	cmd.AddCommand(&cobra.Command{
+		Use:   "miniooni",
+		Short: "Builds miniooni for the current GOOS and GOARCH",
+		Args:  cobra.NoArgs,
+		Run: func(cmd *cobra.Command, args []string) {
+			genericBuildPackage(&buildDeps{}, productMiniooni)
+		},
+	})
+	cmd.AddCommand(&cobra.Command{
+		Use:   "ooniprobe",
+		Short: "Builds ooniprobe for the current GOOS and GOARCH",
+		Args:  cobra.NoArgs,
+		Run: func(cmd *cobra.Command, args []string) {
+			genericBuildPackage(&buildDeps{}, productOoniprobe)
+		},
+	})
+	return cmd
+}
+
+// genericBuildPackage is the generic function for building a package.
+func genericBuildPackage(deps buildtoolmodel.Dependencies, product *product) {
+	deps.PsiphonMaybeCopyConfigFiles()
+	deps.GolangCheck()
+
+	log.Infof("building %s for %s/%s", product.Pkg, runtime.GOOS, runtime.GOARCH)
+
+	argv := runtimex.Try1(shellx.NewArgv("go", "build"))
+	if deps.PsiphonFilesExist() {
+		argv.Append("-tags", "ooni_psiphon_config")
+	}
+	argv.Append("-ldflags", "-s -w")
+	argv.Append(product.Pkg)
+
+	config := &shellx.Config{
+		Logger: log.Log,
+		Flags:  shellx.FlagShowStdoutStderr,
+	}
+
+	runtimex.Try0(shellx.RunEx(config, argv, &shellx.Envp{}))
+}
