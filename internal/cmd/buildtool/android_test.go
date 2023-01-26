b4517f46bc2cca3ace28cdb517ffd8ada7d706c0
Simone Basso <bassosimone@gmail.com>
2023-01-26 19:12:25 +0000

    feat: add unit tests for openssl

--- internal/cmd/buildtool/android_test.go
+++ internal/cmd/buildtool/android_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"os"
+	"runtime"
+	"strconv"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -639,6 +642,300 @@ func TestAndroidBuildCdepsZlib(t *testing.T) {
 
 			shellxtesting.WithCustomLibrary(cc, func() {
 				androidCdepsBuildMain("zlib", deps)
+			})
+
+			expectCalls := map[string]int{
+				buildtooltest.TagAbsoluteCurDir:  4,
+				buildtooltest.TagAndroidNDKCheck: 1,
+				buildtooltest.TagAndroidSDKCheck: 1,
+				buildtooltest.TagMustChdir:       4,
+				buildtooltest.TagVerifySHA256:    4,
+			}
+
+			if diff := cmp.Diff(expectCalls, deps.Counter); diff != "" {
+				t.Fatal(diff)
+			}
+
+			if err := buildtooltest.CheckManyCommands(cc.Commands, testcase.expect); err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
+}
+
+func TestAndroidBuildCdepsOpenSSL(t *testing.T) {
+	faketopdir := (&buildtooltest.DependenciesCallCounter{}).AbsoluteCurDir()
+	fakeBinPath := testAndroidGetFakeBinpath()
+
+	// testspec specifies a test case for this test
+	type testspec struct {
+		// name is the name of the test case
+		name string
+
+		// expectations contains the commands we expect to see
+		expect []buildtooltest.ExecExpectations
+	}
+
+	var testcases = []testspec{{
+		name: "openssl",
+		expect: []buildtooltest.ExecExpectations{{
+			Env: []string{},
+			Argv: []string{
+				"curl", "-fsSLO", "https://www.openssl.org/source/openssl-1.1.1s.tar.gz",
+			},
+		}, {
+			Env: []string{},
+			Argv: []string{
+				"tar", "-xf", "openssl-1.1.1s.tar.gz",
+			},
+		}, {
+			Env: []string{},
+			Argv: []string{
+				"git", "apply", faketopdir + "/CDEPS/openssl/000.patch",
+			},
+		}, {
+			Env: []string{},
+			Argv: []string{
+				"git", "apply", faketopdir + "/CDEPS/openssl/001.patch",
+			},
+		}, {
+			Env: []string{
+				"ANDROID_NDK_HOME=Android/sdk/ndk/25.1.7654321",
+				"CXXFLAGS=-fdata-sections -ffunction-sections -fstack-protector-strong -funwind-tables -no-canonical-prefixes -D_FORTIFY_SOURCE=2 -fpic -Oz -DANDROID -fsanitize=bounds -fsanitize-undefined-trap-on-error -mthumb -Wno-macro-redefined",
+				"ANDROID_HOME=Android/sdk",
+				"CFLAGS=-fdata-sections -ffunction-sections -fstack-protector-strong -funwind-tables -no-canonical-prefixes -D_FORTIFY_SOURCE=2 -fpic -Oz -DANDROID -fsanitize=bounds -fsanitize-undefined-trap-on-error -mthumb -Wno-macro-redefined",
+				"ANDROID_NDK_ROOT=Android/sdk/ndk/25.1.7654321",
+				"PATH=" + fakeBinPath + ":" + os.Getenv("PATH"),
+			},
+			Argv: []string{
+				"./Configure", "no-comp", "no-dtls", "no-ec2m", "no-psk", "no-srp",
+				"no-ssl2", "no-ssl3", "no-camellia", "no-idea", "no-md2", "no-md4",
+				"no-mdc2", "no-rc2", "no-rc4", "no-rc5", "no-rmd160", "no-whirlpool",
+				"no-dso", "no-hw", "no-ui-console", "no-shared", "no-unit-test",
+				"android-arm", "-D__ANDROID_API__=21", "--libdir=lib", "--prefix=/", "--openssldir=/",
+			},
+		}, {
+			Env: []string{
+				"ANDROID_NDK_HOME=Android/sdk/ndk/25.1.7654321",
+				"CXXFLAGS=-fdata-sections -ffunction-sections -fstack-protector-strong -funwind-tables -no-canonical-prefixes -D_FORTIFY_SOURCE=2 -fpic -Oz -DANDROID -fsanitize=bounds -fsanitize-undefined-trap-on-error -mthumb -Wno-macro-redefined",
+				"ANDROID_HOME=Android/sdk",
+				"CFLAGS=-fdata-sections -ffunction-sections -fstack-protector-strong -funwind-tables -no-canonical-prefixes -D_FORTIFY_SOURCE=2 -fpic -Oz -DANDROID -fsanitize=bounds -fsanitize-undefined-trap-on-error -mthumb -Wno-macro-redefined",
+				"ANDROID_NDK_ROOT=Android/sdk/ndk/25.1.7654321",
+				"PATH=" + fakeBinPath + ":" + os.Getenv("PATH"),
+			},
+			Argv: []string{
+				"make", "-j", strconv.Itoa(runtime.NumCPU()),
+			},
+		}, {
+			Env: []string{},
+			Argv: []string{
+				"make",
+				"DESTDIR=" + faketopdir + "/internal/cmd/buildtool/internal/libtor/android/arm",
+				"install_dev",
+			},
+		}, {
+			Env: []string{},
+			Argv: []string{
+				"rm", "-rf",
+				faketopdir + "/internal/cmd/buildtool/internal/libtor/android/arm/lib/pkgconfig",
+			},
+		}, {
+			Env: []string{},
+			Argv: []string{
+				"curl", "-fsSLO", "https://www.openssl.org/source/openssl-1.1.1s.tar.gz",
+			},
+		}, {
+			Env: []string{},
+			Argv: []string{
+				"tar", "-xf", "openssl-1.1.1s.tar.gz",
+			},
+		}, {
+			Env: []string{},
+			Argv: []string{
+				"git", "apply", faketopdir + "/CDEPS/openssl/000.patch",
+			},
+		}, {
+			Env: []string{},
+			Argv: []string{
+				"git", "apply", faketopdir + "/CDEPS/openssl/001.patch",
+			},
+		}, {
+			Env: []string{
+				"ANDROID_NDK_HOME=Android/sdk/ndk/25.1.7654321",
+				"CXXFLAGS=-fdata-sections -ffunction-sections -fstack-protector-strong -funwind-tables -no-canonical-prefixes -D_FORTIFY_SOURCE=2 -fpic -O2 -DANDROID -fsanitize=safe-stack -fsanitize=bounds -fsanitize-undefined-trap-on-error -Wno-macro-redefined",
+				"ANDROID_HOME=Android/sdk",
+				"CFLAGS=-fdata-sections -ffunction-sections -fstack-protector-strong -funwind-tables -no-canonical-prefixes -D_FORTIFY_SOURCE=2 -fpic -O2 -DANDROID -fsanitize=safe-stack -fsanitize=bounds -fsanitize-undefined-trap-on-error -Wno-macro-redefined",
+				"ANDROID_NDK_ROOT=Android/sdk/ndk/25.1.7654321",
+				"PATH=" + fakeBinPath + ":" + os.Getenv("PATH"),
+			},
+			Argv: []string{
+				"./Configure", "no-comp", "no-dtls", "no-ec2m", "no-psk", "no-srp",
+				"no-ssl2", "no-ssl3", "no-camellia", "no-idea", "no-md2", "no-md4",
+				"no-mdc2", "no-rc2", "no-rc4", "no-rc5", "no-rmd160", "no-whirlpool",
+				"no-dso", "no-hw", "no-ui-console", "no-shared", "no-unit-test",
+				"android-arm64", "-D__ANDROID_API__=21", "--libdir=lib", "--prefix=/", "--openssldir=/",
+			},
+		}, {
+			Env: []string{
+				"ANDROID_NDK_HOME=Android/sdk/ndk/25.1.7654321",
+				"CXXFLAGS=-fdata-sections -ffunction-sections -fstack-protector-strong -funwind-tables -no-canonical-prefixes -D_FORTIFY_SOURCE=2 -fpic -O2 -DANDROID -fsanitize=safe-stack -fsanitize=bounds -fsanitize-undefined-trap-on-error -Wno-macro-redefined",
+				"ANDROID_HOME=Android/sdk",
+				"CFLAGS=-fdata-sections -ffunction-sections -fstack-protector-strong -funwind-tables -no-canonical-prefixes -D_FORTIFY_SOURCE=2 -fpic -O2 -DANDROID -fsanitize=safe-stack -fsanitize=bounds -fsanitize-undefined-trap-on-error -Wno-macro-redefined",
+				"ANDROID_NDK_ROOT=Android/sdk/ndk/25.1.7654321",
+				"PATH=" + fakeBinPath + ":" + os.Getenv("PATH"),
+			},
+			Argv: []string{
+				"make", "-j", strconv.Itoa(runtime.NumCPU()),
+			},
+		}, {
+			Env: []string{},
+			Argv: []string{
+				"make",
+				"DESTDIR=" + faketopdir + "/internal/cmd/buildtool/internal/libtor/android/arm64",
+				"install_dev",
+			},
+		}, {
+			Env: []string{},
+			Argv: []string{
+				"rm", "-rf",
+				faketopdir + "/internal/cmd/buildtool/internal/libtor/android/arm64/lib/pkgconfig",
+			},
+		}, {
+			Env: []string{},
+			Argv: []string{
+				"curl", "-fsSLO", "https://www.openssl.org/source/openssl-1.1.1s.tar.gz",
+			},
+		}, {
+			Env: []string{},
+			Argv: []string{
+				"tar", "-xf", "openssl-1.1.1s.tar.gz",
+			},
+		}, {
+			Env: []string{},
+			Argv: []string{
+				"git", "apply", faketopdir + "/CDEPS/openssl/000.patch",
+			},
+		}, {
+			Env: []string{},
+			Argv: []string{
+				"git", "apply", faketopdir + "/CDEPS/openssl/001.patch",
+			},
+		}, {
+			Env: []string{
+				"ANDROID_NDK_HOME=Android/sdk/ndk/25.1.7654321",
+				"CXXFLAGS=-fdata-sections -ffunction-sections -fstack-protector-strong -funwind-tables -no-canonical-prefixes -D_FORTIFY_SOURCE=2 -fPIC -O2 -DANDROID -fsanitize=safe-stack -fstack-clash-protection -fsanitize=bounds -fsanitize-undefined-trap-on-error -mstackrealign -Wno-macro-redefined",
+				"ANDROID_HOME=Android/sdk",
+				"CFLAGS=-fdata-sections -ffunction-sections -fstack-protector-strong -funwind-tables -no-canonical-prefixes -D_FORTIFY_SOURCE=2 -fPIC -O2 -DANDROID -fsanitize=safe-stack -fstack-clash-protection -fsanitize=bounds -fsanitize-undefined-trap-on-error -mstackrealign -Wno-macro-redefined",
+				"ANDROID_NDK_ROOT=Android/sdk/ndk/25.1.7654321",
+				"PATH=" + fakeBinPath + ":" + os.Getenv("PATH"),
+			},
+			Argv: []string{
+				"./Configure", "no-comp", "no-dtls", "no-ec2m", "no-psk", "no-srp",
+				"no-ssl2", "no-ssl3", "no-camellia", "no-idea", "no-md2", "no-md4",
+				"no-mdc2", "no-rc2", "no-rc4", "no-rc5", "no-rmd160", "no-whirlpool",
+				"no-dso", "no-hw", "no-ui-console", "no-shared", "no-unit-test",
+				"android-x86", "-D__ANDROID_API__=21", "--libdir=lib", "--prefix=/", "--openssldir=/",
+			},
+		}, {
+			Env: []string{
+				"ANDROID_NDK_HOME=Android/sdk/ndk/25.1.7654321",
+				"CXXFLAGS=-fdata-sections -ffunction-sections -fstack-protector-strong -funwind-tables -no-canonical-prefixes -D_FORTIFY_SOURCE=2 -fPIC -O2 -DANDROID -fsanitize=safe-stack -fstack-clash-protection -fsanitize=bounds -fsanitize-undefined-trap-on-error -mstackrealign -Wno-macro-redefined",
+				"ANDROID_HOME=Android/sdk",
+				"CFLAGS=-fdata-sections -ffunction-sections -fstack-protector-strong -funwind-tables -no-canonical-prefixes -D_FORTIFY_SOURCE=2 -fPIC -O2 -DANDROID -fsanitize=safe-stack -fstack-clash-protection -fsanitize=bounds -fsanitize-undefined-trap-on-error -mstackrealign -Wno-macro-redefined",
+				"ANDROID_NDK_ROOT=Android/sdk/ndk/25.1.7654321",
+				"PATH=" + fakeBinPath + ":" + os.Getenv("PATH"),
+			},
+			Argv: []string{
+				"make", "-j", strconv.Itoa(runtime.NumCPU()),
+			},
+		}, {
+			Env: []string{},
+			Argv: []string{
+				"make",
+				"DESTDIR=" + faketopdir + "/internal/cmd/buildtool/internal/libtor/android/386",
+				"install_dev",
+			},
+		}, {
+			Env: []string{},
+			Argv: []string{
+				"rm", "-rf",
+				faketopdir + "/internal/cmd/buildtool/internal/libtor/android/386/lib/pkgconfig",
+			},
+		}, {
+			Env: []string{},
+			Argv: []string{
+				"curl", "-fsSLO", "https://www.openssl.org/source/openssl-1.1.1s.tar.gz",
+			},
+		}, {
+			Env: []string{},
+			Argv: []string{
+				"tar", "-xf", "openssl-1.1.1s.tar.gz",
+			},
+		}, {
+			Env: []string{},
+			Argv: []string{
+				"git", "apply", faketopdir + "/CDEPS/openssl/000.patch",
+			},
+		}, {
+			Env: []string{},
+			Argv: []string{
+				"git", "apply", faketopdir + "/CDEPS/openssl/001.patch",
+			},
+		}, {
+			Env: []string{
+				"ANDROID_NDK_HOME=Android/sdk/ndk/25.1.7654321",
+				"CXXFLAGS=-fdata-sections -ffunction-sections -fstack-protector-strong -funwind-tables -no-canonical-prefixes -D_FORTIFY_SOURCE=2 -fPIC -O2 -DANDROID -fsanitize=safe-stack -fstack-clash-protection -fsanitize=bounds -fsanitize-undefined-trap-on-error -Wno-macro-redefined",
+				"ANDROID_HOME=Android/sdk",
+				"CFLAGS=-fdata-sections -ffunction-sections -fstack-protector-strong -funwind-tables -no-canonical-prefixes -D_FORTIFY_SOURCE=2 -fPIC -O2 -DANDROID -fsanitize=safe-stack -fstack-clash-protection -fsanitize=bounds -fsanitize-undefined-trap-on-error -Wno-macro-redefined",
+				"ANDROID_NDK_ROOT=Android/sdk/ndk/25.1.7654321",
+				"PATH=" + fakeBinPath + ":" + os.Getenv("PATH"),
+			},
+			Argv: []string{
+				"./Configure", "no-comp", "no-dtls", "no-ec2m", "no-psk", "no-srp",
+				"no-ssl2", "no-ssl3", "no-camellia", "no-idea", "no-md2", "no-md4",
+				"no-mdc2", "no-rc2", "no-rc4", "no-rc5", "no-rmd160", "no-whirlpool",
+				"no-dso", "no-hw", "no-ui-console", "no-shared", "no-unit-test",
+				"android-x86_64", "-D__ANDROID_API__=21", "--libdir=lib", "--prefix=/", "--openssldir=/",
+			},
+		}, {
+			Env: []string{
+				"ANDROID_NDK_HOME=Android/sdk/ndk/25.1.7654321",
+				"CXXFLAGS=-fdata-sections -ffunction-sections -fstack-protector-strong -funwind-tables -no-canonical-prefixes -D_FORTIFY_SOURCE=2 -fPIC -O2 -DANDROID -fsanitize=safe-stack -fstack-clash-protection -fsanitize=bounds -fsanitize-undefined-trap-on-error -Wno-macro-redefined",
+				"ANDROID_HOME=Android/sdk",
+				"CFLAGS=-fdata-sections -ffunction-sections -fstack-protector-strong -funwind-tables -no-canonical-prefixes -D_FORTIFY_SOURCE=2 -fPIC -O2 -DANDROID -fsanitize=safe-stack -fstack-clash-protection -fsanitize=bounds -fsanitize-undefined-trap-on-error -Wno-macro-redefined",
+				"ANDROID_NDK_ROOT=Android/sdk/ndk/25.1.7654321",
+				"PATH=" + fakeBinPath + ":" + os.Getenv("PATH"),
+			},
+			Argv: []string{
+				"make", "-j", strconv.Itoa(runtime.NumCPU()),
+			},
+		}, {
+			Env: []string{},
+			Argv: []string{
+				"make",
+				"DESTDIR=" + faketopdir + "/internal/cmd/buildtool/internal/libtor/android/amd64",
+				"install_dev",
+			},
+		}, {
+			Env: []string{},
+			Argv: []string{
+				"rm", "-rf",
+				faketopdir + "/internal/cmd/buildtool/internal/libtor/android/amd64/lib/pkgconfig",
+			},
+		}},
+	}}
+
+	for _, testcase := range testcases {
+		t.Run(testcase.name, func(t *testing.T) {
+
+			cc := &buildtooltest.SimpleCommandCollector{}
+
+			deps := &buildtooltest.DependenciesCallCounter{
+				HasPsiphon: false,
+			}
+
+			shellxtesting.WithCustomLibrary(cc, func() {
+				androidCdepsBuildMain("openssl", deps)
 			})
 
 			expectCalls := map[string]int{
