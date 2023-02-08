258b8e09c9a5f2a961da3efc0ed18d8be15aad89
Simone Basso <bassosimone@gmail.com>
2023-02-08 07:01:00 +0000

    doc(model): document location.go data structure

--- internal/model/location.go
+++ internal/model/location.go
@@ -1,12 +1,22 @@
 package model
 
-// LocationProvider is an interface that returns the current location. The
-// [engine.Session] struct implements this interface.
+// LocationProvider is an interface that returns the current location.
 type LocationProvider interface {
+	// ProbeASN is the ASN associated with ProbeIP.
 	ProbeASN() uint
+
+	// ProbeASNString returns the probe ASN as the AS%d string.
 	ProbeASNString() string
+
+	// ProbeCC is the country code associated with ProbeIP.
 	ProbeCC() string
+
+	// ProbeIP is the probe IP address.
 	ProbeIP() string
+
+	// ProbeNetworkName is the name of the ProbeASN.
 	ProbeNetworkName() string
+
+	// ResolverIP is the IP of the resolver.
 	ResolverIP() string
 }
