50054f565972e51ecd4ec72f9a24fc80175317d8
Simone Basso <bassosimone@gmail.com>
2023-03-16 17:24:08 +0100

    [backport] fix(webconnectivitylte): avoid data race
    This diff backports 297360dfa007583c3fcd75bc83e516b82d93f8be.
    
    Closes https://github.com/ooni/probe/issues/2434

--- internal/experiment/webconnectivitylte/measurer.go
+++ internal/experiment/webconnectivitylte/measurer.go
@@ -37,7 +37,7 @@ func (m *Measurer) ExperimentName() string {
 
 // ExperimentVersion implements model.ExperimentMeasurer.
 func (m *Measurer) ExperimentVersion() string {
-	return "0.5.22"
+	return "0.5.23"
 }
 
 // Run implements model.ExperimentMeasurer.
