6e4dad003eccb080ed3002bb568e6aa82a9c1465
DecFox <mehul707gulati@gmail.com>
2023-04-14 23:28:10 +0530

    fix more flaky tests

--- internal/experiment/vanillator/vanillator_test.go
+++ internal/experiment/vanillator/vanillator_test.go
@@ -25,7 +25,7 @@ func TestExperimentNameAndVersion(t *testing.T) {
 	if m.ExperimentName() != "vanilla_tor" {
 		t.Fatal("invalid experiment name")
 	}
-	if m.ExperimentVersion() != "0.2.0" {
+	if m.ExperimentVersion() != "0.3.0" {
 		t.Fatal("invalid experiment version")
 	}
 }
