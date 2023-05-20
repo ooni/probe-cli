5b00fa84e5d182bcea3fd61afc4254320ec27383
Federico Ceratto <federico@debian.org>
2023-05-20 14:21:31 +0100

    Try util-linux

--- QA/rundocker.bash
+++ QA/rundocker.bash
@@ -8,7 +8,7 @@ GOVERSION=$(cat GOVERSION)
 
 cat > QA/Dockerfile << EOF
 FROM golang:$GOVERSION-alpine
-RUN apk add gcc go git musl-dev iptables tmux bind-tools curl sudo python3 setarch
+RUN apk add gcc go git musl-dev iptables tmux bind-tools curl sudo python3 util-linux
 EOF
 
 $DOCKER build -t jafar-qa ./QA/
