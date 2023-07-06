8b6ed0c4277e43cbc1191386587f236f568dd9de
Simone Basso <bassosimone@gmail.com>
2023-07-06 09:58:57 +0200

    x

--- internal/experiment/dnsping/dnsping_test.go
+++ internal/experiment/dnsping/dnsping_test.go
@@ -3,13 +3,16 @@ package dnsping
 import (
 	"context"
 	"errors"
+	"log"
+	"net"
+	"net/url"
 	"testing"
 	"time"
 
-	"github.com/ooni/netem"
-	"github.com/ooni/probe-cli/v3/internal/mocks"
+	"github.com/miekg/dns"
+	"github.com/ooni/probe-cli/v3/internal/legacy/mockable"
 	"github.com/ooni/probe-cli/v3/internal/model"
-	"github.com/ooni/probe-cli/v3/internal/netemx"
+	"github.com/ooni/probe-cli/v3/internal/runtimex"
 )
 
 func TestConfig_domains(t *testing.T) {
@@ -54,8 +57,8 @@ func TestMeasurer_run(t *testing.T) {
 		meas := &model.Measurement{
 			Input: model.MeasurementTarget(input),
 		}
-		sess := &mocks.Session{
-			MockLogger: func() model.Logger { return model.DiscardLogger },
+		sess := &mockable.Session{
+			MockableLogger: model.DiscardLogger,
 		}
 		callbacks := model.NewPrinterCallbacks(model.DiscardLogger)
 		args := &model.ExperimentArgs{
@@ -95,120 +98,67 @@ func TestMeasurer_run(t *testing.T) {
 		}
 	})
 
-	t.Run("with netem: without DPI: expect success", func(t *testing.T) {
-		// create a new test environment
-		env := netemx.NewQAEnv(netemx.QAEnvOptionDNSOverUDPResolvers("8.8.8.8"))
-		defer env.Close()
-
-		// we use the same configuration for all resolvers
-		env.AddRecordToAllResolvers(
-			"example.com",
-			"example.com", // CNAME
-			"93.184.216.34",
-		)
-
-		env.Do(func() {
-			meas, m, err := runHelper("udp://8.8.8.8:53")
-			if err != nil {
-				t.Fatalf("Unexpected error: %s", err)
-			}
-
-			tk, _ := (meas.TestKeys).(*TestKeys)
-			if len(tk.Pings) != expectedPings*2 { // account for A & AAAA pings
-				t.Fatal("unexpected number of pings", len(tk.Pings))
-			}
-
-			ask, err := m.GetSummaryKeys(meas)
-			if err != nil {
-				t.Fatal("cannot obtain summary")
-			}
-			summary := ask.(SummaryKeys)
-			if summary.IsAnomaly {
-				t.Fatal("expected no anomaly")
-			}
-
-			for _, p := range tk.Pings {
-				if p.Query == nil {
-					t.Fatal("QUery should not be nil")
-				}
-				t.Logf("%+v", p.Query)
-				if p.Query.Answers == nil {
-					t.Fatal("p.Query.Answers should not be nil")
-				}
-				if p.Query.QueryType == "A" && p.Query.Failure != nil {
-					t.Fatal("unexpected error", *p.Query.Failure)
-				}
-			}
-		})
+	t.Run("with local listener", func(t *testing.T) {
+		srvrURL, dnsListener, err := startDNSServer()
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer dnsListener.Close()
+		meas, m, err := runHelper(srvrURL)
+		if err != nil {
+			t.Fatal(err)
+		}
+		tk := meas.TestKeys.(*TestKeys)
+		if len(tk.Pings) != expectedPings*2 { // account for A & AAAA pings
+			t.Fatal("unexpected number of pings")
+		}
+		ask, err := m.GetSummaryKeys(meas)
+		if err != nil {
+			t.Fatal("cannot obtain summary")
+		}
+		summary := ask.(SummaryKeys)
+		if summary.IsAnomaly {
+			t.Fatal("expected no anomaly")
+		}
 	})
+}
 
-	t.Run("with netem: with DNS spoofing: expect to see delayed responses", func(t *testing.T) {
-		dumper := netem.NewPCAPDumper("dnsping_delayed.pcap", model.DiscardLogger)
+// startDNSServer starts a local DNS server.
+func startDNSServer() (string, net.PacketConn, error) {
+	dnsListener, err := net.ListenPacket("udp", "127.0.0.1:0")
+	if err != nil {
+		return "", nil, err
+	}
+	go runDNSServer(dnsListener)
+	URL := &url.URL{
+		Scheme: "udp",
+		Host:   dnsListener.LocalAddr().String(),
+		Path:   "/",
+	}
+	return URL.String(), dnsListener, nil
+}
 
-		// create a new test environment
-		env := netemx.NewQAEnv(
-			netemx.QAEnvOptionDNSOverUDPResolvers("8.8.8.8"),
-			netemx.QAEnvOptionClientPCAPDumper(dumper),
-		)
-		defer env.Close()
+// runDNSServer runs the DNS server.
+func runDNSServer(dnsListener net.PacketConn) {
+	ds := &dns.Server{
+		Handler:    &dnsHandler{},
+		Net:        "udp",
+		PacketConn: dnsListener,
+	}
+	err := ds.ActivateAndServe()
+	if !errors.Is(err, net.ErrClosed) {
+		runtimex.PanicOnError(err, "ActivateAndServe failed")
+	}
+}
 
-		// we use the same configuration for all resolvers
-		env.AddRecordToAllResolvers(
-			"example.com",
-			"example.com", // CNAME
-			"93.184.216.34",
-		)
+// dnsHandler handles DNS requests.
+type dnsHandler struct{}
 
-		// use DPI to create DNS spoofing
-		dpi := env.DPIEngine()
-		dpi.AddRule(&netem.DPISpoofDNSResponse{
-			Addresses: []string{
-				"10.10.34.35",
-				"10.10.34.36",
-			},
-			Logger: model.DiscardLogger,
-			Domain: "example.com",
-		})
-
-		env.Do(func() {
-			meas, m, err := runHelper("udp://8.8.8.8:53")
-			if err != nil {
-				t.Fatalf("Unexpected error: %s", err)
-			}
-
-			tk, _ := (meas.TestKeys).(*TestKeys)
-			if len(tk.Pings) != expectedPings*2 { // account for A & AAAA pings
-				t.Fatal("unexpected number of pings", len(tk.Pings))
-			}
-
-			// note: this experiment does not set anomaly but we still want
-			// to have a test here for when we possibly will
-			ask, err := m.GetSummaryKeys(meas)
-			if err != nil {
-				t.Fatal("cannot obtain summary")
-			}
-			summary := ask.(SummaryKeys)
-			if summary.IsAnomaly {
-				t.Fatal("expected no anomaly")
-			}
-
-			for _, p := range tk.Pings {
-				if p.Query == nil {
-					t.Fatal("QUery should not be nil")
-				}
-				//t.Logf("%+v", *p.Query.Failure)
-				t.Logf("%+v", p.Query)
-				if p.Query.Answers == nil {
-					t.Fatal("p.Query.Answers should not be nil")
-				}
-				if p.Query.QueryType == "A" && p.Query.Failure != nil {
-					t.Fatal("unexpected error", *p.Query.Failure)
-				}
-
-				if len(p.DelayedResponses) < 1 {
-					t.Fatal("expected to see delayed responses, found nothing")
-				}
-			}
-		})
-	})
+// ServeDNS serves a DNS request
+func (h *dnsHandler) ServeDNS(rw dns.ResponseWriter, req *dns.Msg) {
+	m := new(dns.Msg)
+	m.Compress = true
+	m.MsgHdr.RecursionAvailable = true
+	m.SetRcode(req, dns.RcodeServerFailure)
+	rw.WriteMsg(m)
 }
