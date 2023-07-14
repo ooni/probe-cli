55936f35c5820081c5b807c6a37ca7293c6a648b
Simone Basso <bassosimone@gmail.com>
2023-07-14 02:34:14 +0200

    fix: just return a string containing the serialized JSON
    This seems better for @aanorbel.

--- pkg/oonimkall/xoonirun_test.go
+++ pkg/oonimkall/xoonirun_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/ooni/probe-cli/v3/internal/runtimex"
 )
 
@@ -14,13 +15,44 @@ func TestXOONIRun(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		resp, err := sess.OONIRunFetch(sess.NewContext(), 297500125102)
+		rawResp, err := sess.OONIRunFetch(sess.NewContext(), 297500125102)
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		data := runtimex.Try1(json.Marshal(resp))
-		t.Log(string(data))
-	})
+		expect := map[string]any{
+			"creation_time": "2023-06-06T09:19:41Z",
+			"descriptor": map[string]any{
+				"author":           "ooni",
+				"description":      "Check whether [WhatsApp](https://ooni.org/nettest/whatsapp/) is blocked",
+				"description_intl": map[string]any{},
+				"icon":             "Md123",
+				"name":             "Updated 2 Instant Messaging",
+				"name_intl": map[string]any{
+					"it": "Instant Messaging IT",
+				},
+				"nettests": []any{
+					map[string]any{
+						"backend_options":           map[string]any{},
+						"inputs":                    []any{},
+						"is_background_run_enabled": true,
+						"is_manual_run_enabled":     false,
+						"options":                   map[string]any{},
+						"test_name":                 "whatsapp",
+					},
+				},
+				"short_description":      "Test the blocking of instant messaging apps",
+				"short_description_intl": map[string]any{},
+			},
+			"v": 1.0,
+		}
 
+		var got map[string]any
+		runtimex.Try0(json.Unmarshal([]byte(rawResp), &got))
+		t.Log(got)
+
+		if diff := cmp.Diff(expect, got); diff != "" {
+			t.Fatal(diff)
+		}
+	})
 }
