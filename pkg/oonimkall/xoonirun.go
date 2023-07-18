8372f47bfd770fc5adf5ff8628fe77daf3a2a649
Simone Basso <bassosimone@gmail.com>
2023-07-18 15:44:43 +0000

    feat: 100% testing for xoonirun

--- pkg/oonimkall/xoonirun.go
+++ pkg/oonimkall/xoonirun.go
@@ -14,13 +14,24 @@ import (
 )
 
 // OONIRunFetch fetches a given OONI run descriptor.
+//
+// The ID argument is the unique identifier of the OONI Run link. For example, in:
+//
+//	https://api.ooni.io/api/_/ooni_run/fetch/297500125102
+//
+// The OONI Run link ID is 297500125102.
+//
+// Warning: this API is currently experimental and we only expose it to facilitate
+// developing OONI Run v2. Do not use this API in production.
 func (sess *Session) OONIRunFetch(ctx *Context, ID int64) (string, error) {
 	sess.mtx.Lock()
 	defer sess.mtx.Unlock()
 
-	clnt := sess.sessp.DefaultHTTPClient()
+	// TODO(bassosimone): this code should be changed to use the probeservices.Client
+	// rather than using an hardcoded URL once we switch to production code. Until then,
+	// we are going to use the test backend server.
 
-	// https://ams-pg-test.ooni.org/api/_/ooni_run/fetch/297500125102
+	// For example: https://ams-pg-test.ooni.org/api/_/ooni_run/fetch/297500125102
 	URL := &url.URL{
 		Scheme:      "https",
 		Opaque:      "",
@@ -34,6 +45,12 @@ func (sess *Session) OONIRunFetch(ctx *Context, ID int64) (string, error) {
 		Fragment:    "",
 		RawFragment: "",
 	}
+
+	return sess.ooniRunFetchWithURLLocked(ctx, URL)
+}
+
+func (sess *Session) ooniRunFetchWithURLLocked(ctx *Context, URL *url.URL) (string, error) {
+	clnt := sess.sessp.DefaultHTTPClient()
 
 	req, err := http.NewRequestWithContext(ctx.ctx, "GET", URL.String(), nil)
 	if err != nil {
--- pkg/oonimkall/xoonirun_test.go
+++ pkg/oonimkall/xoonirun_test.go
@@ -2,46 +2,50 @@ package oonimkall_test
 
 import (
 	"encoding/json"
+	"net"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/ooni/probe-cli/v3/internal/netxlite/filtering"
 	"github.com/ooni/probe-cli/v3/internal/runtimex"
 )
 
-func TestXOONIRun(t *testing.T) {
-	t.Run("smoke testing of the fetch API", func(t *testing.T) {
+func TestOONIRunFetch(t *testing.T) {
+	t.Run("we can fetch a OONI Run link descriptor", func(t *testing.T) {
 		sess, err := NewSessionForTesting()
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		rawResp, err := sess.OONIRunFetch(sess.NewContext(), 297500125102)
+		rawResp, err := sess.OONIRunFetch(sess.NewContext(), 9408643002)
 		if err != nil {
 			t.Fatal(err)
 		}
 
 		expect := map[string]any{
-			"creation_time": "2023-06-06T09:19:41Z",
+			"creation_time": "2023-07-18T15:38:21Z",
 			"descriptor": map[string]any{
-				"author":           "ooni",
-				"description":      "Check whether [WhatsApp](https://ooni.org/nettest/whatsapp/) is blocked",
+				"author":           "simone@openobservatory.org",
+				"description":      "We use this OONI Run descriptor for writing integration tests for ooni/probe-cli/v3/pkg/oonimkall.",
 				"description_intl": map[string]any{},
-				"icon":             "Md123",
-				"name":             "Updated 2 Instant Messaging",
-				"name_intl": map[string]any{
-					"it": "Instant Messaging IT",
-				},
+				"icon":             "",
+				"name":             "OONIMkAll Integration Testing",
+				"name_intl":        map[string]any{},
 				"nettests": []any{
 					map[string]any{
 						"backend_options":           map[string]any{},
-						"inputs":                    []any{},
-						"is_background_run_enabled": true,
+						"inputs":                    []any{string("https://www.example.com/")},
+						"is_background_run_enabled": false,
 						"is_manual_run_enabled":     false,
 						"options":                   map[string]any{},
-						"test_name":                 "whatsapp",
+						"test_name":                 "web_connectivity",
 					},
 				},
-				"short_description":      "Test the blocking of instant messaging apps",
+				"short_description":      "Integration testing descriptor for ooni/probe-cli/v3/pkg/oonimkall.",
 				"short_description_intl": map[string]any{},
 			},
 			"v": 1.0,
@@ -53,6 +57,104 @@ func TestXOONIRun(t *testing.T) {
 
 		if diff := cmp.Diff(expect, got); diff != "" {
 			t.Fatal(diff)
+		}
+	})
+
+	t.Run("we handle the case where the URL is invalid", func(t *testing.T) {
+		sess, err := NewSessionForTesting()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		URL := &url.URL{Host: "\t"} // this URL is invalid
+
+		rawResp, err := sess.OONIRunFetchWithURL(sess.NewContext(), URL)
+		if !strings.HasSuffix(err.Error(), `invalid URL escape "%09"`) {
+			t.Fatal("unexpected error", err)
+		}
+		if rawResp != "" {
+			t.Fatal("expected empty raw response")
+		}
+	})
+
+	t.Run("we handle the case where the response body is not 200", func(t *testing.T) {
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusBadGateway)
+		}))
+		defer server.Close()
+
+		URL := runtimex.Try1(url.Parse(server.URL))
+
+		sess, err := NewSessionForTesting()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rawResp, err := sess.OONIRunFetchWithURL(sess.NewContext(), URL)
+		if !strings.HasSuffix(err.Error(), "HTTP request failed") {
+			t.Fatal("unexpected error", err)
+		}
+		if rawResp != "" {
+			t.Fatal("expected empty raw response")
+		}
+	})
+
+	t.Run("we handle the case where the HTTP round trip fails", func(t *testing.T) {
+		// Implementation note: because we need to backport this patch to the release/3.18
+		// branch, it would be quite verbose and burdensome use netem to implement this test,
+		// since release/3.18 is lagging behind from master in terms of netemx.
+		server := filtering.NewTLSServer(filtering.TLSActionReset)
+		defer server.Close()
+
+		URL := &url.URL{
+			Scheme: "https",
+			Host:   server.Endpoint(),
+			Path:   "/",
+		}
+
+		sess, err := NewSessionForTesting()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rawResp, err := sess.OONIRunFetchWithURL(sess.NewContext(), URL)
+		if !strings.HasSuffix(err.Error(), "connection_reset") {
+			t.Fatal("unexpected error", err)
+		}
+		if rawResp != "" {
+			t.Fatal("expected empty raw response")
+		}
+	})
+
+	t.Run("we handle the case when reading the response body fails", func(t *testing.T) {
+		// Implementation note: because we need to backport this patch to the release/3.18
+		// branch, it would be quite verbose and burdensome use netem to implement this test,
+		// since release/3.18 is lagging behind from master in terms of netemx.
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("{"))
+			hijacker := w.(http.Hijacker)
+			conn, _, err := hijacker.Hijack()
+			runtimex.PanicOnError(err, "hijacker.Hijack failed")
+			if tc, ok := conn.(*net.TCPConn); ok {
+				tc.SetLinger(0)
+			}
+			conn.Close()
+		}))
+		defer server.Close()
+
+		URL := runtimex.Try1(url.Parse(server.URL))
+
+		sess, err := NewSessionForTesting()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rawResp, err := sess.OONIRunFetchWithURL(sess.NewContext(), URL)
+		if !strings.HasSuffix(err.Error(), "connection_reset") {
+			t.Fatal("unexpected error", err)
+		}
+		if rawResp != "" {
+			t.Fatal("expected empty raw response")
 		}
 	})
 }
