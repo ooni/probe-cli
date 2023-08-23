8426d3ab63a2e5678c11d37a0c89b4bb5d7a1bc0
Simone Basso <bassosimone@gmail.com>
2023-08-23 18:13:14 +0200

    feat(netxlite): construct types with custom UnderlyingNetwork (#1197)
    This diff introduces the netxlite.Netx type. This type depends on a
    model.UnderlyingNetwork. When the underlying network is nil, using this
    type is equivalent to using netxlite toplevel functions directly.
    
    To simplify testing this diff, let's move the netem->netxlite
    UnderlyingNetworkAdapter from netemx and let's use netem to write tests,
    so we can have confidence specifically about the most common use case of
    overriding the underlying network.
    
    Originally part of https://github.com/ooni/probe-cli/pull/1185.
    
    The reference issue is https://github.com/ooni/probe/issues/2461.
    
    ---------
    
    Co-authored-by: kelmenhorst <k.elmenhorst@mailbox.org>

--- internal/netemx/adapter.go
+++ internal/netemx/adapter.go
@@ -5,53 +5,12 @@ package netemx
 //
 
 import (
-	"context"
-	"crypto/x509"
-	"net"
-	"time"
-
 	"github.com/ooni/netem"
-	"github.com/ooni/probe-cli/v3/internal/model"
 	"github.com/ooni/probe-cli/v3/internal/netxlite"
-	"github.com/ooni/probe-cli/v3/internal/runtimex"
 )
 
 // WithCustomTProxy executes the given function using the given [netem.UnderlyingNetwork]
 // as the [model.UnderlyingNetwork] used by the [netxlite] package.
 func WithCustomTProxy(tproxy netem.UnderlyingNetwork, function func()) {
-	netxlite.WithCustomTProxy(&adapter{tproxy}, function)
-}
-
-// adapter adapts [netem.UnderlyingNetwork] to [model.UnderlyingNetwork].
-type adapter struct {
-	tp netem.UnderlyingNetwork
-}
-
-var _ model.UnderlyingNetwork = &adapter{}
-
-// DefaultCertPool implements model.UnderlyingNetwork
-func (a *adapter) DefaultCertPool() *x509.CertPool {
-	return runtimex.Try1(a.tp.DefaultCertPool())
-}
-
-// DialContext implements model.UnderlyingNetwork
-func (a *adapter) DialContext(ctx context.Context, timeout time.Duration, network string, address string) (net.Conn, error) {
-	ctx, cancel := context.WithTimeout(ctx, timeout)
-	defer cancel()
-	return a.tp.DialContext(ctx, network, address)
-}
-
-// GetaddrinfoLookupANY implements model.UnderlyingNetwork
-func (a *adapter) GetaddrinfoLookupANY(ctx context.Context, domain string) ([]string, string, error) {
-	return a.tp.GetaddrinfoLookupANY(ctx, domain)
-}
-
-// GetaddrinfoResolverNetwork implements model.UnderlyingNetwork
-func (a *adapter) GetaddrinfoResolverNetwork() string {
-	return a.tp.GetaddrinfoResolverNetwork()
-}
-
-// ListenUDP implements model.UnderlyingNetwork
-func (a *adapter) ListenUDP(network string, addr *net.UDPAddr) (model.UDPLikeConn, error) {
-	return a.tp.ListenUDP(network, addr)
+	netxlite.WithCustomTProxy(&netxlite.NetemUnderlyingNetworkAdapter{UNet: tproxy}, function)
 }
