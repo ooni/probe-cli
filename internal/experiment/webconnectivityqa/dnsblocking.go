f133d9ad1830702d151f6c28a5a5f28138aeb35b
Simone Basso <bassosimone@gmail.com>
2023-08-24 23:34:09 +0200

    x

--- internal/experiment/webconnectivityqa/dnsblocking.go
+++ internal/experiment/webconnectivityqa/dnsblocking.go
@@ -12,7 +12,11 @@ func dnsBlockingAndroidDNSCacheNoData() *TestCase {
 		Flags: TestCaseFlagNoV04,
 		Input: "https://www.example.com/",
 		Configure: func(env *netemx.QAEnv) {
+			// make sure the env knows we want to emulate our getaddrinfo wrapper behavior
 			env.EmulateAndroidGetaddrinfo(true)
+
+			// remove the record so that the DNS query returns NXDOMAIN, which is then
+			// converted into android_dns_cache_no_data by the emulation layer
 			env.ISPResolverConfig().RemoveRecord("www.example.com")
 		},
 		ExpectErr:      false,
