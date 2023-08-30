7592b1747c3bdfe426336dd86187eed1609053a4
kelmenhorst <k.elmenhorst@mailbox.org>
2023-08-30 10:42:56 -0400

    nettests echcheck: use default input

--- cmd/ooniprobe/internal/nettests/echcheck.go
+++ cmd/ooniprobe/internal/nettests/echcheck.go
@@ -3,14 +3,12 @@ package nettests
 // ECHCheck nettest implementation.
 type ECHCheck struct{}
 
-// we use single input as an experimental setup to collect the first baseline measurements
-var exampleInput = []string{"https://www.example.com"}
-
 // Run starts the nettest.
 func (n ECHCheck) Run(ctl *Controller) error {
 	builder, err := ctl.Session.NewExperimentBuilder("echcheck")
 	if err != nil {
 		return err
 	}
-	return ctl.Run(builder, exampleInput)
+	// providing empty input prompts the experiment to use the default URL https://example.org
+	return ctl.Run(builder, []string{})
 }
