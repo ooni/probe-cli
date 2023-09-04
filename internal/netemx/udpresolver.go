f0619b67d05cc06f763baefff17bdb8b75cbd610
Simone Basso <bassosimone@gmail.com>
2023-09-04 23:43:27 +0200

    feat(netemx): implement NetStackServerFactory for DNS-over-UDP (#1225)
    This diff moves forward the work we're doing to try to unify the way in
    which all the possible servers in netemx are constructed.
    
    The general idea is to enable configuring every kind of server using the
    QAEnvOptionNetStack option.
    
    To this end, we need to implement NetStackServerFactory.
    
    In this diff, in particular, we're implementing a NetStackServerFactory
    for constructing DNS-over-UDP servers. These servers would use:
    
    1. the OtherResolversConfig for lookups (which makes sense because the
    [MustNewQAEnv] internally constructs the ISP resolver);
    
    2. the logger configured for the QAEnv.
    
    ## Checklist
    
    - [x] I have read the [contribution
    guidelines](https://github.com/ooni/probe-cli/blob/master/CONTRIBUTING.md)
    - [x] reference issue for this pull request:
    https://github.com/ooni/probe/issues/1803
    - [x] if you changed anything related to how experiments work and you
    need to reflect these changes in the ooni/spec repository, please link
    to the related ooni/spec pull request: N/A
    - [x] if you changed code inside an experiment, make sure you bump its
    version number: N/A

---
+++ internal/netemx/udpresolver.go
@@ -0,0 +1,90 @@
+package netemx
+
+import (
+	"fmt"
+	"io"
+	"sync"
+
+	"github.com/ooni/netem"
+	"github.com/ooni/probe-cli/v3/internal/logx"
+	"github.com/ooni/probe-cli/v3/internal/model"
+	"github.com/ooni/probe-cli/v3/internal/runtimex"
+)
+
+// UDPResolverFactory implements [NetStackServerFactory] for DNS-over-UDP servers.
+//
+// When this factory constructs a [NetStackServer], it will use:
+//
+// 1. the [NetStackServerFactoryEnv.OtherResolversConfig] as DNS configuration;
+//
+// 2. the [NetStackServerFactoryEnv.Logger] as the logger.
+//
+// Use this factory along with [QAEnvOptionNetStack] to create DNS-over-UDP servers.
+type UDPResolverFactory struct{}
+
+var _ NetStackServerFactory = &UDPResolverFactory{}
+
+// MustNewServer implements NetStackServerFactory.
+func (f *UDPResolverFactory) MustNewServer(env NetStackServerFactoryEnv, stack *netem.UNetStack) NetStackServer {
+	return udpResolverMustNewServer(env.OtherResolversConfig(), env.Logger(), stack)
+}
+
+// udpResolverMustNewServer is an internal factory for creating a [NetStackServer] that
+// runs a DNS-over-UDP server using the configured logger, DNS config, and stack.
+func udpResolverMustNewServer(config *netem.DNSConfig, logger model.Logger, stack *netem.UNetStack) NetStackServer {
+	return &udpResolver{
+		closers: []io.Closer{},
+		config:  config,
+		logger:  logger,
+		mu:      sync.Mutex{},
+		unet:    stack,
+	}
+}
+
+type udpResolver struct {
+	closers []io.Closer
+	config  *netem.DNSConfig
+	logger  model.Logger
+	mu      sync.Mutex
+	unet    *netem.UNetStack
+}
+
+// Close implements NetStackServer.
+func (srv *udpResolver) Close() error {
+	// make the method locked as requested by the documentation
+	defer srv.mu.Unlock()
+	srv.mu.Lock()
+
+	// close each of the closers
+	for _, closer := range srv.closers {
+		_ = closer.Close()
+	}
+
+	// be idempotent
+	srv.closers = []io.Closer{}
+	return nil
+}
+
+// MustStart implements NetStackServer.
+func (srv *udpResolver) MustStart() {
+	// make the method locked as requested by the documentation
+	defer srv.mu.Unlock()
+	srv.mu.Lock()
+
+	// Use a prefix logger for the DNS server
+	prefixLogger := &logx.PrefixLogger{
+		Prefix: fmt.Sprintf("%-16s", "RESOLVER"),
+		Logger: srv.logger,
+	}
+
+	// create DNS server
+	server := runtimex.Try1(netem.NewDNSServer(
+		prefixLogger,
+		srv.unet,
+		srv.unet.IPAddress(),
+		srv.config,
+	))
+
+	// track this closable
+	srv.closers = append(srv.closers, server)
+}
