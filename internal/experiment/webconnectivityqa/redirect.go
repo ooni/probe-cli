a91126978547b5a404c2f9f39034475e9c6e7fc2
Simone Basso <bassosimone@gmail.com>
2023-09-06 08:25:35 +0200

    feat(webconnectivityqa): convert more jafar test cases (#1241)
    ## Checklist
    
    - [x] I have read the [contribution
    guidelines](https://github.com/ooni/probe-cli/blob/master/CONTRIBUTING.md)
    - [x] reference issue for this pull request:
    https://github.com/ooni/probe/issues/1803
    - [x] if you changed anything related to how experiments work and you
    need to reflect these changes in the ooni/spec repository, please link
    to the related ooni/spec pull request: N/A
    - [x] if you changed code inside an experiment, make sure you bump its
    version number: N/A
    
    ## Description
    
    This diff moves test cases from QA/webconnectivity.py to the
    ./internal/webconnectivityqa package.

--- internal/experiment/webconnectivityqa/redirect.go
+++ internal/experiment/webconnectivityqa/redirect.go
@@ -188,3 +188,157 @@ func redirectWithConsistentDNSAndThenNXDOMAIN() *TestCase {
 		},
 	}
 }
+
+// redirectWithConsistentDNSAndThenEOFForHTTP is a scenario where the redirect
+// works but then there's connection EOF for an HTTP URL.
+func redirectWithConsistentDNSAndThenEOFForHTTP() *TestCase {
+	return &TestCase{
+		Name:  "redirectWithConsistentDNSAndThenEOFForHTTP",
+		Flags: 0,
+		Input: "https://bit.ly/32447",
+		Configure: func(env *netemx.QAEnv) {
+
+			// make sure we cannot HTTP round trip
+			env.DPIEngine().AddRule(&netem.DPICloseConnectionForString{
+				Logger:          log.Log,
+				ServerIPAddress: netemx.AddressWwwExampleCom,
+				ServerPort:      80,
+				String:          "www.example.com",
+			})
+
+			// make sure we cannot TLS handshake
+			env.DPIEngine().AddRule(&netem.DPICloseConnectionForTLSSNI{
+				Logger: log.Log,
+				SNI:    "www.example.com",
+			})
+
+		},
+		ExpectErr: false,
+		ExpectTestKeys: &testKeys{
+			DNSExperimentFailure:  nil,
+			DNSConsistency:        "consistent",
+			HTTPExperimentFailure: "eof_error",
+			XStatus:               8448, // StatusExperimentHTTP | StatusAnomalyReadWrite
+			XDNSFlags:             0,
+			XBlockingFlags:        8, // analysisFlagHTTPBlocking
+			Accessible:            false,
+			Blocking:              "http-failure",
+		},
+	}
+}
+
+// redirectWithConsistentDNSAndThenEOFForHTTPS is a scenario where the redirect
+// works but then there's connection EOF for an HTTPS URL.
+func redirectWithConsistentDNSAndThenEOFForHTTPS() *TestCase {
+	return &TestCase{
+		Name:  "redirectWithConsistentDNSAndThenEOFForHTTPS",
+		Flags: TestCaseFlagNoLTE, // BUG: LTE thinks this website is accessible (WTF?!)
+		Input: "https://bit.ly/21645",
+		Configure: func(env *netemx.QAEnv) {
+
+			// make sure we cannot connect to the example domain on port 80
+			env.DPIEngine().AddRule(&netem.DPICloseConnectionForString{
+				Logger:          log.Log,
+				ServerIPAddress: netemx.AddressWwwExampleCom,
+				ServerPort:      80,
+				String:          "www.example.com",
+			})
+
+			// make sure we cannot connect to the example domain on port 443
+			env.DPIEngine().AddRule(&netem.DPICloseConnectionForTLSSNI{
+				Logger: log.Log,
+				SNI:    "www.example.com",
+			})
+
+		},
+		ExpectErr: false,
+		ExpectTestKeys: &testKeys{
+			DNSExperimentFailure:  nil,
+			DNSConsistency:        "consistent",
+			HTTPExperimentFailure: "eof_error",
+			XStatus:               8448, // StatusExperimentHTTP | StatusAnomalyReadWrite
+			XDNSFlags:             0,
+			XBlockingFlags:        32, // analysisFlagSuccess
+			Accessible:            false,
+			Blocking:              "http-failure",
+		},
+	}
+}
+
+// redirectWithConsistentDNSAndThenTimeoutForHTTP is a scenario where the redirect
+// works but then there's connection refused for an HTTP URL.
+func redirectWithConsistentDNSAndThenTimeoutForHTTP() *TestCase {
+	return &TestCase{
+		Name:     "redirectWithConsistentDNSAndThenTimeoutForHTTP",
+		Flags:    0,
+		Input:    "https://bit.ly/32447",
+		LongTest: true,
+		Configure: func(env *netemx.QAEnv) {
+
+			// make sure we cannot perform the round trip
+			env.DPIEngine().AddRule(&netem.DPIDropTrafficForString{
+				Logger:          log.Log,
+				ServerIPAddress: netemx.AddressWwwExampleCom,
+				ServerPort:      80,
+				String:          "www.example.com",
+			})
+
+			// make sure we cannot TLS handshake
+			env.DPIEngine().AddRule(&netem.DPIDropTrafficForTLSSNI{
+				Logger: log.Log,
+				SNI:    "www.example.com",
+			})
+
+		},
+		ExpectErr: false,
+		ExpectTestKeys: &testKeys{
+			DNSExperimentFailure:  nil,
+			DNSConsistency:        "consistent",
+			HTTPExperimentFailure: "generic_timeout_error",
+			XStatus:               8704, // StatusExperimentHTTP | StatusAnomalyUnknown
+			XDNSFlags:             0,
+			XBlockingFlags:        8, // analysisFlagHTTPBlocking
+			Accessible:            false,
+			Blocking:              "http-failure",
+		},
+	}
+}
+
+// redirectWithConsistentDNSAndThenTimeoutForHTTPS is a scenario where the redirect
+// works but then there's connection refused for an HTTPS URL.
+func redirectWithConsistentDNSAndThenTimeoutForHTTPS() *TestCase {
+	return &TestCase{
+		Name:     "redirectWithConsistentDNSAndThenTimeoutForHTTPS",
+		Flags:    TestCaseFlagNoLTE, // BUG: LTE thinks this website is accessible (WTF?!)
+		Input:    "https://bit.ly/21645",
+		LongTest: true,
+		Configure: func(env *netemx.QAEnv) {
+
+			// make sure we cannot HTTP round trip
+			env.DPIEngine().AddRule(&netem.DPIDropTrafficForString{
+				Logger:          log.Log,
+				ServerIPAddress: netemx.AddressWwwExampleCom,
+				ServerPort:      80,
+				String:          "www.example.com",
+			})
+
+			// make sure we cannot TLS handshake
+			env.DPIEngine().AddRule(&netem.DPIDropTrafficForTLSSNI{
+				Logger: log.Log,
+				SNI:    "www.example.com",
+			})
+
+		},
+		ExpectErr: false,
+		ExpectTestKeys: &testKeys{
+			DNSExperimentFailure:  nil,
+			DNSConsistency:        "consistent",
+			HTTPExperimentFailure: "generic_timeout_error",
+			XStatus:               8704, // StatusExperimentHTTP | StatusAnomalyUnknown
+			XDNSFlags:             0,
+			XBlockingFlags:        32, // analysisFlagSuccess
+			Accessible:            false,
+			Blocking:              "http-failure",
+		},
+	}
+}
