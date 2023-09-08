35f315b1a1610a6220885f3556eb39ed350c9f8b
kelmenhorst <k.elmenhorst@mailbox.org>
2023-09-08 15:12:18 -0300

    fix: QAEnv config names, remove netemx "success" case

--- internal/experiment/echcheck/measure_test.go
+++ internal/experiment/echcheck/measure_test.go
@@ -23,14 +23,15 @@ func TestNewExperimentMeasurer(t *testing.T) {
 func qaenv() *netemx.QAEnv {
 	cfg := []*netemx.ScenarioDomainAddresses{
 		{
-			Domain:    "crypto.cloudflare.com",
-			Addresses: []string{"130.192.91.7"},
-			Role:      netemx.ScenarioRoleExampleLikeWebServer,
+			Domains:          []string{"crypto.cloudflare.com"},
+			Addresses:        []string{"130.192.91.7"},
+			Role:             netemx.ScenarioRoleWebServer,
+			WebServerFactory: netemx.ExampleWebPageHandlerFactory(),
 		},
 		{
-			Domain:    "mozilla.cloudflare-dns.com",
+			Domains:   []string{"mozilla.cloudflare-dns.com"},
 			Addresses: []string{"130.192.91.13"},
-			Role:      netemx.ScenarioRoleDNSOverHTTPS,
+			Role:      netemx.ScenarioRolePublicDNS,
 		},
 	}
 	return netemx.MustNewScenario(cfg)
@@ -87,44 +88,6 @@ func TestMeasurerMeasureWithInvalidInput(t *testing.T) {
 	if err.Error() != "input is not an URL" {
 		t.Fatal("unexpected error type")
 	}
-}
-
-// TODO(bassosimone,kelmenhorst): this test currently fails because netem does not support ECH; we
-// should consider implementing ECH such that this experiment succeeds in a proper sense.
-func TestMeasurerNetemWithoutInterference(t *testing.T) {
-	// create QAEnv
-	env := qaenv()
-	defer env.Close()
-
-	env.Do(func() {
-		// create measurer
-		measurer := NewExperimentMeasurer(Config{})
-		msrmnt := &model.Measurement{}
-		args := &model.ExperimentArgs{
-			Callbacks:   model.NewPrinterCallbacks(model.DiscardLogger),
-			Measurement: msrmnt,
-			Session:     &mocks.Session{MockLogger: func() model.Logger { return model.DiscardLogger }},
-		}
-
-		// run measurement
-		err := measurer.Run(context.Background(), args)
-		if err != nil {
-			t.Fatal("unexpected error", err)
-		}
-		summary, err := measurer.GetSummaryKeys(&model.Measurement{})
-		if err != nil {
-			t.Fatal("unexpected error GetSummaryKeys", err)
-		}
-		if summary.(SummaryKeys).IsAnomaly != false {
-			t.Fatal("expected false")
-		}
-		tk := msrmnt.TestKeys.(TestKeys)
-		if tk.Control.Failure == nil || tk.Target.Failure == nil {
-			// we expect errors here because we cannot do ECH with netem yet
-			t.Fatal("expected an error here")
-		}
-		t.Skip("netem does not support ECH so we cannot declare this test as successful")
-	})
 }
 
 func TestMeasurementSuccessRealWorld(t *testing.T) {
