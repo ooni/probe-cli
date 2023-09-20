dff7cedd246f05f457c083cf247d86bb43fca798
Simone Basso <bassosimone@gmail.com>
2023-09-20 12:08:44 +0200

    feat(enginenetx): add configurable HTTPS dialer (#1283)
    This commit introduces a configurable HTTPS dialer that we will use for
    implementing beacons, and possibly also other functionality.
    
    We need to perform TCP connect and TLS handshake as part of the same
    goroutine, because we cannot consider a dialing attempt successful after
    a successful TCP connect. Due to network interference, the dialing may
    also fail later during the TLS handshake.
    
    To support several possible HTTPS dialing strategies, we need to extend
    what happens during a LookupHost operation. Generally, one would like to
    have addresses to dial. Rather, here we have tactics, where a single IP
    address MAY be included into more than a single tactic, if we need to
    try different tricks with the same address.
    
    Also, tactics include a delay, which is useful to (a) avoid performing
    all operations in parallel, which is not gentle towards otherwise
    perfectly functioning networks and (b) give penalty to tactics that
    utilize circumvention, such that we don't even attempt them unless we
    need to.
    
    In turn, the DNS resolver is wrapped by a policy for configuring TLS
    dialing. Basically, the policy observes the IP addresses returned by an
    underlying resolver and then it will decide which tactics to produce
    based on that. Note that the policy could also extend the set of
    returned IP addresses when the domain for which we connect is such that
    we have known IP addresses in advance for such a domain.
    
    The default policy we introduce in this commit behaves as follows:
    
    1. it asks the engine to create 16 goroutines for dialing;
    
    2. it uses the DNS lookup results w/o adding any extra IP addr;
    
    3. it produces a tactic for each IP address where we use the domain as
    the SNI and we add a 300 millisecond delay to the second tactic, 600 to
    the third, and so on--which is similar to implementing happy eyeballs.
    
    It's also worth noting that, tactics MAY override the TLS handshaker
    being used (for example, to use uTLS) and, also, because they may use
    different SNIs, the TLS verification is performed AFTER the TLS
    handshake. (Because I set `InsecureSkipVerify`, I expected--or rather
    *demand*--GitHub to produce a security warning for this commit, which I
    will mark as a false positive, because TLS verification is performed a
    few lines of code after that.)
    
    As far as the algorithm for verification is concerned, it comes from the
    Go examples and it matches closely the code used by the standard library
    to perform verification. More details about this in comments in the
    commit code.
    
    Part of https://github.com/ooni/probe/issues/2531

---
+++ internal/enginenetx/httpsdialer_test.go
@@ -0,0 +1,424 @@
+package enginenetx_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/apex/log"
+	"github.com/google/go-cmp/cmp"
+	"github.com/ooni/netem"
+	"github.com/ooni/probe-cli/v3/internal/enginenetx"
+	"github.com/ooni/probe-cli/v3/internal/model"
+	"github.com/ooni/probe-cli/v3/internal/netemx"
+	"github.com/ooni/probe-cli/v3/internal/netxlite"
+)
+
+// Flags controlling when [httpsDialerPolicyCancelingContext] cancels the context
+const (
+	httpsDialerPolicyCancelingContextOnStarting = 1 << iota
+	httpsDialerPolicyCancelingContextOnSuccess
+)
+
+// httpsDialerPolicyCancelingContext is an [enginenetsx.HTTPSDialerPolicy] with a cancel
+// function that causes the context to be canceled once we start dialing.
+//
+// This struct helps with testing [enginenetx.HTTPSDialer] is WAI when the context
+// has been canceled and we correctly shutdown all goroutines.
+type httpsDialerPolicyCancelingContext struct {
+	cancel context.CancelFunc
+	flags  int
+	policy enginenetx.HTTPSDialerPolicy
+}
+
+var _ enginenetx.HTTPSDialerPolicy = &httpsDialerPolicyCancelingContext{}
+
+// LookupTactics implements enginenetx.HTTPSDialerPolicy.
+func (p *httpsDialerPolicyCancelingContext) LookupTactics(ctx context.Context, domain string, reso model.Resolver) ([]enginenetx.HTTPSDialerTactic, error) {
+	tactics, err := p.policy.LookupTactics(ctx, domain, reso)
+	if err != nil {
+		return nil, err
+	}
+	var out []enginenetx.HTTPSDialerTactic
+	for _, tactic := range tactics {
+		out = append(out, &httpsDialerTacticCancelingContext{
+			HTTPSDialerTactic: tactic,
+			cancel:            p.cancel,
+			flags:             p.flags,
+		})
+	}
+	return out, nil
+}
+
+// Parallelism implements enginenetx.HTTPSDialerPolicy.
+func (p *httpsDialerPolicyCancelingContext) Parallelism() int {
+	return p.policy.Parallelism()
+}
+
+// httpsDialerTacticCancelingContext is the tactic returned by [httpsDialerPolicyCancelingContext].
+type httpsDialerTacticCancelingContext struct {
+	enginenetx.HTTPSDialerTactic
+	cancel context.CancelFunc
+	flags  int
+}
+
+// OnStarting implements enginenetx.HTTPSDialerTactic.
+func (t *httpsDialerTacticCancelingContext) OnStarting() {
+	if (t.flags & httpsDialerPolicyCancelingContextOnStarting) != 0 {
+		t.cancel()
+	}
+}
+
+// OnSuccess implements enginenetx.HTTPSDialerTactic.
+func (t *httpsDialerTacticCancelingContext) OnSuccess() {
+	if (t.flags & httpsDialerPolicyCancelingContextOnSuccess) != 0 {
+		t.cancel()
+	}
+}
+
+func TestHTTPSDialerWAI(t *testing.T) {
+	// testcase is a test case implemented by this function
+	type testcase struct {
+		// name is the name of the test case
+		name string
+
+		// short indicates whether this is a short test
+		short bool
+
+		// policy is the dialer policy
+		policy enginenetx.HTTPSDialerPolicy
+
+		// endpoint is the endpoint to connect to consisting of a domain
+		// name or IP address followed by a TCP port
+		endpoint string
+
+		// scenario is the netemx testing scenario to create
+		scenario []*netemx.ScenarioDomainAddresses
+
+		// configureDPI configures DPI rules (just add an empty
+		// function if you don't need any)
+		configureDPI func(dpi *netem.DPIEngine)
+
+		// expectErr is the error string we expect to see
+		expectErr string
+	}
+
+	allTestCases := []testcase{
+
+		// This test case ensures that we handle the corner case of a missing port
+		{
+			name:     "net.SplitHostPort failure",
+			short:    true,
+			policy:   &enginenetx.HTTPSDialerNullPolicy{},
+			endpoint: "www.example.com", // note: here the port is missing
+			scenario: netemx.InternetScenario,
+			configureDPI: func(dpi *netem.DPIEngine) {
+				// nothing
+			},
+			expectErr: "address www.example.com: missing port in address",
+		},
+
+		// This test case ensures that we handle the case of a nonexistent domain
+		{
+			name:     "hd.policy.LookupTactics failure",
+			short:    true,
+			policy:   &enginenetx.HTTPSDialerNullPolicy{},
+			endpoint: "www.example.nonexistent:443", // note: the domain does not exist
+			scenario: netemx.InternetScenario,
+			configureDPI: func(dpi *netem.DPIEngine) {
+				// nothing
+			},
+			expectErr: "dns_nxdomain_error",
+		},
+
+		// This test case is the common case: all is good with multiple addresses to dial (I am
+		// not testing the case of a single address because it's a subcase of this one)
+		{
+			name:     "successful dial with multiple addresses",
+			short:    true,
+			policy:   &enginenetx.HTTPSDialerNullPolicy{},
+			endpoint: "www.example.com:443",
+			scenario: []*netemx.ScenarioDomainAddresses{{
+				Domains: []string{
+					"www.example.com",
+				},
+				Addresses: []string{
+					"93.184.216.34",
+					"93.184.216.35",
+					"93.184.216.36",
+					"93.184.216.37",
+				},
+				Role:             netemx.ScenarioRoleWebServer,
+				WebServerFactory: netemx.ExampleWebPageHandlerFactory(),
+			}},
+			configureDPI: func(dpi *netem.DPIEngine) {
+				// nothing
+			},
+			expectErr: "",
+		},
+
+		// Here we make sure that we're doing OK if the addresses are TCP-blocked
+		{
+			name:     "with TCP connect errors",
+			short:    true,
+			policy:   &enginenetx.HTTPSDialerNullPolicy{},
+			endpoint: "www.example.com:443",
+			scenario: []*netemx.ScenarioDomainAddresses{{
+				Domains: []string{
+					"www.example.com",
+				},
+				Addresses: []string{
+					"93.184.216.34",
+					"93.184.216.35",
+				},
+				Role:             netemx.ScenarioRoleWebServer,
+				WebServerFactory: netemx.ExampleWebPageHandlerFactory(),
+			}},
+			configureDPI: func(dpi *netem.DPIEngine) {
+				// we force closing the connection for all the known server endpoints
+				dpi.AddRule(&netem.DPICloseConnectionForServerEndpoint{
+					Logger:          log.Log,
+					ServerIPAddress: "93.184.216.34",
+					ServerPort:      443,
+				})
+				dpi.AddRule(&netem.DPICloseConnectionForServerEndpoint{
+					Logger:          log.Log,
+					ServerIPAddress: "93.184.216.35",
+					ServerPort:      443,
+				})
+			},
+			expectErr: "connection_refused\nconnection_refused",
+		},
+
+		// Here we're making sure it's all WAI when there is TLS interference
+		{
+			name:     "with TLS handshake errors",
+			short:    true,
+			policy:   &enginenetx.HTTPSDialerNullPolicy{},
+			endpoint: "www.example.com:443",
+			scenario: []*netemx.ScenarioDomainAddresses{{
+				Domains: []string{
+					"www.example.com",
+				},
+				Addresses: []string{
+					"93.184.216.34",
+					"93.184.216.35",
+				},
+				Role:             netemx.ScenarioRoleWebServer,
+				WebServerFactory: netemx.ExampleWebPageHandlerFactory(),
+			}},
+			configureDPI: func(dpi *netem.DPIEngine) {
+				// we force resetting the connection for www.example.com
+				dpi.AddRule(&netem.DPIResetTrafficForTLSSNI{
+					Logger: log.Log,
+					SNI:    "www.example.com",
+				})
+			},
+			expectErr: "connection_reset\nconnection_reset",
+		},
+
+		// Note: this is where we test that TLS verification is WAI. The netemx scenario role
+		// constructs the equivalent of real world's badssl.com and we're checking whether
+		// we would accept a certificate valid for another hostname. The answer should be "NO!".
+		{
+			name:     "with a TLS certificate valid for ANOTHER domain",
+			short:    true,
+			policy:   &enginenetx.HTTPSDialerNullPolicy{},
+			endpoint: "wrong.host.badssl.com:443",
+			scenario: []*netemx.ScenarioDomainAddresses{{
+				Domains: []string{
+					"wrong.host.badssl.com",
+					"untrusted-root.badssl.com",
+					"expired.badssl.com",
+				},
+				Addresses: []string{
+					"93.184.216.34",
+					"93.184.216.35",
+				},
+				Role: netemx.ScenarioRoleBadSSL,
+			}},
+			configureDPI: func(dpi *netem.DPIEngine) {
+				// nothing
+			},
+			expectErr: "ssl_invalid_hostname\nssl_invalid_hostname",
+		},
+
+		// Note: this is another TLS related test case where we make sure that
+		// we can handle an untrusted root/self signed certificate
+		{
+			name:     "with TLS certificate signed by an unknown authority",
+			short:    true,
+			policy:   &enginenetx.HTTPSDialerNullPolicy{},
+			endpoint: "untrusted-root.badssl.com:443",
+			scenario: []*netemx.ScenarioDomainAddresses{{
+				Domains: []string{
+					"wrong.host.badssl.com",
+					"untrusted-root.badssl.com",
+					"expired.badssl.com",
+				},
+				Addresses: []string{
+					"93.184.216.34",
+					"93.184.216.35",
+				},
+				Role: netemx.ScenarioRoleBadSSL,
+			}},
+			configureDPI: func(dpi *netem.DPIEngine) {
+				// nothing
+			},
+			expectErr: "ssl_unknown_authority\nssl_unknown_authority",
+		},
+
+		// Note: this is another TLS related test case where we make sure that
+		// we can handle a certificate that has now expired.
+		{
+			name:     "with expired TLS certificate",
+			short:    true,
+			policy:   &enginenetx.HTTPSDialerNullPolicy{},
+			endpoint: "expired.badssl.com:443",
+			scenario: []*netemx.ScenarioDomainAddresses{{
+				Domains: []string{
+					"wrong.host.badssl.com",
+					"untrusted-root.badssl.com",
+					"expired.badssl.com",
+				},
+				Addresses: []string{
+					"93.184.216.34",
+					"93.184.216.35",
+				},
+				Role: netemx.ScenarioRoleBadSSL,
+			}},
+			configureDPI: func(dpi *netem.DPIEngine) {
+				// nothing
+			},
+			expectErr: "ssl_invalid_certificate\nssl_invalid_certificate",
+		},
+
+		// This is a corner case: what if the context is canceled after the DNS lookup
+		// but before we start dialing? Are we closing all goroutines and returning correctly?
+		{
+			name:  "with context being canceled in OnStarting",
+			short: true,
+			policy: &httpsDialerPolicyCancelingContext{
+				cancel: nil,
+				flags:  httpsDialerPolicyCancelingContextOnStarting,
+				policy: &enginenetx.HTTPSDialerNullPolicy{},
+			},
+			endpoint: "www.example.com:443",
+			scenario: []*netemx.ScenarioDomainAddresses{{
+				Domains: []string{
+					"www.example.com",
+				},
+				Addresses: []string{
+					"93.184.216.34",
+					"93.184.216.35",
+				},
+				Role:             netemx.ScenarioRoleWebServer,
+				WebServerFactory: netemx.ExampleWebPageHandlerFactory(),
+			}},
+			configureDPI: func(dpi *netem.DPIEngine) {
+				// nothing
+			},
+			expectErr: "context canceled",
+		},
+
+		// This is another corner case: what happens if the context is canceled after we
+		// have a good connection but before we're able to report it to the caller?
+		{
+			name:  "with context being canceled in OnSuccess for the first success",
+			short: true,
+			policy: &httpsDialerPolicyCancelingContext{
+				cancel: nil,
+				flags:  httpsDialerPolicyCancelingContextOnSuccess,
+				policy: &enginenetx.HTTPSDialerNullPolicy{},
+			},
+			endpoint: "www.example.com:443",
+			scenario: []*netemx.ScenarioDomainAddresses{{
+				Domains: []string{
+					"www.example.com",
+				},
+				Addresses: []string{
+					"93.184.216.34",
+					"93.184.216.35",
+				},
+				Role:             netemx.ScenarioRoleWebServer,
+				WebServerFactory: netemx.ExampleWebPageHandlerFactory(),
+			}},
+			configureDPI: func(dpi *netem.DPIEngine) {
+				// nothing
+			},
+			expectErr: "context canceled",
+		}}
+
+	for _, tc := range allTestCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// make sure we honor `go test -short`
+			if !tc.short && testing.Short() {
+				t.Skip("skip test in short mode")
+			}
+
+			// create the QA environment
+			env := netemx.MustNewScenario(tc.scenario)
+			defer env.Close()
+
+			// possibly add specific DPI rules
+			tc.configureDPI(env.DPIEngine())
+
+			// create the proper underlying network
+			unet := &netxlite.NetemUnderlyingNetworkAdapter{UNet: env.ClientStack}
+
+			// create the network proper
+			netx := &netxlite.Netx{Underlying: unet}
+
+			// create the getaddrinfo resolver
+			resolver := netx.NewStdlibResolver(log.Log)
+
+			// create the TLS dialer
+			dialer := enginenetx.NewHTTPSDialer(
+				log.Log,
+				tc.policy,
+				resolver,
+				unet,
+			)
+			defer dialer.CloseIdleConnections()
+
+			// configure cancellable context--some tests are going to use cancel
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			// Possibly tell the httpsDialerPolicyCancelingContext about the cancel func
+			// depending on which flags have been configured.
+			if p, ok := tc.policy.(*httpsDialerPolicyCancelingContext); ok {
+				p.cancel = cancel
+			}
+
+			// dial the TLS connection
+			tlsConn, err := dialer.DialTLSContext(ctx, "tcp", tc.endpoint)
+
+			t.Logf("%+v %+v", tlsConn, err)
+
+			// make sure the error is the one we expected
+			switch {
+			case err != nil && tc.expectErr == "":
+				t.Fatal("expected", tc.expectErr, "got", err)
+
+			case err == nil && tc.expectErr != "":
+				t.Fatal("expected", tc.expectErr, "got", err)
+
+			case err != nil && tc.expectErr != "":
+				if diff := cmp.Diff(tc.expectErr, err.Error()); diff != "" {
+					t.Fatal(diff)
+				}
+
+			case err == nil && tc.expectErr == "":
+				// all good
+			}
+
+			// make sure we close the conn
+			if tlsConn != nil {
+				defer tlsConn.Close()
+			}
+
+			// wait for background connections to join
+			dialer.WaitGroup().Wait()
+		})
+	}
+}
