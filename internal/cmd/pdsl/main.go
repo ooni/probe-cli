d63dbfc6a5761b1886068e657618c971500fc11b
Simone Basso <bassosimone@gmail.com>
2023-10-01 22:10:09 +0200

    show how to compose stuff

--- internal/cmd/pdsl/main.go
+++ internal/cmd/pdsl/main.go
@@ -9,26 +9,19 @@ import (
 	"github.com/ooni/probe-cli/v3/internal/pdsl"
 )
 
-func main() {
-	log.Log = &log.Logger{Level: log.InfoLevel, Handler: logx.NewHandlerWithDefaultSettings()}
-
-	ctx := context.Background()
-	rt := pdsl.NewMinimalRuntime(log.Log)
-
-	domain := pdsl.DomainName("www.example.com")
+func measureWithTLS(ctx context.Context, rt pdsl.Runtime,
+	domain pdsl.DomainName, udpResolverEndpoint pdsl.Endpoint) <-chan pdsl.Result[pdsl.TLSConn] {
+	// create a suitable TLS configuration
 	tlsConfig := &tls.Config{
 		NextProtos: []string{"h2", "http/1.1"},
-		ServerName: "www.example.com",
+		ServerName: string(domain),
 		RootCAs:    nil, // use netxlite default CA
 	}
-
-	// make sure we close all open connections
-	defer rt.Close()
 
 	// resolve IP addresses using two resolvers and deduplicate the results
 	ipAddrs := pdsl.DNSLookupDeduplicate(pdsl.Merge(
 		pdsl.DNSLookupGetaddrinfo(ctx, rt)(domain),
-		pdsl.DNSLookupUDP(ctx, rt, pdsl.Endpoint("8.8.8.8:53"))(domain),
+		pdsl.DNSLookupUDP(ctx, rt, udpResolverEndpoint)(domain),
 	))
 
 	// convert the IP addresses to endpoints
@@ -38,8 +31,20 @@ func main() {
 	conns := pdsl.Merge(pdsl.Fork(4, pdsl.TCPConnect(ctx, rt), endpoints)...)
 
 	// create TLS connections also using a goroutine pool
-	tconns := pdsl.Merge(pdsl.Fork(2, pdsl.TLSHandshake(ctx, rt, tlsConfig), conns)...)
+	return pdsl.Merge(pdsl.Fork(2, pdsl.TLSHandshake(ctx, rt, tlsConfig), conns)...)
+}
 
-	// make sure we run until completion
-	pdsl.Drain(tconns)
+func main() {
+	log.Log = &log.Logger{Level: log.InfoLevel, Handler: logx.NewHandlerWithDefaultSettings()}
+
+	ctx := context.Background()
+	rt := pdsl.NewMinimalRuntime(log.Log)
+
+	// make sure we close all open connections
+	defer rt.Close()
+
+	pdsl.Drain(
+		measureWithTLS(ctx, rt, "www.example.com", "8.8.8.8:53"),
+		measureWithTLS(ctx, rt, "www.youtube.com", "8.8.8.8:53"),
+	)
 }
