ba7e913cf39a370d004b14d75ce0e6f5e787c65b
Simone Basso <bassosimone@gmail.com>
2023-10-11 12:58:58 +0200

    fix(ooniprobe): make sure we actually run echcheck (#1358)
    While there, emit some logs while running it.
    
    While there, make the warning emitted when a nettest is disabled much
    more specific and informative for the user.
    
    Part of https://github.com/ooni/probe/issues/2547
    
    Part of https://github.com/ooni/probe/issues/2553

--- cmd/ooniprobe/internal/nettests/echcheck.go
+++ cmd/ooniprobe/internal/nettests/echcheck.go
@@ -9,6 +9,7 @@ func (n ECHCheck) Run(ctl *Controller) error {
 	if err != nil {
 		return err
 	}
-	// providing empty input causes the experiment to use the default URL
-	return ctl.Run(builder, []string{})
+	// providing an input containing an empty string causes the experiment
+	// to recognize the empty string and use the default URL
+	return ctl.Run(builder, []string{""})
 }
