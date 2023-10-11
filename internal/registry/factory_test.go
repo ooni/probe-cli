0584584fc453d2682a88c8c5873909048230f04d
Simone Basso <bassosimone@gmail.com>
2023-10-11 13:02:59 +0200

    fix(registry): mark torsf as disabled by default
    There may be upcoming changes in torsf which may cause it to fail
    consistently as it occurred during the 3.18 cycle.
    
    Therefore, be defensive and make it disabled by default.
    
    Part of https://github.com/ooni/probe/issues/2553
    
    While there, use slightly better naming for an echcheck function.

--- internal/registry/factory_test.go
+++ internal/registry/factory_test.go
@@ -481,8 +481,11 @@ func TestNewFactory(t *testing.T) {
 			inputPolicy:      model.InputNone,
 		},
 		"torsf": {
-			enabledByDefault: true,
-			inputPolicy:      model.InputNone,
+			// We suspect there will be changes in torsf SNI soon. We are not prepared to
+			// serve these changes using the check-in API. Hence, disable torsf by default
+			// and require enabling it using the check-in API feature flags.
+			//enabledByDefault: false,
+			inputPolicy: model.InputNone,
 		},
 		"urlgetter": {
 			enabledByDefault: true,
