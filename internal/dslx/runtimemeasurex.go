a2ea6db9f221287ca52d4143a7a7c0102c5944c1
Simone Basso <bassosimone@gmail.com>
2023-10-18 09:43:13 +0200

    refactor(dslx): prepare for improving testing
    Rather than using custom fields for testing, we can configure in the
    runtime a custom model.MeasuringNetwork.
    
    We're not doing this just to simplify the codebase, rather the underlying
    intent here is making sure we don't need to keep much state in each
    function, so we can refactor them to be pure functions wrapped by an
    adapter that produces the desired type.
    
    In turn, by doing that, we will be able to factor complexity around
    invoking functions and parsing their results.
    
    In turn, by doing that, we will be able to modify the signature of the
    functions and do the following:
    
    1. allow the DSL model to include stages that take in input a Maybe
    value rather than a value, so we can observe failures more easily than
    we do now and we can write inline code to save into test keys;
    
    2. allow the DSL model to much more easily be refactored to use
    channels, which in turn enables us to compose operations more naturally
    and increase the amount of overlapping (think, e.g., how this enables
    the possibility of waiting additional time for a DNS-over-UDP resolver
    to wait for late/duplicate replies).

--- internal/dslx/runtimemeasurex.go
+++ internal/dslx/runtimemeasurex.go
@@ -5,23 +5,42 @@ import (
 
 	"github.com/ooni/probe-cli/v3/internal/measurexlite"
 	"github.com/ooni/probe-cli/v3/internal/model"
+	"github.com/ooni/probe-cli/v3/internal/netxlite"
 )
 
-// NewRuntimeMeasurexLite creates a [Runtime] using [measurexlite] to collect [*Observations].
-func NewRuntimeMeasurexLite(logger model.Logger, zeroTime time.Time) *RuntimeMeasurexLite {
-	return &RuntimeMeasurexLite{
-		MinimalRuntime: NewMinimalRuntime(logger, zeroTime),
+// RuntimeMeasurexLiteOption is an option for initializing a [*RuntimeMeasurexLite].
+type RuntimeMeasurexLiteOption func(rt *RuntimeMeasurexLite)
+
+// RuntimeMeasurexLiteOptionMeasuringNetwork allows to configure which [model.MeasuringNetwork] to use.
+func RuntimeMeasurexLiteOptionMeasuringNetwork(netx model.MeasuringNetwork) RuntimeMeasurexLiteOption {
+	return func(rt *RuntimeMeasurexLite) {
+		rt.netx = netx
 	}
+}
+
+// NewRuntimeMeasurexLite creates a [Runtime] using [measurexlite] to collect [*Observations].
+func NewRuntimeMeasurexLite(logger model.Logger, zeroTime time.Time, options ...RuntimeMeasurexLiteOption) *RuntimeMeasurexLite {
+	rt := &RuntimeMeasurexLite{
+		MinimalRuntime: NewMinimalRuntime(logger, zeroTime),
+		netx:           &netxlite.Netx{Underlying: nil}, // implies using the host's network
+	}
+	for _, option := range options {
+		option(rt)
+	}
+	return rt
 }
 
 // RuntimeMeasurexLite uses [measurexlite] to collect [*Observations.]
 type RuntimeMeasurexLite struct {
 	*MinimalRuntime
+	netx model.MeasuringNetwork
 }
 
 // NewTrace implements Runtime.
 func (p *RuntimeMeasurexLite) NewTrace(index int64, zeroTime time.Time, tags ...string) Trace {
-	return measurexlite.NewTrace(index, zeroTime, tags...)
+	trace := measurexlite.NewTrace(index, zeroTime, tags...)
+	trace.Netx = p.netx
+	return trace
 }
 
 var _ Runtime = &RuntimeMeasurexLite{}
