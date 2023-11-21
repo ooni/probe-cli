64f0cdfc13fe1b6af10e60e2420e37685592d5ff
Simone Basso <bassosimone@gmail.com>
2023-11-21 14:45:06 +0100

    fix: bump version number also in tests

--- internal/experiment/signal/signal_test.go
+++ internal/experiment/signal/signal_test.go
@@ -17,7 +17,7 @@ func TestNewExperimentMeasurer(t *testing.T) {
 	if measurer.ExperimentName() != "signal" {
 		t.Fatal("unexpected name")
 	}
-	if measurer.ExperimentVersion() != "0.2.3" {
+	if measurer.ExperimentVersion() != "0.2.4" {
 		t.Fatal("unexpected version")
 	}
 }
