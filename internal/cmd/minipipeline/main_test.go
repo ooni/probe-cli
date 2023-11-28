bb7dd9c1f6c3a3ea60323f1d23fd21a31f3344ba
Simone Basso <bassosimone@gmail.com>
2023-11-28 09:47:17 +0100

    feat: minipipeline to analyze measurements (#1393)
    This diff introduces the `minipipeline` package and command. The general
    idea here is to analyze experiments results using an architecture that
    matches https://github.com/ooni/data as closely as possible.
    
    The advantage of using this architecture is that we can neatly divide
    the data analysis process into three phases:
    
    1. ingestion converts an OONI measurement into observations, which are
    flat data structures;
    
    2. analysis converts observations into an analysis structure, which is
    significantly simpler than observations;
    
    3. scoring is the process (implemented by each experiment) to turn the
    analysis into top-level keys.
    
    That is:
    
    ```mermaid
    stateDiagram-v2
      state "{ingestion}" as ingestion
      MEASUREMENT --> ingestion
      ingestion --> OBSERVATIONS
      state "{analysis}" as analysis
      OBSERVATIONS --> analysis
      analysis --> ANALYSIS
      state "{scoring}" as scoring
      ANALYSIS --> scoring
      scoring --> TOP_LEVEL_KEYS
    ```
    
    This diff in particular tackles points 1 and 2. We'll implement 3 for
    Web Connectivity LTE in a separate diff.
    
    The diff itself commits several observations and analysis files along
    with the measurements from which they originate, which we all
    collectively use to write regression tests for the `minipipeline`
    package.
    
    The motivation for implementing these changes is that I need a more
    clearly defined model to address the differences between Web
    Connectivity LTE and v0.4 (see also
    https://github.com/ooni/probe-cli/pull/1392). Because there is already
    an architectural model for implementing these changes in ooni/data, it
    seems logical to borrow liberally from ooni/data.
    
    Incidentally, this change has the benefit of making it easier, in the
    future, to align ooni/probe-cli and ooni/data.
    
    The reference issue is https://github.com/ooni/probe/issues/2634.
    Implementing this change seems now a precondition for implementing extra
    changes that would address such an issue for good, by changing LTE's
    scoring.
    
    Unlike the ooni/data pipeline, this pipeline is minimal (hence the
    name): it only aims to process measurements collected by a recent
    version of ooniprobe, rather than all measurements from the beginning of
    time.
    
    While there, disable signal integration tests because of
    https://github.com/ooni/probe/issues/2636.

---
+++ internal/cmd/minipipeline/main_test.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/ooni/probe-cli/v3/internal/must"
+)
+
+func mustloadfile(filename string) (object map[string]any) {
+	data := must.ReadFile(filename)
+	must.UnmarshalJSON(data, &object)
+	return
+}
+
+func mustloaddata(contentmap map[string][]byte, key string) (object map[string]any) {
+	data := contentmap[key]
+	must.UnmarshalJSON(data, &object)
+	return
+}
+
+func TestMainSuccess(t *testing.T) {
+	// make sure we set the destination directory
+	*destdir = "xo"
+
+	// make sure we're reading from the expected input
+	*measurement = filepath.Join("testdata", "measurement.json")
+
+	// make sure we store the expected output
+	contentmap := make(map[string][]byte)
+	mustWriteFileFn = func(filename string, content []byte, mode fs.FileMode) {
+		contentmap[filename] = content
+	}
+
+	// make sure osExit is correct
+	osExit = os.Exit
+
+	// also check whether we can add a prefix
+	*prefix = "y-"
+
+	// run the main function
+	main()
+
+	// make sure the generated observations are good
+	expectedObservations := mustloadfile(filepath.Join("testdata", "observations.json"))
+	gotObservations := mustloaddata(contentmap, filepath.Join("xo", "y-observations.json"))
+	if diff := cmp.Diff(expectedObservations, gotObservations); diff != "" {
+		t.Fatal(diff)
+	}
+
+	// make sure the generated analysis is good
+	expectedAnalysis := mustloadfile(filepath.Join("testdata", "analysis.json"))
+	gotAnalysis := mustloaddata(contentmap, filepath.Join("xo", "y-analysis.json"))
+	if diff := cmp.Diff(expectedAnalysis, gotAnalysis); diff != "" {
+		t.Fatal(diff)
+	}
+}
+
+func TestMainUsage(t *testing.T) {
+	// make sure we clear the destination directory
+	*destdir = ""
+
+	// make sure the expected input file is empty
+	*measurement = ""
+
+	// make sure we panic if we try to write on disk
+	mustWriteFileFn = func(filename string, content []byte, mode fs.FileMode) {
+		panic(errors.New("mustWriteFileFn"))
+	}
+
+	// make sure osExit is correct
+	osExit = func(code int) {
+		panic(fmt.Errorf("osExit: %d", code))
+	}
+
+	// make sure the prefix is also clean
+	*prefix = ""
+
+	var err error
+	func() {
+		// intercept panic caused by osExit or other panics
+		defer func() {
+			if r := recover(); r != nil {
+				err = r.(error)
+			}
+		}()
+
+		// run the main function with the given args
+		main()
+	}()
+
+	// make sure we've got the expected error
+	if err == nil || err.Error() != "osExit: 1" {
+		t.Fatal("expected", "os.Exit: 1", "got", err)
+	}
+}
---
+++ internal/minipipeline/qa_test.go
@@ -0,0 +1,79 @@
+package minipipeline_test
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/ooni/probe-cli/v3/internal/minipipeline"
+	"github.com/ooni/probe-cli/v3/internal/must"
+	"github.com/ooni/probe-cli/v3/internal/runtimex"
+)
+
+func testCmpDiffUsingGenericMaps(origLeft, origRight any) string {
+	rawLeft := must.MarshalJSON(origLeft)
+	rawRight := must.MarshalJSON(origRight)
+	var left map[string]any
+	must.UnmarshalJSON(rawLeft, &left)
+	var right map[string]any
+	must.UnmarshalJSON(rawRight, &right)
+	return cmp.Diff(left, right)
+}
+
+func testMustRunAllWebTestCases(t *testing.T, topdir string) {
+	t.Run(topdir, func(t *testing.T) {
+		entries := runtimex.Try1(os.ReadDir(topdir))
+		for _, entry := range entries {
+			if !entry.IsDir() {
+				continue
+			}
+			t.Run(entry.Name(), func(t *testing.T) {
+				fullpath := filepath.Join(topdir, entry.Name())
+				// read the raw measurement from the test case
+				measurementFile := filepath.Join(fullpath, "measurement.json")
+				measurementRaw := must.ReadFile(measurementFile)
+				var measurementData minipipeline.WebMeasurement
+				must.UnmarshalJSON(measurementRaw, &measurementData)
+
+				// load the expected container from the test case
+				expectedContainerFile := filepath.Join(fullpath, "observations.json")
+				expectedContainerRaw := must.ReadFile(expectedContainerFile)
+				var expectedContainerData minipipeline.WebObservationsContainer
+				must.UnmarshalJSON(expectedContainerRaw, &expectedContainerData)
+
+				// load the expected analysis from the test case
+				expectedAnalysisFile := filepath.Join(fullpath, "analysis.json")
+				expectedAnalysisRaw := must.ReadFile(expectedAnalysisFile)
+				var expectedAnalysisData minipipeline.WebAnalysis
+				must.UnmarshalJSON(expectedAnalysisRaw, &expectedAnalysisData)
+
+				// load the measurement into the pipeline
+				gotContainerData, err := minipipeline.IngestWebMeasurement(&measurementData)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				// analyze the measurement
+				gotAnalysisData := minipipeline.AnalyzeWebObservations(gotContainerData)
+
+				t.Run("observations", func(t *testing.T) {
+					if diff := testCmpDiffUsingGenericMaps(&expectedContainerData, gotContainerData); diff != "" {
+						t.Fatal(diff)
+					}
+				})
+
+				t.Run("analysis", func(t *testing.T) {
+					if diff := testCmpDiffUsingGenericMaps(&expectedAnalysisData, gotAnalysisData); diff != "" {
+						t.Fatal(diff)
+					}
+				})
+			})
+		}
+	})
+}
+
+func TestQAWeb(t *testing.T) {
+	testMustRunAllWebTestCases(t, filepath.Join("testdata", "webconnectivity", "generated"))
+	testMustRunAllWebTestCases(t, filepath.Join("testdata", "webconnectivity", "manual"))
+}
