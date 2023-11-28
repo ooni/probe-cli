7e105507e5f1f455bf9393ad76b0118dfafa2962
Simone Basso <bassosimone@gmail.com>
2023-11-28 14:46:03 +0100

    fix(minipipeline): DNSLookupFailures must be a slice (#1395)
    `WebObservationsContainer.DNSLookupFailures` MUST be a slice because of
    https://github.com/ooni/probe/issues/2624. We have the same transaction
    IDs for both A and AAAA queries. Until we fix this bug, we must use a
    list because that allows for duplication.
    
    While there, create `measurexlite.webTitleRegexp` just once.
    
    While there, implement an `-help` flag for
    `./internal/cmd/minipipeline`.

--- internal/measurexlite/web.go
+++ internal/measurexlite/web.go
@@ -6,12 +6,15 @@ package measurexlite
 
 import "regexp"
 
+// webTitleRegexp is the regexp to extract the title
+//
+// MK used {1,128} but we're making it larger here to get longer titles
+// e.g. <http://www.isa.gov.il/Pages/default.aspx>'s one
+var webTitleRegexp = regexp.MustCompile(`(?i)<title>([^<]{1,512})</title>`)
+
 // WebGetTitle returns the title or an empty string.
 func WebGetTitle(measurementBody string) string {
-	// MK used {1,128} but we're making it larger here to get longer titles
-	// e.g. <http://www.isa.gov.il/Pages/default.aspx>'s one
-	re := regexp.MustCompile(`(?i)<title>([^<]{1,512})</title>`)
-	v := re.FindStringSubmatch(measurementBody)
+	v := webTitleRegexp.FindStringSubmatch(measurementBody)
 	if len(v) < 2 {
 		return ""
 	}
