18c38ee539a3c07ffbd01a15849e62d2d1e9ee23
Simone Basso <bassosimone@gmail.com>
2023-11-28 20:45:02 +0100

    cleanup(minipipeline): factor code into utility functions (#1400)
    While there, make the code slightly more correct and/or compact:
    
    1. do not extract the AS org name
    2. only extract the HTTP response if failure is nil
    3. only extract the control HTTP response if failure is nil
    4. do not include the zero ASN in results
    
    Also, write a script that updates all the minipipeline regression tests.
    
    Part of https://github.com/ooni/probe/issues/2634

--- internal/minipipeline/utils.go
+++ internal/minipipeline/utils.go
@@ -1,8 +1,63 @@
 package minipipeline
 
+import (
+	"strings"
+
+	"github.com/ooni/probe-cli/v3/internal/geoipx"
+	"github.com/ooni/probe-cli/v3/internal/model"
+	"github.com/ooni/probe-cli/v3/internal/optional"
+)
+
 func utilsStringPointerToString(failure *string) (out string) {
 	if failure != nil {
 		out = *failure
 	}
 	return
+}
+
+func utilsGeoipxLookupASN(ipAddress string) optional.Value[int64] {
+	if asn, _, err := geoipx.LookupASN(ipAddress); err == nil && asn > 0 {
+		return optional.Some(int64(asn))
+	}
+	return optional.None[int64]()
+}
+
+func utilsExtractHTTPHeaderKeys[T ~string](input map[string]T) optional.Value[map[string]bool] {
+	output := make(map[string]bool)
+	for key := range input {
+		output[key] = true
+	}
+	return optional.Some(output)
+}
+
+func utilsExtractHTTPLocation(input map[string]model.ArchivalScrubbedMaybeBinaryString) optional.Value[string] {
+	for key, value := range input {
+		if strings.ToLower(key) == "location" {
+			return optional.Some(string(value))
+		}
+	}
+	return optional.None[string]()
+}
+
+func utilsDetermineWhetherHTTPResponseIsFinal(status int64) optional.Value[bool] {
+	switch status / 100 {
+	case 2, 4, 5:
+		return optional.Some(true)
+	default:
+		return optional.Some(false)
+	}
+}
+
+func utilsForEachIPAddress(answers []model.ArchivalDNSAnswer, fx func(ipAddr string)) {
+	for _, ans := range answers {
+		// extract the IP address we resolved
+		switch ans.AnswerType {
+		case "A":
+			fx(ans.IPv4)
+		case "AAAA":
+			fx(ans.IPv6)
+		default:
+			// nothing
+		}
+	}
 }
