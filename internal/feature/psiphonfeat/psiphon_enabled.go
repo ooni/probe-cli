bd0129622d9dae7d01346cf9e6900f02970e7251
Simone Basso <bassosimone@gmail.com>
2023-12-12 22:08:18 +0100

    fixxx

--- internal/feature/psiphonfeat/psiphon_enabled.go
+++ internal/feature/psiphonfeat/psiphon_enabled.go
@@ -10,6 +10,19 @@ import (
 
 // Start attempts to start the Psiphon tunnel and returns either a Tunnel or an error.
 func Start(ctx context.Context, config []byte, workdir string) (Tunnel, error) {
-	return clientlib.StartTunnel(ctx, config, "", clientlib.Parameters{
+	tun, err := clientlib.StartTunnel(ctx, config, "", clientlib.Parameters{
 		DataRootDirectory: &workdir}, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &tunnel{tun}, nil
+}
+
+type tunnel struct {
+	*clientlib.PsiphonTunnel
+}
+
+// GetSOCKSProxyPort implements Tunnel.
+func (t *tunnel) GetSOCKSProxyPort() int {
+	return t.SOCKSProxyPort
 }
