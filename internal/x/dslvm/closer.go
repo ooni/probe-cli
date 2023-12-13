00822589285424e8f198d755002d5731e258035b
Simone Basso <bassosimone@gmail.com>
2023-12-13 17:55:15 +0100

    x

--- internal/x/dslvm/closer.go
+++ internal/x/dslvm/closer.go
@@ -2,7 +2,7 @@ package dslvm
 
 import "github.com/ooni/probe-cli/v3/internal/model"
 
-// Closer is something that [Discard] should explicitly close.
+// Closer is something that [Drop] should explicitly close.
 type Closer interface {
 	Close(logger model.Logger) error
 }
