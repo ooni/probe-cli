3a448302e081327a1772174341cdcf4a0b2d1d78
Simone Basso <bassosimone@gmail.com>
2024-01-24 15:09:25 +0100

    chore(minipipeline): add http://firefox.com test case (#1470)
    Closes https://github.com/ooni/probe/issues/1530.
    
    While there sort entries inside of the
    `./script/updateminipipeline.bash` script.

--- script/updateminipipeline.bash
+++ script/updateminipipeline.bash
@@ -10,16 +10,20 @@ go run ./internal/cmd/minipipeline \
 	-measurement ./internal/minipipeline/testdata/webconnectivity/manual/dnsgoogle80/measurement.json
 
 go run ./internal/cmd/minipipeline \
+	-destdir ./internal/minipipeline/testdata/webconnectivity/manual/firefoxcom \
+	-measurement ./internal/minipipeline/testdata/webconnectivity/manual/firefoxcom/measurement.json
+
+go run ./internal/cmd/minipipeline \
+	-destdir ./internal/minipipeline/testdata/webconnectivity/manual/issue-2456 \
+	-measurement ./internal/minipipeline/testdata/webconnectivity/manual/issue-2456/measurement.json
+
+go run ./internal/cmd/minipipeline \
 	-destdir ./internal/minipipeline/testdata/webconnectivity/manual/noipv6 \
 	-measurement ./internal/minipipeline/testdata/webconnectivity/manual/noipv6/measurement.json
 
 go run ./internal/cmd/minipipeline \
 	-destdir ./internal/minipipeline/testdata/webconnectivity/manual/youtube \
 	-measurement ./internal/minipipeline/testdata/webconnectivity/manual/youtube/measurement.json
-
-go run ./internal/cmd/minipipeline \
-	-destdir ./internal/minipipeline/testdata/webconnectivity/manual/issue-2456 \
-	-measurement ./internal/minipipeline/testdata/webconnectivity/manual/issue-2456/measurement.json
 
 go run ./internal/cmd/minipipeline \
 	-measurement ./internal/cmd/minipipeline/testdata/measurement.json \
