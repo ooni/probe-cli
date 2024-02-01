6cb78367f211c68690bba6c049c9546882a151b4
Simone Basso <bassosimone@gmail.com>
2024-02-01 23:02:25 +0100

    x

--- script/go.bash
+++ script/go.bash
@@ -2,4 +2,6 @@
 set -euxo pipefail
 # We invoke ./script/internal/go.bash through the gofixpath subcommand such that
 # the "go" binary in PATH is the correct version of go.
+#
+# See https://github.com/ooni/probe/issues/2664
 go run ./internal/cmd/buildtool gofixpath -- ./script/internal/go.bash "$@"
--- script/internal/go.bash
+++ script/internal/go.bash
@@ -2,4 +2,6 @@
 set -euxo pipefail
 # If this script is invoked by ./script/go.bash, then go is
 # the correct version of go expected by the buildtool.
+#
+# See https://github.com/ooni/probe/issues/2664
 go "$@"
