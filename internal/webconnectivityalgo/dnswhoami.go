515104ebc395fabae1117cebebd9a0c2233126eb
Simone Basso <bassosimone@gmail.com>
2024-02-08 15:08:37 +0100

    x

--- internal/webconnectivityalgo/dnswhoami.go
+++ internal/webconnectivityalgo/dnswhoami.go
@@ -23,7 +23,7 @@ type DNSWhoamiInfoEntry struct {
 	Address string `json:"address"`
 }
 
-// dnsWhoamiInfoTimedEntry keeps an address and the time we create the entry together.
+// dnsWhoamiInfoTimedEntry keeps an address and the time we created the entry together.
 type dnsWhoamiInfoTimedEntry struct {
 	Addr string
 	T    time.Time
