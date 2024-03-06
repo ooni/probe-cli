2b9f480a332ee5a3a0498497c18f33b3a62ad508
Ain Ghazal <ainghazal42@gmail.com>
2024-03-06 22:57:11 +0100

    wip

--- internal/experiment/openvpn/endpoint.go
+++ internal/experiment/openvpn/endpoint.go
@@ -1,6 +1,12 @@
 package openvpn
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/ooni/minivpn/pkg/config"
+	vpnconfig "github.com/ooni/minivpn/pkg/config"
+	vpntracex "github.com/ooni/minivpn/pkg/tracex"
+)
 
 // endpoint is a single endpoint to be probed.
 type endpoint struct {
@@ -41,4 +47,45 @@ func sampleRandomEndpoint(all []endpoint) endpoint {
 	// guaranteed to be random
 	// https://xkcd.com/221/
 	return all[0]
+}
+
+var defaultOptionsByProvider = map[string]*vpnconfig.OpenVPNOptions{
+	"riseup": {
+		Cipher: "AES-256-GCM",
+		Auth:   "SHA512",
+		CA:     []byte{},
+		Key:    []byte{},
+		Cert:   []byte{},
+	},
+}
+
+// getVPNConfig gets a properly configured [*vpnconfig.Config] object for the given endpoint.
+// To obtain that, we merge the endpoint specific configuration with base options.
+// These base options are for the moment hardcoded. In the future we will want to be smarter
+// about getting information for different providers.
+func getVPNConfig(tracer *vpntracex.Tracer, endpoint *endpoint) (*vpnconfig.Config, error) {
+	// TODO(ainghazal): use options merge
+	provider := endpoint.Provider
+	// TODO(ainghazal): return error if provider unknown. we're in the happy path for now.
+	baseOptions := defaultOptionsByProvider[provider]
+
+	cfg := vpnconfig.NewConfig(
+		vpnconfig.WithOpenVPNOptions(
+			&vpnconfig.OpenVPNOptions{
+				Remote: endpoint.IPAddr,
+				Port:   endpoint.Port,
+				Proto:  vpnconfig.Proto(endpoint.Transport),
+
+				// options coming from the default values,
+				// to be changed by check-in API info in the future.
+				CA:     baseOptions.CA,
+				Cert:   baseOptions.Cert,
+				Key:    baseOptions.Key,
+				Cipher: baseOptions.Cipher,
+				Auth:   baseOptions.Auth,
+			},
+		),
+		config.WithHandshakeTracer(tracer))
+	// TODO: validate here and return an error, maybe.
+	return cfg, nil
 }
