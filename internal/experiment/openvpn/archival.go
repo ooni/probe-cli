54c9510b6f28a9eb48fa5b575a24367c1eb925fa
Ain Ghazal <ainghazal42@gmail.com>
2024-03-07 19:42:27 +0100

    wip

--- internal/experiment/openvpn/archival.go
+++ internal/experiment/openvpn/archival.go
@@ -2,7 +2,7 @@ package openvpn
 
 import "time"
 
-// TODO(ainghazal): move to archiva package when consolidated.
+// TODO(ainghazal): move to ooni internal archival package when consolidated.
 
 // ArchivalOpenVPNHandshakeResult contains the result of a OpenVPN handshake.
 type ArchivalOpenVPNHandshakeResult struct {
@@ -13,10 +13,13 @@ type ArchivalOpenVPNHandshakeResult struct {
 	Provider      string                       `json:"provider"`
 	Status        ArchivalOpenVPNConnectStatus `json:"status"`
 	StartTime     time.Time                    `json:"handshake_start_time"`
-	T0            float64                      `json:"t0,omitempty"`
-	T             float64                      `json:"t"`
-	Tags          []string                     `json:"tags"`
-	TransactionID int64                        `json:"transaction_id,omitempty"`
+
+	// T0 can differ from StartTime because for TCP we take T0 *after* dialing has successfully completed.
+	// This might be counterintuitive, review.
+	T0            float64  `json:"t0,omitempty"`
+	T             float64  `json:"t"`
+	Tags          []string `json:"tags"`
+	TransactionID int64    `json:"transaction_id,omitempty"`
 }
 
 // ArchivalOpenVPNConnectStatus is the status of ArchivalOpenVPNConnectResult.
@@ -27,7 +30,11 @@ type ArchivalOpenVPNConnectStatus struct {
 }
 
 type ArchivalNetworkEvent struct {
-	// TODO(ainghazal): need to properly propagate I/O errors during the handshake.
+	// TODO(ainghazal): could properly propagate I/O errors during the handshake.
+	// Right now the network events WILL NOT append any RW error, but I think following
+	// other experiments one would expect such errors to be appended as failures in the operation.
+	// However, getting the start boundary for the failed operation might be tricky,
+	// I need to think about it.
 	Address       string   `json:"address,omitempty"`
 	Failure       *string  `json:"failure"`
 	NumBytes      int64    `json:"num_bytes,omitempty"`
