1b612392f2e516df2407dc0d40d07b08ecd7469f
Simone Basso <bassosimone@gmail.com>
2024-04-04 17:50:18 +0200

    fix integration tests

--- internal/cmd/miniooni/main_test.go
+++ internal/cmd/miniooni/main_test.go
@@ -1,12 +1,18 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/ooni/probe-cli/v3/internal/version"
+)
 
 func TestSimple(t *testing.T) {
 	if testing.Short() {
 		t.Skip("skip test in short mode")
 	}
 	MainWithConfiguration("example", &Options{
-		Yes: true,
+		SoftwareName:    "miniooni",
+		SoftwareVersion: version.Version,
+		Yes:             true,
 	})
 }
