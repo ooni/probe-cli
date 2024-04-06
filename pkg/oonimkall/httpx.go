bf8d0b12f0ca8ad3040b58aa4ac6ec513f3f442d
Norbel AMBANUMBEN <aanorbel@gmail.com>
2024-04-06 17:20:41 +0100

    chore : Update `HTTPRequest` property name from `URL` to `Url`, the former doesnt keep values on iOS

--- pkg/oonimkall/httpx.go
+++ pkg/oonimkall/httpx.go
@@ -31,7 +31,7 @@ type HTTPRequest struct {
 	Method string
 
 	// URL is the MANDATORY request URL.
-	URL string
+	Url string
 }
 
 // HTTPResponse is an HTTP response.
@@ -57,7 +57,7 @@ func (sess *Session) httpDoLocked(ctx *Context, jreq *HTTPRequest) (*HTTPRespons
 
 	log.Printf("BONSOIR ELLIOT: %+v", jreq)
 
-	req, err := http.NewRequestWithContext(ctx.ctx, jreq.Method, jreq.URL, nil)
+	req, err := http.NewRequestWithContext(ctx.ctx, jreq.Method, jreq.Url, nil)
 	if err != nil {
 		return nil, err
 	}
