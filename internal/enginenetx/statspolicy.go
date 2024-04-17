a23c8efc8ee50a6334482c6d6c04e154946ef31c
Simone Basso <bassosimone@gmail.com>
2024-04-17 11:10:55 +0200

    x

--- internal/enginenetx/statspolicy.go
+++ internal/enginenetx/statspolicy.go
@@ -30,6 +30,7 @@ var _ httpsDialerPolicy = &statsPolicy{}
 
 // LookupTactics implements HTTPSDialerPolicy.
 func (p *statsPolicy) LookupTactics(ctx context.Context, domain string, port string) <-chan *httpsDialerTactic {
+	// avoid emitting nil tactics and duplicate tactics
 	return filterOnlyKeepUniqueTactics(filterOutNilTactics(mixSequentially(
 		// give priority to what we know from stats
 		streamTacticsFromSlice(statsPolicyFilterStatsTactics(p.Stats.LookupTactics(domain, port))),
