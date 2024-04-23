341fcf2a31eb20956eafc910e2dacc36ef06ca59
Simone Basso <bassosimone@gmail.com>
2024-04-23 15:21:35 +0200

    x

--- internal/probeservices/tor.go
+++ internal/probeservices/tor.go
@@ -10,14 +10,21 @@ import (
 
 // FetchTorTargets returns the targets for the tor experiment.
 func (c Client) FetchTorTargets(ctx context.Context, cc string) (result map[string]model.OOAPITorTarget, err error) {
+	// get credentials and authentication token
 	_, auth, err := c.GetCredsAndAuth()
 	if err != nil {
 		return nil, err
 	}
+
+	// format Authorization header value
 	s := fmt.Sprintf("Bearer %s", auth.Token)
+
 	client := c.APIClientTemplate.BuildWithAuthorization(s)
+
+	// create query string
 	query := url.Values{}
 	query.Add("country_code", cc)
+
 	err = client.GetJSONWithQuery(
 		ctx, "/api/v1/test-list/tor-targets", query, &result)
 	return
