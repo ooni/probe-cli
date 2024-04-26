d2be96341b782c540a0250d5040772988778a6c5
Simone Basso <bassosimone@gmail.com>
2024-04-26 16:55:31 +0200

    y

--- internal/probeservices/tor.go
+++ internal/probeservices/tor.go
@@ -5,37 +5,20 @@ import (
 	"fmt"
 	"net/url"
 
-	"github.com/ooni/probe-cli/v3/internal/httpclientx"
 	"github.com/ooni/probe-cli/v3/internal/model"
-	"github.com/ooni/probe-cli/v3/internal/urlx"
 )
 
 // FetchTorTargets returns the targets for the tor experiment.
-func (c Client) FetchTorTargets(ctx context.Context, cc string) (map[string]model.OOAPITorTarget, error) {
-	// get credentials and authentication token
+func (c Client) FetchTorTargets(ctx context.Context, cc string) (result map[string]model.OOAPITorTarget, err error) {
 	_, auth, err := c.GetCredsAndAuth()
 	if err != nil {
 		return nil, err
 	}
-
-	// format Authorization header value
 	s := fmt.Sprintf("Bearer %s", auth.Token)
-
-	// create query string
+	client := c.APIClientTemplate.BuildWithAuthorization(s)
 	query := url.Values{}
 	query.Add("country_code", cc)
-
-	// construct the URL to use
-	URL, err := urlx.ResolveReference(c.BaseURL, "/api/v1/test-list/tor-targets", query.Encode())
-	if err != nil {
-		return nil, err
-	}
-
-	// get response
-	return httpclientx.GetJSON[map[string]model.OOAPITorTarget](ctx, URL, &httpclientx.Config{
-		Authorization: s,
-		Client:        c.HTTPClient,
-		Logger:        c.Logger,
-		UserAgent:     c.UserAgent,
-	})
+	err = client.GetJSONWithQuery(
+		ctx, "/api/v1/test-list/tor-targets", query, &result)
+	return
 }
