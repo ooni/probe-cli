37937a8c44ecd697fcb2d29380fc9e396526234b
Simone Basso <bassosimone@gmail.com>
2024-04-29 12:27:32 +0200

    x

--- internal/probeservices/bouncer.go
+++ internal/probeservices/bouncer.go
@@ -1,29 +1,14 @@
 package probeservices
 
-//
-// bouncer.go - GET /api/v1/test-helpers
-//
-
 import (
 	"context"
 
-	"github.com/ooni/probe-cli/v3/internal/httpclientx"
 	"github.com/ooni/probe-cli/v3/internal/model"
-	"github.com/ooni/probe-cli/v3/internal/urlx"
 )
 
-// GetTestHelpers queries the /api/v1/test-helpers API.
-func (c *Client) GetTestHelpers(ctx context.Context) (map[string][]model.OOAPIService, error) {
-	// construct the URL to use
-	URL, err := urlx.ResolveReference(c.BaseURL, "/api/v1/test-helpers", "")
-	if err != nil {
-		return nil, err
-	}
-
-	// get the response
-	return httpclientx.GetJSON[map[string][]model.OOAPIService](ctx, URL, &httpclientx.Config{
-		Client:    c.HTTPClient,
-		Logger:    c.Logger,
-		UserAgent: c.UserAgent,
-	})
+// GetTestHelpers is like GetCollectors but for test helpers.
+func (c Client) GetTestHelpers(
+	ctx context.Context) (output map[string][]model.OOAPIService, err error) {
+	err = c.APIClientTemplate.WithBodyLogging().Build().GetJSON(ctx, "/api/v1/test-helpers", &output)
+	return
 }
