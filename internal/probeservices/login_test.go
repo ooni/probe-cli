70aa57df46c1970bea6d06e07094eec1c46ccafa
Simone Basso <bassosimone@gmail.com>
2024-04-29 11:18:30 +0200

    x

--- internal/probeservices/login_test.go
+++ internal/probeservices/login_test.go
@@ -201,7 +201,7 @@ func TestMaybeLogin(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		// now call loging and we expect no error because we should
+		// now call login and we expect no error because we should
 		// already have what we need to perform a login
 		if err := clnt.MaybeLogin(context.Background()); err != nil {
 			t.Fatal(err)
