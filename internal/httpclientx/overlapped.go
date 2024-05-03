9c877240336d27cf4339919c21b1c0039f48d55d
Simone Basso <bassosimone@gmail.com>
2024-05-03 11:34:33 +0200

    x

--- internal/httpclientx/overlapped.go
+++ internal/httpclientx/overlapped.go
@@ -164,7 +164,7 @@ func (ovx *Overlapped[Output]) Map(ctx context.Context, epnts ...*Endpoint) []*O
 
 		select {
 		// this event means that a child goroutine completed
-		// so we store the res; on success interrupt all the
+		// so we store the result; on success interrupt all the
 		// background goroutines and stop ticking
 		//
 		// note that we MUST continue reading until we have
