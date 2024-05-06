f169fb6c9c1678de2c34b2986e2104daec894b0c
Ain Ghazal <ainghazal42@gmail.com>
2024-05-06 15:56:39 +0200

    test stub

---
+++ internal/probeservices/openvpn_test.go
@@ -0,0 +1,12 @@
+package probeservices
+
+import (
+	"testing"
+)
+
+func TestFetchOpenVPNConfig(t *testing.T) {
+
+	t.Run("is working as intended with a local test server", func(t *testing.T) {
+		// TODO(ain)
+	})
+}
