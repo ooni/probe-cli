d915ab4528616caaab7791267f55c01c4b46a10c
DecFox <33030671+DecFox@users.noreply.github.com>
2024-08-07 20:40:19 +0530

    chore: update assets and definitions (#1647)
    ## Checklist
    
    - [x] I have read the [contribution
    guidelines](https://github.com/ooni/probe-cli/blob/master/CONTRIBUTING.md)
    - [x] reference issue for this pull request:
    https://github.com/ooni/probe/issues/2783
    - [ ] if you changed anything related to how experiments work and you
    need to reflect these changes in the ooni/spec repository, please link
    to the related ooni/spec pull request: <!-- add URL here -->
    - [ ] if you changed code inside an experiment, make sure you bump its
    version number
    
    <!-- Reminder: Location of the issue tracker:
    https://github.com/ooni/probe -->
    
    ## Description
    
    This diff updates assets and definitions for release v3.23.0.

--- internal/model/http.go
+++ internal/model/http.go
@@ -15,7 +15,7 @@ const (
 	// HTTPHeaderUserAgent is the User-Agent header used for measuring. The current header
 	// is 36.86% of the browser population as of 2024-05-13 according to the
 	// https://www.useragents.me/ webpage.
-	HTTPHeaderUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.3"
+	HTTPHeaderUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.3"
 )
 
 // Additional strings used to report HTTP errors. They're currently only used by
