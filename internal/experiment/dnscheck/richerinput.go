d3c1c93f700b63a25d548d58264fc541e182a81f
decfox <mehul@openobservatory.org>
2024-09-05 12:54:45 +0530

    fix(dnscheck): reinstate richer input loader

--- internal/experiment/dnscheck/richerinput.go
+++ internal/experiment/dnscheck/richerinput.go
@@ -117,9 +117,183 @@ var defaultInput = []model.ExperimentTarget{
 			DefaultAddrs: "8.8.8.8 8.8.4.4",
 		},
 	},
-
-	// TODO(bassosimone,DecFox): before releasing, we need to either sync up
-	// this list with ./internal/targetloader or implement a backend API.
+	&Target{
+		URL: "https://cloudflare-dns.com/dns-query",
+		Config: &Config{
+			HTTP3Enabled: true,
+			DefaultAddrs: "1.1.1.1 1.0.0.1",
+		},
+	},
+	&Target{
+		URL: "https://cloudflare-dns.com/dns-query",
+		Config: &Config{
+			DefaultAddrs: "1.1.1.1 1.0.0.1",
+		},
+	},
+	&Target{
+		URL: "https://dns.quad9.net/dns-query",
+		Config: &Config{
+			HTTP3Enabled: true,
+			DefaultAddrs: "9.9.9.9",
+		},
+	},
+	&Target{
+		URL: "https://dns.quad9.net/dns-query",
+		Config: &Config{
+			DefaultAddrs: "9.9.9.9",
+		},
+	},
+	&Target{
+		URL: "https://family.cloudflare-dns.com/dns-query",
+		Config: &Config{
+			HTTP3Enabled: true,
+		},
+	},
+	&Target{
+		URL:    "https://family.cloudflare-dns.com/dns-query",
+		Config: &Config{},
+	},
+	&Target{
+		URL: "https://dns11.quad9.net/dns-query",
+		Config: &Config{
+			HTTP3Enabled: true,
+		},
+	},
+	&Target{
+		URL:    "https://dns11.quad9.net/dns-query",
+		Config: &Config{},
+	},
+	&Target{
+		URL: "https://dns9.quad9.net/dns-query",
+		Config: &Config{
+			HTTP3Enabled: true,
+		},
+	},
+	&Target{
+		URL:    "https://dns9.quad9.net/dns-query",
+		Config: &Config{},
+	},
+	&Target{
+		URL: "https://dns12.quad9.net/dns-query",
+		Config: &Config{
+			HTTP3Enabled: true,
+		},
+	},
+	&Target{
+		URL:    "https://dns12.quad9.net/dns-query",
+		Config: &Config{},
+	},
+	&Target{
+		URL: "https://1dot1dot1dot1.cloudflare-dns.com/dns-query",
+		Config: &Config{
+			HTTP3Enabled: true,
+		},
+	},
+	&Target{
+		URL:    "https://1dot1dot1dot1.cloudflare-dns.com/dns-query",
+		Config: &Config{},
+	},
+	&Target{
+		URL: "https://dns.adguard.com/dns-query",
+		Config: &Config{
+			HTTP3Enabled: true,
+		},
+	},
+	&Target{
+		URL:    "https://dns.adguard.com/dns-query",
+		Config: &Config{},
+	},
+	&Target{
+		URL: "https://dns-family.adguard.com/dns-query",
+		Config: &Config{
+			HTTP3Enabled: true,
+		},
+	},
+	&Target{
+		URL:    "https://dns-family.adguard.com/dns-query",
+		Config: &Config{},
+	},
+	&Target{
+		URL: "https://dns.cloudflare.com/dns-query",
+		Config: &Config{
+			HTTP3Enabled: true,
+		},
+	},
+	&Target{
+		URL:    "https://dns.cloudflare.com/dns-query",
+		Config: &Config{},
+	},
+	&Target{
+		URL: "https://adblock.doh.mullvad.net/dns-query",
+		Config: &Config{
+			HTTP3Enabled: true,
+		},
+	},
+	&Target{
+		URL:    "https://adblock.doh.mullvad.net/dns-query",
+		Config: &Config{},
+	},
+	&Target{
+		URL: "https://dns.alidns.com/dns-query",
+		Config: &Config{
+			HTTP3Enabled: true,
+		},
+	},
+	&Target{
+		URL:    "https://dns.alidns.com/dns-query",
+		Config: &Config{},
+	},
+	&Target{
+		URL: "https://doh.opendns.com/dns-query",
+		Config: &Config{
+			HTTP3Enabled: true,
+		},
+	},
+	&Target{
+		URL:    "https://doh.opendns.com/dns-query",
+		Config: &Config{},
+	},
+	&Target{
+		URL: "https://dns.nextdns.io/dns-query",
+		Config: &Config{
+			HTTP3Enabled: true,
+		},
+	},
+	&Target{
+		URL:    "https://dns.nextdns.io/dns-query",
+		Config: &Config{},
+	},
+	&Target{
+		URL: "https://dns10.quad9.net/dns-query",
+		Config: &Config{
+			HTTP3Enabled: true,
+		},
+	},
+	&Target{
+		URL:    "https://dns10.quad9.net/dns-query",
+		Config: &Config{},
+	},
+	&Target{
+		URL: "https://security.cloudflare-dns.com/dns-query",
+		Config: &Config{
+			HTTP3Enabled: true,
+		},
+	},
+	&Target{
+		URL:    "https://security.cloudflare-dns.com/dns-query",
+		Config: &Config{},
+	},
+	&Target{
+		URL: "https://dns.switch.ch/dns-query",
+		Config: &Config{
+			HTTP3Enabled: true,
+		},
+	},
+	&Target{
+		URL:    "https://dns.switch.ch/dns-query",
+		Config: &Config{},
+	},
+	// TODO(DecFox): implement a backend API to serve this list as dnscheck inputs
 }
 
 func (tl *targetLoader) loadFromBackend(_ context.Context) ([]model.ExperimentTarget, error) {
