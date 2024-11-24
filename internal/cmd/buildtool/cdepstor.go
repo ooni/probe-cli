e3fe0ef53263cf93a1fc5872ef24b743f1bc40f1
DecFox <33030671+DecFox@users.noreply.github.com>
2024-11-25 03:59:42 +0530

    chore: upgrade C dependencies (#1664)
    ## Checklist
    
    - [x] I have read the [contribution
    guidelines](https://github.com/ooni/probe-cli/blob/master/CONTRIBUTING.md)
    - [x] reference issue for this pull request:
    https://github.com/ooni/probe/issues/2813
    - [ ] if you changed anything related to how experiments work and you
    need to reflect these changes in the ooni/spec repository, please link
    to the related ooni/spec pull request: <!-- add URL here -->
    - [ ] if you changed code inside an experiment, make sure you bump its
    version number
    
    <!-- Reminder: Location of the issue tracker:
    https://github.com/ooni/probe -->
    
    ## Description
    
    This diff updates the engine C dependencies.

--- internal/cmd/buildtool/cdepstor.go
+++ internal/cmd/buildtool/cdepstor.go
@@ -27,13 +27,13 @@ func cdepsTorBuildMain(globalEnv *cBuildEnv, deps buildtoolmodel.Dependencies) {
 	defer restore()
 
 	// See https://github.com/Homebrew/homebrew-core/blob/master/Formula/t/tor.rb
-	cdepsMustFetch("https://www.torproject.org/dist/tor-0.4.8.12.tar.gz")
+	cdepsMustFetch("https://www.torproject.org/dist/tor-0.4.8.13.tar.gz")
 	deps.VerifySHA256( // must be mockable
-		"ca7cc735d98e3747b58f2f3cc14f804dd789fa0fb333a84dcb6bd70adbb8c874",
-		"tor-0.4.8.12.tar.gz",
+		"9baf26c387a2820b3942da572146e6eb77c2bc66862af6297cd02a074e6fba28",
+		"tor-0.4.8.13.tar.gz",
 	)
-	must.Run(log.Log, "tar", "-xf", "tor-0.4.8.12.tar.gz")
-	_ = deps.MustChdir("tor-0.4.8.12") // must be mockable
+	must.Run(log.Log, "tar", "-xf", "tor-0.4.8.13.tar.gz")
+	_ = deps.MustChdir("tor-0.4.8.13") // must be mockable
 
 	mydir := filepath.Join(topdir, "CDEPS", "tor")
 	for _, patch := range cdepsMustListPatches(mydir) {
