5d7cb51ad2e0bb71bd477476a3f62eeea8f12c69
decfox <mehul@openobservatory.org>
2025-06-12 03:18:04 +0530

    chore: update http user agent

--- internal/model/http.go
+++ internal/model/http.go
@@ -15,7 +15,7 @@ const (
 	// HTTPHeaderUserAgent is the User-Agent header used for measuring. The current header
 	// is 40.98% of the browser population as of 2025-02-09 according to the
 	// https://www.useragents.me/ webpage.
-	HTTPHeaderUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.3"
+	HTTPHeaderUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.10 Safari/605.1.1"
 )
 
 // Additional strings used to report HTTP errors. They're currently only used by
