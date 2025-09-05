c5c46eb7f2285d9a87de5b53591e64223250f9f3
decfox <mehul@openobservatory.org>
2025-09-05 23:53:33 +0530

    chore: update user agent header for tests

--- internal/model/http.go
+++ internal/model/http.go
@@ -13,7 +13,7 @@ const (
 	HTTPHeaderAcceptLanguage = "en-US,en;q=0.9"
 
 	// HTTPHeaderUserAgent is the User-Agent header used for measuring. The current header
-	// is 40.98% of the browser population as of 2025-02-09 according to the
+	// is 43.03% of the browser population as of 2025-07-05 according to the
 	// https://www.useragents.me/ webpage.
 	HTTPHeaderUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.10 Safari/605.1.1"
 )
