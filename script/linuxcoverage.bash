7a928715963ba9a03009c82223cccf285ddf2f2c
decfox <mehul@openobservatory.org>
2026-07-24 13:35:37 +0530

    chore: build covdata explicitly

--- script/linuxcoverage.bash
+++ script/linuxcoverage.bash
@@ -17,5 +17,15 @@ if [[ ! -x $go ]]; then
 	exit 1
 fi
 
+# Since go1.25 the toolchain no longer ships covdata as a prebuilt binary. When
+# `go test -cover` needs it for a package that has no test files, go tries to obtain
+# it by downloading a toolchain, which cannot work inside the network namespace we
+# enter below. See https://github.com/golang/go/issues/75031, which upstream only 
+# fixes in go1.27.
+tooldir=$($go env GOROOT)/pkg/tool/$($go env GOOS)_$($go env GOARCH)
+if [[ ! -x $tooldir/covdata ]]; then
+	$go build -o "$tooldir/covdata" cmd/covdata
+fi
+
 # run tests using a different network namespace
 sudo unshare --net ./script/linuxcoveragerun.bash "$go"
