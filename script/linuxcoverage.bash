2540dc2a19689968fce77d36586ee01f3e6d2db6
DecFox <33030671+DecFox@users.noreply.github.com>
2026-07-24 13:41:59 +0530

    chore: upgrade to go1.25.3 (#1791)
    ## Checklist
    
    - [x] I have read the [contribution
    guidelines](https://github.com/ooni/probe-cli/blob/master/CONTRIBUTING.md)
    - [x] reference issue for this pull request:
    https://github.com/ooni/probe-cli/issues/1790
    - [ ] if you changed anything related to how experiments work and you
    need to reflect these changes in the ooni/spec repository, please link
    to the related ooni/spec pull request: <!-- add URL here -->
    - [ ] if you changed code inside an experiment, make sure you bump its
    version number
    
    <!-- Reminder: Location of the issue tracker:
    https://github.com/ooni/probe -->
    
    ## Description
    
    This is part of the routine release to upgrade to go 1.25

--- script/linuxcoverage.bash
+++ script/linuxcoverage.bash
@@ -7,12 +7,25 @@
 
 set -euxo pipefail
 
-# obtain the full path of the go executable
-go=$(which go)
+# Use the pinned toolchain (see GOVERSION) to vendor.
+./script/go.bash mod vendor
 
-# populate the vendor directory so we don't need the network in `go test`
-go mod vendor
+# Resolve the absolute path of the pinned go binary.
+go=$HOME/sdk/go$(cat GOVERSION)/bin/go
+if [[ ! -x $go ]]; then
+	echo "FATAL: expected the pinned go toolchain at $go" 1>&2
+	exit 1
+fi
+
+# Since go1.25 the toolchain no longer ships covdata as a prebuilt binary. When
+# `go test -cover` needs it for a package that has no test files, go tries to obtain
+# it by downloading a toolchain, which cannot work inside the network namespace we
+# enter below. See https://github.com/golang/go/issues/75031, which upstream only 
+# fixes in go1.27.
+tooldir=$($go env GOROOT)/pkg/tool/$($go env GOOS)_$($go env GOARCH)
+if [[ ! -x $tooldir/covdata ]]; then
+	$go build -o "$tooldir/covdata" cmd/covdata
+fi
 
 # run tests using a different network namespace
-sudo unshare --net ./script/linuxcoveragerun.bash $go
-
+sudo unshare --net ./script/linuxcoveragerun.bash "$go"
